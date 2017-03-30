ed6efd1ac95932f8787f121d18b213560f113f7f
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-03-30 14:38:38 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var Button = require('./lib/Button/Button');
+var Button = require('./lib/Button');
 exports.Button = Button.default;
 
-var CircularLoading = require('./lib/CircularLoading/CircularLoading');
+var CircularLoading = require('./lib/CircularLoading');
 exports.CircularLoading = CircularLoading.default;
 
-var TouchRipple = require('./lib/TouchRipple/TouchRipple');
+var TouchRipple = require('./lib/TouchRipple');
 exports.TouchRipple = TouchRipple.default;
