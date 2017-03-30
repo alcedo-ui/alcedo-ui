c0f1e42d59acc978da46f3284853577edb872e75
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-03-30 14:17:12 +0800

    update

---
+++ index.es.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import _Button from './lib/Button/Button';
+export {_Button as Button};
+
+import _CircularLoading from './lib/CircularLoading/CircularLoading';
+export {_CircularLoading as CircularLoading};
+
+import _TouchRipple from './lib/TouchRipple/TouchRipple';
+export {_TouchRipple as TouchRipple};
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-module.exports = require('./lib/React');
+var Button = require('./lib/Button/Button');
+exports.Button = Button.default;
+
+var CircularLoading = require('./lib/CircularLoading/CircularLoading');
+exports.CircularLoading = CircularLoading.default;
+
+var TouchRipple = require('./lib/TouchRipple/TouchRipple');
+exports.TouchRipple = TouchRipple.default;
