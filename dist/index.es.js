54cdbb68bf153fabd3a55688ed9f6f61d3743824
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-04-06 14:05:45 +0800

    update

---
+++ dist/index.es.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import _RaisedButton from './RaisedButton';
+export {_RaisedButton as RaisedButton};
+
+import _CircularLoading from './CircularLoading';
+export {_CircularLoading as CircularLoading};
+
+import _TouchRipple from './TouchRipple';
+export {_TouchRipple as TouchRipple};
---
+++ dist/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var RaisedButton = require('./RaisedButton');
+exports.RaisedButton = RaisedButton.default;
+
+var CircularLoading = require('./CircularLoading');
+exports.CircularLoading = CircularLoading.default;
+
+var TouchRipple = require('./TouchRipple');
+exports.TouchRipple = TouchRipple.default;
