8e08994e7c60c19a6eff569b25d81918edb48f80
chao.zhang <chao.zhang@derbysoft.com>
2017-04-07 17:26:18 +0800

    dialog,circularprogress

---
+++ examples/containers/app/modules/progress/CircularProgressExample.js
@@ -0,0 +1,16 @@
+/**
+ * Created by DT314 on 2017/4/7.
+ */
+import React, {Component, PropTypes} from 'react'
+
+import CircularLoading from '../../../../../dist/CircularLoading'
+
+export default class Test extends Component{
+    constructor(props){
+        super()
+    }
+
+    render() {
+        return <CircularLoading />
+    }
+}
