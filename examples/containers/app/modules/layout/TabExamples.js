66583b5d7d6e32ec8c12907e5cf410a3f1eb506b
Wendy <wendy.wei@derbysoft.com>
2017-04-19 16:38:38 +0800

    update

--- examples/containers/app/modules/layout/TabExamples.js
+++ examples/containers/app/modules/layout/TabExamples.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
 import Tab from 'dist/Tab';
+import Widget from 'dist/Widget';
+import WidgetHeader from 'dist/WidgetHeader';
 
 import 'sass/containers/app/modules/layout/TabExamples.scss';
 
@@ -20,64 +22,125 @@ export default class TabExamples extends Component {
 
     render() {
         return (
-            <div className="example widget-examples">
+            <div className="example tab-example">
 
-                <div className="examples">
-                    <div className="examples-title">Tabs</div>
-                    <div className="examples-wrapper">
+                <h2 className="example-title">Tabs</h2>
 
-                        <Tab tabs={[{
-                            value: 'Tab 1',
-                            renderer: <div>
-                                <h1>Tab 1</h1>
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="Tabs Simple Example"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <Tab tabs={[{
+                                    value: 'Tab 1',
+                                    renderer: <div>
+                                        <h1>Tab 1</h1>
+                                    </div>
+                                }, {
+                                    value: 'Tab 2',
+                                    renderer: <div>
+                                        <h1>Tab 2</h1>
+                                    </div>
+                                }, {
+                                    value: 'onActive',
+                                    renderer: <div>
+                                        <h1>onActive</h1>
+                                    </div>,
+                                    onActive: this.activeHandle
+                                }, {
+                                    value: 'Disabled',
+                                    disabled: true
+                                }]}
+                                     isTabFullWidth={false}/>
+
                             </div>
-                        }, {
-                            value: 'Tab 2',
-                            renderer: <div>
-                                <h1>Tab 2</h1>
-                            </div>
-                        }, {
-                            value: 'onActive',
-                            renderer: <div>
-                                <h1>onActive</h1>
-                            </div>,
-                            onActive: this.activeHandle
-                        }, {
-                            value: 'Disabled',
-                            disabled: true
-                        }]}
-                             isTabFullWidth={false}/>
 
+                        </div>
                     </div>
+
+                </Widget>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="Full Width Tabs Simple Example"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <Tab tabs={[{
+                                    value: 'Tab 1',
+                                    renderer: <div>
+                                        <h1>Tab 1</h1>
+                                    </div>
+                                }, {
+                                    value: 'Tab 2',
+                                    renderer: <div>
+                                        <h1>Tab 2</h1>
+                                    </div>
+                                }, {
+                                    value: 'onActive',
+                                    renderer: <div>
+                                        <h1>onActive</h1>
+                                    </div>,
+                                    onActive: this.activeHandle
+                                }, {
+                                    value: 'Disabled',
+                                    disabled: true
+                                }]}/>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <div className="example-properties">
+                    <h3>Properties</h3>
+                    <table className="example-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Default</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="properties-name">className</td>
+                                <td className="properties-type">PropTypes.string</td>
+                                <td></td>
+                                <td>The css class name of the root element.</td>
+                            </tr>
+                            <tr>
+                                <td className="properties-name">style</td>
+                                <td className="properties-type">PropTypes.object</td>
+                                <td></td>
+                                <td>The styles of the root element.</td>
+                            </tr>
+                            <tr>
+                                <td className="properties-name">isTabFullWidth</td>
+                                <td className="properties-type">PropTypes.bool</td>
+                                <td>true</td>
+                                <td>Whether the tabs is FullWidth.</td>
+                            </tr>
+                            <tr>
+                                <td className="properties-name">tabs</td>
+                                <td className="properties-type">PropTypes.array</td>
+                                <td></td>
+                                <td>Children passed into the TabsItem.</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
 
-                <div className="examples">
-                    <div className="examples-title">Full Width Tabs</div>
-                    <div className="examples-wrapper">
-
-                        <Tab tabs={[{
-                            value: 'Tab 1',
-                            renderer: <div>
-                                <h1>Tab 1</h1>
-                            </div>
-                        }, {
-                            value: 'Tab 2',
-                            renderer: <div>
-                                <h1>Tab 2</h1>
-                            </div>
-                        }, {
-                            value: 'onActive',
-                            renderer: <div>
-                                <h1>onActive</h1>
-                            </div>,
-                            onActive: this.activeHandle
-                        }, {
-                            value: 'Disabled',
-                            disabled: true
-                        }]}/>
-
-                    </div>
-                </div>
 
             </div>
         );
--- examples/containers/app/modules/layout/WidgetExamples.js
+++ examples/containers/app/modules/layout/WidgetExamples.js
@@ -16,24 +16,69 @@ export default class WidgetExamples extends Component {
         return (
             <div className="example widget-examples">
 
-                <div className="examples">
-                    <div className="examples-title">Widget</div>
-                    <div className="examples-wrapper">
+                <h2 className="example-title">Widget</h2>
 
-                        <Widget>
+                <Widget>
 
-                            <WidgetHeader title="Title">
-                                <IconButton iconCls="fa fa-thumbs-up"/>
-                                <IconButton iconCls="fa fa-trash"/>
-                            </WidgetHeader>
+                    <WidgetHeader className="example-header" title="Widget Simple Example"/>
 
-                            <div className="widget-content">
-                                content
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <Widget>
+
+                                    <WidgetHeader title="Title">
+                                        <IconButton iconCls="fa fa-thumbs-up"/>
+                                        <IconButton iconCls="fa fa-trash"/>
+                                    </WidgetHeader>
+
+                                    <div className="widget-content">
+                                        content
+                                    </div>
+
+                                </Widget>
+
                             </div>
 
-                        </Widget>
-
+                        </div>
                     </div>
+
+                </Widget>
+
+                <div className="example-properties">
+                    <h3>Properties</h3>
+                    <table className="example-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Default</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="properties-name">className</td>
+                                <td className="properties-type">PropTypes.string</td>
+                                <td></td>
+                                <td>The css class name of the root element.</td>
+                            </tr>
+                            <tr>
+                                <td className="properties-name">style</td>
+                                <td className="properties-type">PropTypes.object</td>
+                                <td></td>
+                                <td>The styles of the root element.</td>
+                            </tr>
+                            <tr>
+                                <td className="properties-name">depth</td>
+                                <td className="properties-type">PropTypes.number</td>
+                                <td>1</td>
+                                <td>This number represents the zDepth of the root element shadow.</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
 
             </div>
