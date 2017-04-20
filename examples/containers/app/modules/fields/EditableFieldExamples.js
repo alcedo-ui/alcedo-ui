780507ac357457d6b920cdfa90ce7a73bcc4aea2
chao.zhang <chao.zhang@derbysoft.com>
2017-04-20 10:04:14 +0800

    fix

--- examples/containers/app/modules/fields/EditableFieldExamples.js
+++ examples/containers/app/modules/fields/EditableFieldExamples.js
@@ -9,11 +9,15 @@ export default class EditableFieldExamples extends Component{
         super();
     }
 
+    onChange(text) {
+        console.log(text);
+    }
+
     render() {
         return (
             <div style={{margin: '20px'}}>
                 <div>Example</div>
-                <EditableField value={'text1'} style={{height: '30px', lineHeight: '30px'}}/>
+                <EditableField value={'text1'} style={{height: '30px', lineHeight: '30px'}} onChange={this.onChange}/>
             </div>
         );
     }
