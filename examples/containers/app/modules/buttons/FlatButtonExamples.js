425d82e14a27a5b7fdcee6e80a461e7a02a8a444
Wendy <wendy.wei@derbysoft.com>
2017-04-21 16:32:12 +0800

    FlatButtonExample  update

--- examples/containers/app/modules/buttons/FlatButtonExamples.js
+++ examples/containers/app/modules/buttons/FlatButtonExamples.js
@@ -17,86 +17,14 @@ export default class FlatButtonExamples extends Component {
 
     render() {
 
-        // let propertiesList = [{
-        //     Name: 'className',
-        //     Type: 'PropTypes.string',
-        //     Default: '',
-        //     Description: 'The css class name of the root element.'
-        // }, {
-        //     Name: 'style',
-        //     Type: 'PropTypes.object',
-        //     Default: '',
-        //     Description: 'The styles of the root element.'
-        // }, {
-        //     Name: 'theme',
-        //     Type: 'PropTypes.string',
-        //     Default: '',
-        //     Description: 'The button theme.'
-        // }, {
-        //     Name: 'isRounded',
-        //     Type: 'PropTypes.bool',
-        //     Default: 'false',
-        //     Description: 'Whether need rounded corners.'
-        // }, {
-        //     Name: 'isCircular',
-        //     Type: 'PropTypes.bool',
-        //     Default: 'false',
-        //     Description: 'Round button.'
-        // }, {
-        //     Name: 'value',
-        //     Type: 'PropTypes.string || PropTypes.number',
-        //     Default: '',
-        //     Description: 'The button text value.'
-        // }, {
-        //     Name: 'type',
-        //     Type: 'PropTypes.string',
-        //     Default: 'button',
-        //     Description: ''
-        // }, {
-        //     Name: 'disabled',
-        //     Type: 'PropTypes.bool',
-        //     Default: 'false',
-        //     Description: 'Button disabled.'
-        // }, {
-        //     Name: 'isLoading',
-        //     Type: 'PropTypes.bool',
-        //     Default: 'false',
-        //     Description: ''
-        // }, {
-        //     Name: 'disableTouchRipple',
-        //     Type: ' PropTypes.bool',
-        //     Default: 'false',
-        //     Description: ''
-        // }, {
-        //     Name: 'iconCls',
-        //     Type: ' PropTypes.string',
-        //     Default: '',
-        //     Description: 'Button left icon className.'
-        // }, {
-        //     Name: 'rightIconCls',
-        //     Type: ' PropTypes.string',
-        //     Default: '',
-        //     Description: 'Button right icon className.'
-        // }, {
-        //     Name: 'rippleDisplayCenter',
-        //     Type: ' PropTypes.bool',
-        //     Default: 'false',
-        //     Description: 'Ripple effect is centered.'
-        // }, {
-        //     Name: 'onTouchTap',
-        //     Type: ' PropTypes.func',
-        //     Default: '',
-        //     Description: 'The function that trigger when the button is clicked.'
-        // }];
-
         return (
-            <div className="example flat-button-example">
+            <div className="example flat-button-examples">
 
                 <h2 className="example-title">Flat Button</h2>
 
                 <Widget>
 
-                    <WidgetHeader className="example-header" title="Flat Button Simple Example"/>
+                    <WidgetHeader className="example-header" title="Flat Button Example"/>
 
                     <div className="widget-content">
                         <div className="example-content">
@@ -201,36 +129,6 @@ export default class FlatButtonExamples extends Component {
                 <h2 className="example-title">Properties</h2>
 
                 <PropTypeDescTable data={FlatButtonDoc}/>
-
-                {/*<div className="example-properties">*/}
-                    {/*<h3>Properties</h3>*/}
-                    {/*<table className="example-table">*/}
-                        {/*<thead>*/}
-                            {/*<tr>*/}
-                                {/*<th>Name</th>*/}
-                                {/*<th>Type</th>*/}
-                                {/*<th>Default</th>*/}
-                                {/*<th>Description</th>*/}
-                            {/*</tr>*/}
-                        {/*</thead>*/}
-                        {/*<tbody>*/}
-                            {/*{*/}
-                                {/*propertiesList && propertiesList.length > 0 ?*/}
-                                    {/*(*/}
-                                        {/*propertiesList.map(function (val) {*/}
-                                            {/*return <tr>*/}
-                                                {/*<td className="properties-name">{val.Name}</td>*/}
-                                                {/*<td className="properties-type">{val.Type}</td>*/}
-                                                {/*<td>{val.Default}</td>*/}
-                                                {/*<td>{val.Description}</td>*/}
-                                            {/*</tr>*/}
-                                        {/*})*/}
-                                    {/*)*/}
-                                    {/*: null*/}
-                            {/*}*/}
-                        {/*</tbody>*/}
-                    {/*</table>*/}
-                {/*</div>*/}
 
             </div>
         );
