35e3b680cf71d09083e5112e2375824306669d00
Wendy <wendy.wei@derbysoft.com>
2017-04-21 15:19:16 +0800

    tip toaster update

--- examples/containers/app/modules/tips/TipExamples.js
+++ examples/containers/app/modules/tips/TipExamples.js
@@ -2,22 +2,49 @@
  * Created by Wendy on 2017/4/10.
  */
 import React, {Component, PropTypes} from 'react'
+import Widget from 'dist/Widget';
+import WidgetHeader from 'dist/WidgetHeader';
+
+import PropTypeDescTable from '../PropTypeDescTable';
+import TipDoc from 'docs/Tip.json';
 
 import Tip from '../../../../../dist/Tip/Tip';
 
 
-export default class DialogExamples extends Component{
-    constructor(props){
+export default class DialogExamples extends Component {
+    constructor(props) {
         super(props);
 
     }
 
-    render(){
-        return(
-            <div>
-                <h3>tip Examples</h3>
-                <p>Please click this button to show tip.</p>
-                <Tip text="hello"/>
+    render() {
+        return (
+            <div className="example tip-examples">
+
+                <h2 className="example-title">Tip</h2>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="Tip Simple Example"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <Tip text="hello"/>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <h2 className="example-title">Properties</h2>
+
+                <PropTypeDescTable data={TipDoc}/>
+
             </div>
         )
     }
