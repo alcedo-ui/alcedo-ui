9a7438161a08fcee359ed1cd07b592fd4c7be409
chao.zhang <chao.zhang@derbysoft.com>
2017-05-08 18:28:07 +0800

    textArea

--- src/TextArea/TextArea.js
+++ src/TextArea/TextArea.js
@@ -3,7 +3,7 @@
  */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-
+import Event from '../_vendors/Event';
 
 import './TextArea.css';
 
@@ -12,33 +12,56 @@ export default class TextArea extends Component {
         super();
 
         this.state = {
-            focus: false
+            focus: false,
+            width: props.initialWidth,
+            height: props.initialHeight,
+            value: props.value
         };
 
         this.onFocus = this :: this.onFocus;
         this.onBlur = this :: this.onBlur;
         this.onChange = this :: this.onChange;
+        this.resizeHandle = this :: this.resizeHandle;
+        this.mouseUpHandle = this :: this.mouseUpHandle;
 
     }
 
     onFocus() {
         this.setState({
             focus: true
+        }, () => {
+            this.props.onFocus && this.props.onFocus();
         });
     }
 
     onBlur() {
         this.setState({
             focus: false
+        }, () => {
+            this.props.onBlur && this.props.onBlur();
         });
+    }
+
+    resizeHandle(e) {
+        this.setState({
+            width: e.target.clientWidth + 2,
+            height: e.target.clientHeight + 2
+        });
+    }
+
+    mouseUpHandle(e) {
+        if (e.target.clientWidth + 2 !== this.state.width || e.target.clientHeight + 2 !== this.state.height) {
+            this.resizeHandle(e);
+        }
     }
 
     onChange(ev) {
         const {initialHeight, maxHeight, autoSize} = this.props;
+        const oEvent = ev || event;
+        const {target} = oEvent;
 
         if (autoSize) {
-            const oEvent = ev || event;
-            const {target} = oEvent;
+
             const {style} = target;
 
             style.height = initialHeight + 'px';
@@ -52,30 +75,66 @@ export default class TextArea extends Component {
                 }
             }
         }
+
+        this.setState({
+            value: target.value
+        }, () => this.props.onChange && this.props.onChange());
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {width, height, value} = this.state;
+
+        if (nextProps.initialWidth !== width || nextProps.initialHeight !== height || nextProps.value !== value) {
+            this.setState({
+                width: nextProps.initialWidth,
+                height: nextProps.initialHeight,
+                value: nextProps.value
+            });
+        }
+    }
+
+    componentDidMount() {
+        const {autoSize} = this.props;
+
+        if (!autoSize) {
+            Event.addEvent(this.refs.textarea, 'mouseup', this.mouseUpHandle);
+        }
+    }
+
+    componentWillUnmount() {
+        const {autoSize} = this.props;
+
+        if (!autoSize) {
+            Event.removeEvent(this.refs.textarea, 'mouseup', this.mouseUpHandle);
+        }
     }
 
     render() {
-        const {className, style, cols, rows, initialHeight, autoSize} = this.props;
-        const {focus} = this.state;
+        const {className, style, cols, rows, initialHeight, autoSize, placeholder, maxWidth} = this.props;
+        const {focus, width, height, value} = this.state;
 
         let textStyle = {
-            height: initialHeight,
-            overflow: autoSize ? 'hidden' : 'auto'
+            height: height,
+            width: width,
+            overflow: autoSize ? 'hidden' : 'auto',
+            resize: autoSize ? 'none' : 'both',
+            maxWidth: maxWidth,
+            ...style
         };
 
         return (
-            <div className={`text-area ${className}`}
-                 style={style}>
-                <textarea cols={cols}
-                          rows={rows}
-                          placeholder="Please input something."
-                          onFocus={this.onFocus}
-                          onBlur={this.onBlur}
-                          onChange={this.onChange}
-                          className={focus ? 'area-focus' : ''}
-                          style={{...textStyle}}>
-                </textarea>
-            </div>
+            <textarea cols={cols}
+                      rows={rows}
+                      placeholder={placeholder}
+                      value={value}
+                      onFocus={this.onFocus}
+                      onBlur={this.onBlur}
+                      onChange={this.onChange}
+                      className={`text-area ${focus ? 'area-focus' : ''} ${className}`}
+                      style={{...textStyle}}
+                      ref='textarea'>
+            </textarea>
         );
     }
 };
@@ -108,14 +167,50 @@ TextArea.propTypes = {
     initialHeight: PropTypes.number,
 
     /**
+     * The initial height of textArea.
+     */
+    initialWidth: PropTypes.number,
+
+    /**
+     * The value of textArea.
+     */
+    value: PropTypes.string,
+
+    /**
      * The max height of textArea.
      */
     maxHeight: PropTypes.number,
 
     /**
+     * The max width of textArea.
+     */
+    maxWidth: PropTypes.number,
+
+    /**
      * If true, the textArea will change its size by words.
      */
-    autoSize: PropTypes.bool
+    autoSize: PropTypes.bool,
+
+    /**
+     * The placeholder of textArea.
+     */
+    placeholder: PropTypes.string,
+
+    /**
+     * When the value of textArea change, it will execute.
+     */
+    onChange: PropTypes.func,
+
+    /**
+     * When the textArea onBlur, it will execute.
+     */
+    onBlur: PropTypes.func,
+
+    /**
+     * When the textArea onFocus, it will execute.
+     */
+    onFocus: PropTypes.func
+
 };
 
 TextArea.defaultProps = {
@@ -126,7 +221,11 @@ TextArea.defaultProps = {
     cols: 50,
     rows: 3,
     initialHeight: 30,
+    initialWidth: 200,
     maxHeight: 100,
-    autoSize: true
+    maxWidth: 500,
+    autoSize: true,
+    placeholder: 'Please input something.',
+    value: ''
 
 };
