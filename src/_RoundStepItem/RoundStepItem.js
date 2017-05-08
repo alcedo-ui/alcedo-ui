b07c48a8c226006e91156fe21de6e9cb985249ab
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-05-08 16:07:00 +0800

    update RoundStep

--- src/_RoundStepItem/RoundStepItem.js
+++ src/_RoundStepItem/RoundStepItem.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import './RoundStepItem.css';
+
 export default class RoundStepItem extends Component {
 
     constructor(props) {
