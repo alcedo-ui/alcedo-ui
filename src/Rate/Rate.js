5ead68764f1bcc858ebe27128aebf92d5e1458e4
sunday.wei <sunday.wei@derbysoft.com>
2017-05-08 11:23:52 +0800

    rate

--- src/Rate/Rate.js
+++ src/Rate/Rate.js
@@ -10,79 +10,176 @@ export default class Rate extends Component {
         super(props);
 
         this.state = {
-            value: 0
+            value: 0,
+            items:[]
         };
+
+        this.hoverHandle = this::this.hoverHandle;
+        this.createItems = this::this.createItems;
+        this.renderStar = this::this.renderStar;
     }
 
-    hoverHandle(value){
+    triggerEventHandle(el, triggerEl) {
 
-        if(value){
-            this.setState({
-                value
-            })
+        while (el) {
+            if (el == triggerEl) {
+                return true;
+            }
+            el = el.parentNode;
         }
+
+        return false;
+
     }
-    componentDidMount() {
-        const {value} =this.props;
-        if(value){
-            this.setState({
-                value
-            })
+
+    hoverHandle(e){
+        const mouseEnterFalg = this.triggerEventHandle(e.target , require('react-dom').findDOMNode(this.refs.rate));
+        let {value}=this.state;
+        if(mouseEnterFalg){
+            if(e.target.className.indexOf('filter') != -1) {
+
+            }
+        }else{
+            value = 0;
         }
+        const items = this.createItems(value);
+        this.setState({
+            items
+        })
     }
 
-    render() {
-        const {count, className, style ,allowHalf} = this.props;
-        const {value} = this.state;
-
-        const items = [];
-
+    createItems(value){
+        const {allowHalf,count} =this.props;
+        let items=[];
         if(allowHalf){
             for(let i = 0; i < count; i++){
-                let item =(
-                    <div className="half-star">
-                        <i className={`fa fa-star-half ${i < value ? 'full':'zero'}`}
-                           onMouseEnter={() => {
-                               this.hoverHandle(i);
-                           }}></i>
-                        <i className={`fa fa-star-half right  ${i < value && i+1 <= value ? 'full':'zero'}`}
-                           onMouseEnter={() => {
-                               this.hoverHandle(i+0.5);
-                           }}></i>
-                    </div>
-                )
-                items.push(item)
+                if(i <= value - 1){
+                    items.push('full')
+                }else if(i < value && i > value - 1){
+                    items.push('full-zero')
+                }else if(i > value){
+                    items.push('full')
+                }else{
+                    items.push('zero')
+                }
             }
         }else{
             for(let i = 0; i < count; i++){
-                let item =(
-                    <div className="star"
-                         onMouseEnter={() => {
-                             if(i=0){
-                                 this.hoverHandle(i);
-                             }else{
-                                 return
-                             }
-                         }}
-                    >
-                        <i className={`fa fa-star ${i < value ? 'full':'zero'}`}
-                           onMouseEnter={() => {
-                               this.hoverHandle(i+1);
-                           }}></i>
-                    </div>
-                )
-                items.push(item)
+                if( i < value){
+                    items.push('full')
+                }else if(i > value){
+                    items.push('zero')
+                }else{
+                    items.push('zero')
+                }
             }
         }
+        return items
+    }
+
+    renderStar(type,key){
+        const {allowHalf} = this.props;
+        if(allowHalf){
+            switch (type){
+                case 'zero':
+                    return (<div className="half-star"
+                                  key={key}>
+                        <div className="half-star-left">
+                            <i className={`fa fa-star-half zero`}></i>
+                        </div>
+                        <div className="half-star-right">
+                            <i className={`fa fa-star-half zero`}></i>
+                        </div>
+                    </div>)
+                    break;
+                case 'full':
+                    return (<div className="half-star"
+                                  key={key}>
+                        <div className="half-star-left">
+                            <i className={`fa fa-star-half full`}></i>
+                        </div>
+                        <div className="half-star-right">
+                            <i className={`fa fa-star-half full`}></i>
+                        </div>
+                    </div>)
+                    break;
+                case 'full-zero':
+                    return (<div className="half-star"
+                                  key={key}>
+                        <div className="half-star-left">
+                            <i className={`fa fa-star-half full`}></i>
+                        </div>
+                        <div className="half-star-right">
+                            <i className={`fa fa-star-half zero`}></i>
+                        </div>
+                    </div>)
+                    break;
+                default:
+                    return (<div className="half-star"
+                                  key={key}>
+                        <div className="half-star-left">
+                            <i className={`fa fa-star-half zero`}></i>
+                        </div>
+                        <div className="half-star-right">
+                            <i className={`fa fa-star-half zero`}></i>
+                        </div>
+                    </div>)
+            }
+        }else{
+            switch (type){
+                case 'zero':
+                    return (<div className="star"
+                                  key={key}>
+                        <i className={`fa fa-star zero`}></i>
+                    </div>)
+                    break;
+                case 'full':
+                    return (<div className="star"
+                                  key={key}>
+                        <i className={`fa fa-star full`}></i>
+                    </div>)
+                    break;
+                default:
+                    return (<div className="star"
+                                  key={key}>
+                        <i className={`fa fa-star zero`}></i>
+                    </div>)
+            }
+        }
+
+    }
+
+    componentDidMount() {
+        const {value} =this.props;
+        const items = this.createItems(value);
+        this.setState({
+            value,
+            items
+        })
+        Event.addEvent(document, 'mousemove', this.mouseMoveHandle);
+    }
+
+    componentWillUnmount() {
+        Event.removeEvent(document, 'mousemove', this.mouseMoveHandle);
+        this.unrenderTimeout && clearTimeout(this.unrenderTimeout);
+    }
+
+
+
+
+    render() {
+        const {count, className, style ,allowHalf} = this.props;
+        const {value,items} = this.state;
 
 
         return (
             <div className={`rate ${className ? className : ''}`}
-                 style={style}>
+                 style={style}
+                 ref = "rate">
                 {
                     items.map((item,key)=>{
                        return (
-                           <div key={key}>{item}</div>
+                           this.renderStar(item,key)
                        )
                     })
                 }
