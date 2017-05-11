ca098a9d007656a2d617cba75f849acd927f38f9
sunday.wei <sunday.wei@derbysoft.com>
2017-05-11 14:51:31 +0800

    rate

--- examples/containers/app/modules/Rate/RateExamples.js
+++ examples/containers/app/modules/Rate/RateExamples.js
@@ -34,7 +34,7 @@ export default class RateExamples extends Component {
         return (
             <div className="example rate-examples">
 
-                <h2 className="example-title">TimeLine</h2>
+                <h2 className="example-title">Rate</h2>
 
                 <p>
                     The Rate is used to display time flow information vertically.
