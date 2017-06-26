f7f4d2ee78bf2fe79424d04a8ac40e9a32652bcf
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-26 13:47:00 +0800

    update Waterfall

--- src/Waterfall/Waterfall.js
+++ src/Waterfall/Waterfall.js
@@ -114,7 +114,7 @@ export default class Waterfall extends Component {
         });
     }
 
-    componentWillReceiveProps(nextProps) {
+    componentWillReceiveProps() {
         this.shouldRender = true;
     }
 
