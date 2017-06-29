0032253f61607b0cd9789fdea3d9b0654c3e2be1
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-29 16:46:51 +0800

    update Popup

--- src/_PopupBody/PopupBody.js
+++ src/_PopupBody/PopupBody.js
@@ -251,7 +251,7 @@ PopupBody.Position = {
     BOTTOM_RIGHT: 'bottom-right',
 
     LEFT_TOP: 'left-top',
-    LEFT: 'left-top',
+    LEFT: 'left',
     LEFT_BOTTOM: 'left-bottom',
 
     RIGHT_TOP: 'right-top',
