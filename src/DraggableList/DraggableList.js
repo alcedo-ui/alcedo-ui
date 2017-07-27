b2a4bd5b1014e541da1745ce6a5faf03bcaeca4f
Liangxiaojun <fatalxiao@163.com>
2017-07-27 17:32:57 +0800

    update DraggableList

---
+++ src/DraggableList/DraggableList.js
@@ -0,0 +1,453 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import DraggableListGroup from '../DraggableListGroup';
+import DraggableListItem from '../DraggableListItem';
+import Tip from '../Tip';
+import Theme from '../Theme';
+
+import Util from '../_vendors/Util';
+
+import './DraggableList.css';
+
+export default class DraggableList extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            value: this.initValue(props)
+        };
+
+        this.initValue = this::this.initValue;
+        this.isItemChecked = this::this.isItemChecked;
+        this.listGroupedItemsRenderer = this::this.listGroupedItemsRenderer;
+        this.listItemsRenderer = this::this.listItemsRenderer;
+        this.listItemTouchTapHandle = this::this.listItemTouchTapHandle;
+        this.listItemSelectHandle = this::this.listItemSelectHandle;
+        this.listItemDeselectHandle = this::this.listItemDeselectHandle;
+
+    }
+
+    initValue(props) {
+
+        if (!props) {
+            return;
+        }
+
+        const {value, mode} = props;
+
+        if (!mode) {
+            return;
+        }
+
+        if (value) {
+            return value;
+        }
+
+        switch (mode) {
+            case DraggableList.Mode.CHECKBOX:
+                return [];
+            case DraggableList.Mode.RADIO:
+                return null;
+            default:
+                return value;
+        }
+
+    }
+
+    isItemChecked(item) {
+
+        const {mode, valueField, displayField} = this.props,
+            {value} = this.state;
+
+        if (!item || !value) {
+            return false;
+        }
+
+        if (mode === DraggableList.Mode.CHECKBOX) {
+            return _.isArray(value) && value.filter(valueItem => {
+                return Util.isValueEqual(valueItem, item, valueField, displayField);
+            }).length > 0;
+        } else if (mode === DraggableList.Mode.RADIO) {
+            return Util.isValueEqual(value, item, valueField, displayField);
+        }
+
+    }
+
+    listGroupedItemsRenderer(items = this.props.items) {
+        return _.isArray(items) ?
+            items.map((group, groupIndex) => {
+
+                if (group && group.name) {
+                    return (
+                        <DraggableListGroup key={`group${groupIndex}`}
+                                            text={group.name}>
+                            {
+                                group.children && group.children.length > 0 ?
+                                    this.listItemsRenderer(group.children)
+                                    :
+                                    null
+                            }
+                        </DraggableListGroup>
+                    );
+                }
+
+                return;
+
+            })
+            :
+            null;
+    }
+
+    listItemsRenderer(items = this.props.items) {
+
+        const {valueField, displayField, descriptionField, disabled, isLoading, mode} = this.props;
+
+        return _.isArray(items) && items.length > 0 ?
+            (
+                items.map((item, index) => {
+
+                    if (!item) {
+                        return null;
+                    }
+
+                    return typeof item === 'object' ?
+                        (
+                            <DraggableListItem key={index}
+                                               {...item}
+                                               checked={this.isItemChecked(item)}
+                                               value={Util.getValueByValueField(item, valueField, displayField)}
+                                               text={Util.getTextByDisplayField(item, displayField, valueField)}
+                                               desc={item[descriptionField] || null}
+                                               disabled={disabled || item.disabled}
+                                               isLoading={isLoading || item.isLoading}
+                                               mode={mode}
+                                               onTouchTap={() => {
+                                                   this.listItemTouchTapHandle(item, index);
+                                                   item.onTouchTap && item.onTouchTap();
+                                               }}
+                                               onSelect={() => {
+                                                   this.listItemSelectHandle(item, index);
+                                               }}
+                                               onDeselect={() => {
+                                                   this.listItemDeselectHandle(item, index);
+                                               }}/>
+                        )
+                        :
+                        (
+                            <DraggableListItem key={index}
+                                               checked={this.isItemChecked(item)}
+                                               value={item}
+                                               text={item}
+                                               disabled={disabled}
+                                               isLoading={isLoading}
+                                               mode={mode}
+                                               onTouchTap={() => {
+                                                   this.listItemTouchTapHandle(item, index);
+                                               }}
+                                               onSelect={() => {
+                                                   this.listItemSelectHandle(item, index);
+                                               }}
+                                               onDeselect={() => {
+                                                   this.listItemDeselectHandle(item, index);
+                                               }}/>
+                        );
+
+                })
+            )
+            :
+            null;
+
+    }
+
+    listItemTouchTapHandle(value, index) {
+
+        const {mode} = this.props;
+
+        if (mode !== DraggableList.Mode.NORMAL) {
+            return;
+        }
+
+        this.setState({
+            value
+        }, () => {
+            const {onItemTouchTap, onChange} = this.props;
+            onItemTouchTap && onItemTouchTap(value, index);
+            onChange && onChange(value, index);
+        });
+
+    }
+
+    listItemSelectHandle(item, index) {
+
+        const {mode} = this.props;
+
+        if (mode === DraggableList.Mode.NORMAL) {
+            return;
+        }
+
+        let {value} = this.state;
+
+        if (mode === DraggableList.Mode.CHECKBOX) {
+
+            if (!value || !_.isArray(value)) {
+                value = [];
+            }
+
+            value.push(item);
+
+        } else if (mode === DraggableList.Mode.RADIO) {
+            value = item;
+        }
+
+        this.setState({
+            value
+        }, () => {
+            const {onChange} = this.props;
+            onChange && onChange(value, index);
+        });
+
+    }
+
+    listItemDeselectHandle(item, index) {
+
+        const {mode} = this.props;
+
+        if (mode !== DraggableList.Mode.CHECKBOX) {
+            return;
+        }
+
+        const {valueField, displayField} = this.props;
+        let {value} = this.state;
+
+        if (!value || !_.isArray(value)) {
+            value = [];
+        } else {
+            value = value.filter(valueItem => {
+                return Util.getValueByValueField(valueItem, valueField, displayField)
+                    != Util.getValueByValueField(item, valueField, displayField);
+            });
+        }
+
+        this.setState({
+            value
+        }, () => {
+            const {onChange} = this.props;
+            onChange && onChange(value, index);
+        });
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.value !== this.state.value) {
+            this.setState({
+                value: this.initValue(nextProps)
+            });
+        }
+    }
+
+    render() {
+
+        const {children, className, style, disabled, isGrouped} = this.props,
+            listClassName = (isGrouped ? ' grouped' : '') + (className ? ' ' + className : '');
+
+        let renderEl;
+        if (isGrouped) {
+            renderEl = this.listGroupedItemsRenderer();
+        } else {
+            renderEl = this.listItemsRenderer();
+        }
+
+        return (
+            <div className={'draggable-list' + listClassName}
+                 disabled={disabled}
+                 style={style}>
+
+                {renderEl}
+
+                {children}
+
+            </div>
+        );
+    }
+};
+
+DraggableList.Mode = DraggableListItem.Mode;
+
+DraggableList.propTypes = {
+
+    /**
+     * The CSS class name of the root element.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Override the styles of the root element.
+     */
+    style: PropTypes.object,
+
+    /**
+     * Children passed into the DraggableListItem.
+     */
+    items: PropTypes.oneOfType([
+
+        // not grouped
+        PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.shape({
+
+            /**
+             * The CSS class name of the list button.
+             */
+            className: PropTypes.string,
+
+            /**
+             * Override the styles of the list button.
+             */
+            style: PropTypes.object,
+
+            /**
+             * The theme of the list button.
+             */
+            theme: PropTypes.oneOf(Object.keys(Theme).map(key => Theme[key])),
+
+            /**
+             * The text value of the list button.Type can be string or number.
+             */
+            value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+            /**
+             * The list item's display text. Type can be string, number or bool.
+             */
+            text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+            /**
+             * The desc value of the list button. Type can be string or number.
+             */
+            desc: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+            /**
+             * If true,the item will be in the selected state.
+             */
+            checked: PropTypes.bool,
+
+            /**
+             * If true,the list item will be disabled.
+             */
+            disabled: PropTypes.bool,
+
+            /**
+             * If true,the button will be have loading effect.
+             */
+            isLoading: PropTypes.bool,
+
+            /**
+             * If true,the element's ripple effect will be disabled.
+             */
+            disableTouchRipple: PropTypes.bool,
+
+            /**
+             * Use this property to display an icon. It will display on the left.
+             */
+            iconCls: PropTypes.string,
+
+            /**
+             * Use this property to display an icon. It will display on the right.
+             */
+            rightIconCls: PropTypes.string,
+
+            /**
+             * The message of tip.
+             */
+            tip: PropTypes.string,
+
+            /**
+             * The position of tip.
+             */
+            tipPosition: PropTypes.oneOf(Util.enumerateValue(Tip.Position)),
+
+            /**
+             * If true,the item will have center displayed ripple effect.
+             */
+            rippleDisplayCenter: PropTypes.bool,
+
+            /**
+             * You can create a complicated renderer callback instead of value and desc prop.
+             */
+            renderer: PropTypes.func,
+
+            /**
+             * Callback function fired when a list item touch-tapped.
+             */
+            onTouchTap: PropTypes.func
+
+        }), PropTypes.string, PropTypes.number])),
+
+        // grouped
+        PropTypes.array
+
+    ]).isRequired,
+
+    /**
+     * The value field name in data. (default: "value")
+     */
+    valueField: PropTypes.string,
+
+    /**
+     * The display field name in data. (default: "text")
+     */
+    displayField: PropTypes.string,
+
+    /**
+     * The description field name in data. (default: "desc")
+     */
+    descriptionField: PropTypes.string,
+
+    /**
+     * If true, the list will be disabled.
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * If true, the list will be at loading status.
+     */
+    isLoading: PropTypes.bool,
+
+    /**
+     * The mode of listItem.Can be normal,checkbox.
+     */
+    mode: PropTypes.oneOf(Util.enumerateValue(DraggableListItem.Mode)),
+
+    /**
+     * If true,the listData will be grouped.
+     */
+    isGrouped: PropTypes.bool,
+
+    /**
+     * Callback function fired when the list-item select.
+     */
+    onItemTouchTap: PropTypes.func,
+
+    /**
+     * Callback function fired when the list changed.
+     **/
+    onChange: PropTypes.func
+
+};
+
+DraggableList.defaultProps = {
+
+    className: '',
+    style: null,
+
+    items: [],
+
+    valueField: 'value',
+    displayField: 'text',
+    descriptionField: 'desc',
+    disabled: false,
+    mode: DraggableListItem.Mode.NORMAL,
+    isGrouped: false
+
+};
---
+++ src/DraggableListItem/DraggableListItem.js
@@ -0,0 +1,382 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import Checkbox from '../Checkbox';
+import CircularLoading from '../CircularLoading';
+import TipContainer from '../TipContainer';
+import TouchRipple from '../TouchRipple';
+import Theme from '../Theme';
+
+import Util from '../_vendors/Util';
+
+import './DraggableListItem.css';
+
+export default class DraggableListItem extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            checked: props.checked
+        };
+
+        this.checkboxChangeHandle = this::this.checkboxChangeHandle;
+        this.clickHandle = this::this.clickHandle;
+        this.startRipple = this::this.startRipple;
+        this.endRipple = this::this.endRipple;
+        this.mouseEnterHandle = this::this.mouseEnterHandle;
+        this.mouseLeaveHandle = this::this.mouseLeaveHandle;
+
+    }
+
+    checkboxChangeHandle(checked, callback) {
+        this.setState({
+            checked
+        }, () => {
+
+            const {onSelect, onDeselect} = this.props;
+
+            if (checked) {
+                onSelect && onSelect();
+            } else {
+                onDeselect && onDeselect();
+            }
+
+            callback && typeof callback === 'function' && callback();
+
+        });
+    }
+
+    radioChangeHandle(callback) {
+
+        const {checked} = this.state;
+
+        if (!checked) {
+            this.setState({
+                checked: true
+            }, () => {
+                const {onSelect} = this.props;
+                onSelect && onSelect();
+                callback && typeof callback === 'function' && callback();
+            });
+        } else {
+            callback && typeof callback === 'function' && callback();
+        }
+
+    }
+
+    clickHandle(e) {
+
+        const {disabled, isLoading, readOnly} = this.props;
+
+        if (disabled || isLoading || readOnly) {
+            return;
+        }
+
+        const {mode} = this.props,
+            callback = () => {
+                const {onTouchTap} = this.props;
+                onTouchTap && onTouchTap(e);
+            };
+
+        switch (mode) {
+            case DraggableListItem.Mode.CHECKBOX:
+                this.checkboxChangeHandle(!this.state.checked, callback);
+                return;
+            case DraggableListItem.Mode.RADIO:
+                this.radioChangeHandle(callback);
+                return;
+            case DraggableListItem.Mode.NORMAL:
+                callback();
+                return;
+        }
+
+    }
+
+    startRipple(e) {
+        this.refs.touchRipple.addRipple(e);
+    }
+
+    endRipple() {
+        this.refs.touchRipple.removeRipple();
+    }
+
+    mouseEnterHandle(e) {
+
+        const {onMouseEnter} = this.props;
+        onMouseEnter && onMouseEnter();
+    }
+
+    mouseLeaveHandle() {
+        const {onMouseLeave} = this.props;
+        onMouseLeave && onMouseLeave();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.checked !== this.state.checked) {
+            this.setState({
+                checked: nextProps.checked
+            });
+        }
+    }
+
+    render() {
+
+        const {
+                className, style, theme, text, desc, iconCls, rightIconCls, tip, tipPosition,
+                disabled, isLoading, disableTouchRipple, rippleDisplayCenter, mode, renderer, readOnly
+            } = this.props,
+            {checked} = this.state,
+            listItemClassName = (theme ? ` theme-${theme}` : '') + (checked ? ' activated' : '')
+                + (className ? ' ' + className : ''),
+            loadingIconPosition = (rightIconCls && !iconCls) ? 'right' : 'left';
+
+        return (
+            <TipContainer className='block'
+                          text={tip}
+                          tipPosition={tipPosition}>
+
+                <div className={'draggable-list-item' + listItemClassName}
+                     style={style}
+                     disabled={disabled || isLoading}
+                     readOnly={readOnly}
+                     onClick={this.clickHandle}
+                     onMouseEnter={this.mouseEnterHandle}
+                     onMouseLeave={this.mouseLeaveHandle}>
+
+                    {
+                        mode === DraggableListItem.Mode.CHECKBOX ?
+                            <Checkbox className="draggable-list-item-checkbox"
+                                      value={checked}/>
+                            :
+                            null
+                    }
+
+                    {
+                        mode === DraggableListItem.Mode.RADIO ?
+                            <i className={'fa fa-check draggable-list-item-checked' + (checked ? ' activated' : '')}
+                               aria-hidden="true"></i>
+                            :
+                            null
+                    }
+
+                    {
+                        isLoading && loadingIconPosition === 'left' ?
+                            <CircularLoading className="button-icon button-icon-left button-loading-icon"
+                                             size="small"/>
+                            :
+                            (
+                                iconCls ?
+                                    <i className={`button-icon button-icon-left ${iconCls}`}
+                                       aria-hidden="true"></i>
+                                    :
+                                    null
+                            )
+                    }
+
+                    {
+                        renderer && typeof renderer === 'function' ?
+                            renderer(this.props)
+                            :
+                            (
+                                desc ?
+                                    <div className="draggable-list-item-content">
+                                        <div className="draggable-list-item-content-value">
+                                            {text}
+                                        </div>
+                                        <div className="draggable-list-item-content-desc">
+                                            {desc}
+                                        </div>
+                                    </div>
+                                    :
+                                    text
+                            )
+                    }
+
+                    {
+                        isLoading && loadingIconPosition === 'right' ?
+                            <CircularLoading className="button-icon button-icon-right button-loading-icon"
+                                             size="small"/>
+                            :
+                            (
+                                rightIconCls ?
+                                    <i className={`button-icon button-icon-right ${rightIconCls}`}
+                                       aria-hidden="true"></i>
+                                    :
+                                    null
+                            )
+                    }
+
+                    {
+                        disableTouchRipple || readOnly ?
+                            null
+                            :
+                            <TouchRipple ref="touchRipple"
+                                         className={disabled || isLoading ? 'hidden' : ''}
+                                         displayCenter={rippleDisplayCenter}/>
+                    }
+
+                </div>
+            </TipContainer>
+        );
+
+    }
+};
+
+DraggableListItem.Mode = {
+    NORMAL: 'normal',
+    CHECKBOX: 'checkbox',
+    RADIO: 'radio'
+};
+
+DraggableListItem.propTypes = {
+
+    /**
+     * The CSS class name of the list button.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Override the styles of the list button.
+     */
+    style: PropTypes.object,
+
+    /**
+     * The theme of the list button.
+     */
+    theme: PropTypes.oneOf(Object.keys(Theme).map(key => Theme[key])),
+
+    /**
+     * The text value of the list button. Type can be string or number.
+     */
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+    /**
+     * The list item's display text. Type can be string, number or bool.
+     */
+    text: PropTypes.any,
+
+    /**
+     * The desc value of the list button. Type can be string or number.
+     */
+    desc: PropTypes.string,
+
+    /**
+     * If true, the list button will be disabled.
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * If true,the button will be have loading effect.
+     */
+    isLoading: PropTypes.bool,
+
+    /**
+     * If true,the element's ripple effect will be disabled.
+     */
+    disableTouchRipple: PropTypes.bool,
+
+    /**
+     * Use this property to display an icon. It will display on the left.
+     */
+    iconCls: PropTypes.string,
+
+    /**
+     * Use this property to display an icon. It will display on the right.
+     */
+    rightIconCls: PropTypes.string,
+
+    /**
+     *
+     */
+    tip: PropTypes.string,
+
+    /**
+     *
+     */
+    tipPosition: PropTypes.oneOf(Util.enumerateValue(TipContainer.Position)),
+
+    /**
+     *
+     */
+    rippleDisplayCenter: PropTypes.bool,
+
+    /**
+     * You can create a complicated renderer callback instead of value and desc prop.
+     */
+    renderer: PropTypes.func,
+
+    /**
+     *
+     */
+    checked: PropTypes.bool,
+
+    /**
+     *
+     */
+    mode: PropTypes.oneOf(Util.enumerateValue(DraggableListItem.Mode)),
+
+    /**
+     *
+     */
+    readOnly: PropTypes.bool,
+
+    /**
+     * Callback function fired when a list item touch-tapped.
+     */
+    onTouchTap: PropTypes.func,
+
+    /**
+     *
+     */
+    onSelect: PropTypes.func,
+
+    /**
+     *
+     */
+    onDeselect: PropTypes.func,
+
+    /**
+     *
+     */
+    onMouseEnter: PropTypes.func,
+
+    /**
+     *
+     */
+    onMouseLeave: PropTypes.func
+
+};
+
+DraggableListItem.defaultProps = {
+
+    className: '',
+    style: null,
+
+    theme: Theme.DEFAULT,
+
+    value: '',
+    text: '',
+    desc: '',
+
+    disabled: false,
+    isLoading: false,
+
+    disableTouchRipple: false,
+
+    iconCls: '',
+    rightIconCls: '',
+
+    tip: '',
+    tipPosition: TipContainer.Position.BOTTOM,
+
+    rippleDisplayCenter: false,
+
+    checked: false,
+
+    mode: DraggableListItem.Mode.NORMAL,
+
+    readOnly: false
+
+};
