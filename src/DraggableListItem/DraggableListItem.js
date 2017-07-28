eb889c87285de8d3f12ccd08ddeab277c223f959
Liangxiaojun <fatalxiao@163.com>
2017-07-28 16:14:55 +0800

    update Draggable List

--- src/DraggableListItem/DraggableListItem.js
+++ src/DraggableListItem/DraggableListItem.js
@@ -1,76 +1,20 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {findDOMNode} from 'react-dom';
 import {DragSource, DropTarget} from 'react-dnd';
 
 import CircularLoading from '../CircularLoading';
 import Theme from '../Theme';
 
+import DragDrop from '../_vendors/DragDrop';
+
 import './DraggableListItem.css';
 
 const DRAG_TYPE_SYMBOL = Symbol('DRAG_TYPE');
 
-const cardSource = {
-    beginDrag(props) {
-        return {
-            index: props.index
-        };
-    }
-};
-
-const cardTarget = {
-    hover(props, monitor, component) {
-
-        const dragIndex = monitor.getItem().index;
-        const hoverIndex = props.index;
-
-        // Don't replace items with themselves
-        if (dragIndex === hoverIndex) {
-            return;
-        }
-
-        // Determine rectangle on screen
-        const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
-
-        // Get vertical middle
-        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-        // Determine mouse position
-        const clientOffset = monitor.getClientOffset();
-
-        // Get pixels to the top
-        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-        // Only perform the move when the mouse has crossed half of the items height
-        // When dragging downwards, only move when the cursor is below 50%
-        // When dragging upwards, only move when the cursor is above 50%
-
-        // Dragging downwards
-        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-            return;
-        }
-
-        // Dragging upwards
-        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-            return;
-        }
-
-        // Time to actually perform the action
-        props.moveListItem(dragIndex, hoverIndex);
-
-        // Note: we're mutating the monitor item here!
-        // Generally it's better to avoid mutations,
-        // but it's good here for the sake of performance
-        // to avoid expensive index searches.
-        monitor.getItem().index = hoverIndex;
-
-    }
-};
-
-@DropTarget(DRAG_TYPE_SYMBOL, cardTarget, connect => ({
+@DropTarget(DRAG_TYPE_SYMBOL, DragDrop.getTarget(), connect => ({
     connectDropTarget: connect.dropTarget()
 }))
-@DragSource(DRAG_TYPE_SYMBOL, cardSource, (connect, monitor) => ({
+@DragSource(DRAG_TYPE_SYMBOL, DragDrop.getSource(), (connect, monitor) => ({
     connectDragSource: connect.dragSource(),
     isDragging: monitor.isDragging()
 }))
