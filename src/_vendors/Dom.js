15f01b95e48f64b8003280e6fd23369296e31dab
Liangxiaojun <fatalxiao@163.com>
2017-08-08 17:17:12 +0800

    bug fixed

--- src/_vendors/Dom.js
+++ src/_vendors/Dom.js
@@ -4,6 +4,10 @@ function getOffset(el) {
         return null;
     }
 
+    if (el.getBoundingClientRect) {
+        return el.getBoundingClientRect();
+    }
+
     let offset = {
         top: el.offsetTop,
         left: el.offsetLeft
@@ -50,6 +54,62 @@ function hasClass(el, className) {
 
 }
 
+function addClass(el, className) {
+
+    if (!el || !className) {
+        return;
+    }
+
+    const elClassName = el.className;
+
+    if (!elClassName) {
+        el.className = className;
+        return;
+    }
+
+    const elClass = elClassName.split(/\s+/),
+        index = elClass.findIndex(item => item === className);
+
+    if (index !== -1) { // already has this className
+        return;
+    }
+
+    elClass.push(className);
+    el.className = elClass.join(' ');
+
+}
+
+function removeClass(el, className) {
+
+    if (!el || !className) {
+        return;
+    }
+
+    const elClassName = el.className;
+
+    if (!elClassName) {
+        return;
+    }
+
+    const elClass = elClassName.split(/\s+/),
+        index = elClass.findIndex(item => item === className);
+
+    if (index === -1) { // no this className
+        return;
+    }
+
+    elClass.splice(index, 1);
+    el.className = elClass.join(' ');
+
+}
+
+function toggleClass(el, className, bool) {
+    bool ?
+        addClass(el, className)
+        :
+        removeClass(el, className);
+}
+
 function findParent(el, className) {
 
     if (!el || !className) {
@@ -72,5 +132,8 @@ export default {
     getScrollHeight,
     getScrollTop,
     hasClass,
+    addClass,
+    removeClass,
+    toggleClass,
     findParent
 };
