942d19451653b5d457c21acbc4da6adfcf83353a
Liangxiaojun <fatalxiao@163.com>
2017-08-11 11:27:15 +0800

    update react-router to v4

--- examples/index.js
+++ examples/index.js
@@ -7,28 +7,12 @@ import {Route} from 'react-router';
 import createHashHistory from 'history/createHashHistory';
 import {ConnectedRouter} from 'react-router-redux';
 
-// import configureRoute from './config.routes';
 import configureStore from 'reduxes/store';
 
 import Root from 'containers/Root';
 
 const history = createHashHistory();
 const store = configureStore(history);
-
-/**
- * IE polyfill
- */
-if (!(Object.setPrototypeOf || {}.__proto__)) {
-    var nativeGetPrototypeOf = Object.getPrototypeOf;
-
-    Object.getPrototypeOf = function (object) {
-        if (object.__proto__) {
-            return object.__proto__;
-        } else {
-            return nativeGetPrototypeOf.call(Object, object);
-        }
-    };
-}
 
 render(
     <Provider store={store}>
