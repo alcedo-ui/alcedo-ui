2db5a10a27edf9a4c0adfed917f9893397426859
Liangxiaojun <fatalxiao@163.com>
2017-08-14 17:29:43 +0800

    update onTouchTap to Grid

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -159,9 +159,9 @@ export default class Grid extends Component {
                                       isLoading={isLoading || item.isLoading}
                                       mode={mode}
                                       renderer={renderer}
-                                      onTouchTap={() => {
+                                      onTouchTap={(e) => {
                                           this.listItemTouchTapHandle(item, index);
-                                          item.onTouchTap && item.onTouchTap();
+                                          item.onTouchTap && item.onTouchTap(e);
                                       }}
                                       onSelect={() => {
                                           this.listItemSelectHandle(item, index);
