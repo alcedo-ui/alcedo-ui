e7fd942968174815e1db0da7777a1980a6bcea00
Liangxiaojun <fatalxiao@163.com>
2017-08-28 14:48:32 +0800

    optimize Menu

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -9,45 +9,16 @@ export default class Menu extends Component {
     static TriggerMode = MenuBody.TriggerMode;
 
     constructor(props) {
-
         super(props);
-
-        this.state = {
-            visible: !!props.visible
-        };
-
-        this.requestCloseHandle = this::this.requestCloseHandle;
-
-    }
-
-    requestCloseHandle() {
-
-        const {onRequestClose} = this.props;
-
-        this.setState({
-            visible: false
-        }, () => {
-            onRequestClose && onRequestClose();
-        });
-
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.visible !== this.state.visible) {
-            this.setState({
-                visible: !!nextProps.visible
-            });
-        }
     }
 
     render() {
 
-        const {visible} = this.state;
+        const {visible} = this.props;
 
         return (
             <SubtreeContainer visible={visible}>
-                <MenuBody {...this.props}
-                          onRequestClose={this.requestCloseHandle}/>
+                <MenuBody {...this.props}/>
             </SubtreeContainer>
         );
 
