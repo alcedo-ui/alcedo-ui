db6b632c11466d03db19d4fab5843e107f935f7a
Liangxiaojun <fatalxiao@163.com>
2017-08-28 16:17:31 +0800

    draggable grid item loading style bug fixed

--- src/_DraggableGridItem/DraggableGridItem.js
+++ src/_DraggableGridItem/DraggableGridItem.js
@@ -167,8 +167,10 @@ export default class DraggableGridItem extends Component {
 
                         {
                             isLoading && loadingIconPosition === 'left' ?
-                                <CircularLoading className="button-icon button-icon-left button-loading-icon"
-                                                 size="small"/>
+                                <div className="button-icon button-icon-left">
+                                    <CircularLoading className="button-loading-icon"
+                                                     size="small"/>
+                                </div>
                                 :
                                 (
                                     iconCls ?
