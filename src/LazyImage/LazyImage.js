30ad3539dfbad15aa2f37fc9c7033ced414b6b27
Liangxiaojun <fatalxiao@163.com>
2017-09-01 10:06:21 +0800

    add LazyImage

--- src/LazyImage/LazyImage.js
+++ src/LazyImage/LazyImage.js
@@ -1,23 +1,57 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import CircularLoading from '../CircularLoading';
+
+import Event from '../_vendors/Event';
+
 import './LazyImage.css';
 
 export default class LazyImage extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.state = {
+            isLoading: true
+        };
+
+    }
+
+    componentDidMount() {
+
     }
 
     render() {
 
-        const {className, style} = this.props,
+        const {className, style, alt, placeholder} = this.props,
+            {isLoading} = this.state,
 
             lazyImageClassName = (className ? ' ' + className : '');
 
         return (
-            <img className={'lazy-image' + lazyImageClassName}
-                 style={style}/>
+            <div className="lazy-image-wrapper">
+
+                <img className={'lazy-image' + lazyImageClassName}
+                     style={style}
+                     alt={alt}/>
+
+                {
+                    isLoading ?
+                        (
+                            placeholder ?
+                                <div className="lazy-image-placeholder">
+                                    {placeholder}
+                                </div>
+                                :
+                                <CircularLoading className="lazy-image-loading"/>
+                        )
+                        :
+                        null
+                }
+
+            </div>
         );
 
     }
@@ -33,13 +67,31 @@ LazyImage.propTypes = {
     /**
      * Override the styles of the root element.
      */
-    style: PropTypes.object
+    style: PropTypes.object,
+
+    /**
+     * Image src url.
+     */
+    src: PropTypes.string.isRequired,
+
+    /**
+     * Image alt text.
+     */
+    alt: PropTypes.string,
+
+    /**
+     * Image placeholder.
+     */
+    placeholder: PropTypes.any
 
 };
 
 LazyImage.defaultProps = {
 
     className: '',
-    style: null
+    style: null,
+
+    alt: '',
+    placeholder: ''
 
 };
