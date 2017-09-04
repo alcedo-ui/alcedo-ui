f0afeb5512cda96913399cb56f070cb5d915a4ef
Liangxiaojun <fatalxiao@163.com>
2017-09-04 23:08:21 +0800

    update

--- examples/index.js
+++ examples/index.js
@@ -4,38 +4,21 @@ import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {Provider} from 'react-redux';
-import {Route} from 'react-router';
+import {renderRoutes} from 'react-router-config';
 import createHashHistory from 'history/createHashHistory';
 import {ConnectedRouter} from 'react-router-redux';
 
 import configureStore from 'reduxes/store';
-import Root from 'containers/Root';
+import routes from './config.routes';
 
 const history = createHashHistory();
 const store = configureStore(history);
 injectTapEventPlugin();
 
-/**
- * IE polyfill
- */
-if (!(Object.setPrototypeOf || {}.__proto__)) {
-    var nativeGetPrototypeOf = Object.getPrototypeOf;
-
-    Object.getPrototypeOf = function (object) {
-        if (object.__proto__) {
-            return object.__proto__;
-        } else {
-            return nativeGetPrototypeOf.call(Object, object);
-        }
-    };
-}
-
 render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <div>
-                <Route path="/" component={Root}/>
-            </div>
+            {renderRoutes(routes)}
         </ConnectedRouter>
     </Provider>,
     document.getElementById('app-container')
