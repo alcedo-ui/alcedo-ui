858b53d97b14d9cc5eb4a6a2f05dd10ba9468419
Liangxiaojun <fatalxiao@163.com>
2017-09-05 10:08:08 +0800

    update

--- examples/containers/AppRoot.js
+++ examples/containers/AppRoot.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {renderRoutes} from 'react-router-config';
+import {Redirect, withRouter} from 'react-router-dom';
 
 import * as actions from 'reduxes/actions';
 import Event from 'vendors/Event';
@@ -44,7 +45,16 @@ class AppRoot extends Component {
     render() {
         return (
             <div className="app-root">
+
                 {renderRoutes(this.props.route.routes)}
+
+                {
+                    location.hash.slice(1) === '/' ?
+                        <Redirect from="/" to="/landing"/>
+                        :
+                        null
+                }
+
             </div>
         );
     }
@@ -65,4 +75,4 @@ function mapDispatchToProps(dispatch) {
     return bindActionCreators(actions, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AppRoot);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AppRoot));
