8cd366104e158976702111806a5af54659579fec
Liangxiaojun <fatalxiao@163.com>
2017-09-05 09:17:20 +0800

    update

--- examples/index.js
+++ examples/index.js
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import {Provider} from 'react-redux';
-import {renderRoutes} from 'react-router-config';
 import createHashHistory from 'history/createHashHistory';
+import {renderRoutes} from 'react-router-config';
+import {Provider} from 'react-redux';
 import {ConnectedRouter} from 'react-router-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import configureStore from 'reduxes/store';
 import routes from './config.routes';
