78a977d07c3889a9a09070caad5158cc5ea5b34f
Liangxiaojun <fatalxiao@163.com>
2017-09-12 13:56:46 +0800

    optimize SpanLoading

--- src/SpanLoading/SpanLoading.js
+++ src/SpanLoading/SpanLoading.js
@@ -8,16 +8,18 @@ import './SpanLoading.css';
 
 export default class SpanLoading extends Component {
 
-    constructor(props) {
-        super(props);
-        const spanning = props.spanning;
+    constructor(props, ...restArgs) {
+
+        super(props, ...restArgs);
+
         this.state = {
-            spanning
+            spanning: props.spanning
         };
 
     }
 
     componentWillReceiveProps(nextProps) {
+
         const spanning = nextProps.spanning;
         const {delay} = this.props;
 
@@ -27,18 +29,16 @@ export default class SpanLoading extends Component {
             this.setState({spanning});
         }
 
-
     }
 
     componentWillUnmount() {
-        if (this.delayTimeout) {
-            clearTimeout(this.delayTimeout);
-        }
+        this.delayTimeout && clearTimeout(this.delayTimeout);
     }
 
     render() {
-        const {className, style, description, size, children}=this.props;
-        const {spanning} = this.state;
+
+        const {className, style, description, size, children} = this.props,
+            {spanning} = this.state;
 
         return (
             <div className={`span-loading span-loading-${size} ${className}`}
@@ -48,10 +48,10 @@ export default class SpanLoading extends Component {
                     spanning
                         ?
                         <span className={`span-loading-dot ${size ? size : ''}`}>
-                            <i />
-                            <i />
-                            <i />
-                            <i />
+                            <i/>
+                            <i/>
+                            <i/>
+                            <i/>
                         </span>
                         :
                         null
@@ -70,7 +70,7 @@ export default class SpanLoading extends Component {
 
 
             </div>
-        )
+        );
     }
 }
 
@@ -104,13 +104,14 @@ SpanLoading.propTypes = {
     /**
      * Set the delay time of loading animation.
      */
-    delay: PropTypes.number,
+    delay: PropTypes.number
 };
 
 SpanLoading.defaultProps = {
 
     className: '',
     style: null,
+
     spanning: true,
     tip: '',
     type: ''
