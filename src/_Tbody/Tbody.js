e0923557a785f57d229215f1643362ab8a96006f
Liangxiaojun <fatalxiao@163.com>
2017-09-13 11:29:18 +0800

    optimize Tbody and add comment

--- src/_Tbody/Tbody.js
+++ src/_Tbody/Tbody.js
@@ -1,11 +1,13 @@
+/**
+ * @file Tbody component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import TableRow from '../_TableRow';
 
 import Util from '../_vendors/Util';
-
-import './Tbody.css';
 
 export default class Tbody extends Component {
 
