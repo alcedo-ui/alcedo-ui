f30348510484d2ec867fd4f25bf28b87711d037c
Liangxiaojun <fatalxiao@163.com>
2017-09-13 15:20:01 +0800

    update PageLoading comment

--- src/PageLoading/PageLoading.js
+++ src/PageLoading/PageLoading.js
@@ -1,5 +1,6 @@
 /**
- * Created by DT314 on 2017/7/13.
+ * @file PageLoading component
+ * @author chao(chao.zhang@derbysoft.com)
  */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
