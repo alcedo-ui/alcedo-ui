c49b8002ec6b46d5620fad9463099037c94131e6
Liangxiaojun <fatalxiao@163.com>
2017-09-13 14:27:58 +0800

    update MultipleSelect comment

--- src/MultipleSelect/MultipleSelect.js
+++ src/MultipleSelect/MultipleSelect.js
@@ -1,3 +1,7 @@
+/**
+ * @file MultipleSelect component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {findDOMNode} from 'react-dom';
--- src/MultipleSelect/index.js
+++ src/MultipleSelect/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file MultipleSelect
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 export default from './MultipleSelect';
