315a3a89d4346b131c65220a8c13aab23775954c
Liangxiaojun <fatalxiao@163.com>
2017-09-13 14:38:52 +0800

    update TouchRipple comment

--- src/TouchRipple/TouchRipple.js
+++ src/TouchRipple/TouchRipple.js
@@ -1,3 +1,7 @@
+/**
+ * @file TouchRipple component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
--- src/TouchRipple/index.js
+++ src/TouchRipple/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file TouchRipple
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 export default from './TouchRipple';
