678a1be074b1270d38a3127cc0cc7687fb1e3c2e
Liangxiaojun <fatalxiao@163.com>
2017-09-13 15:04:49 +0800

    update SpanLoading comment

--- src/SpanLoading/SpanLoading.js
+++ src/SpanLoading/SpanLoading.js
@@ -1,5 +1,6 @@
 /**
- * Created by Wendy on 2017/5/9.
+ * @file SpanLoading component
+ * @author wendy(wendy.wei@derbysoft.com)
  */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
