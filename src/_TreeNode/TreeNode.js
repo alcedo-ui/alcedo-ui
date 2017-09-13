108f0c20e758ae1ebfb30aac4e7cd44cbe036690
Liangxiaojun <fatalxiao@163.com>
2017-09-13 15:08:31 +0800

    update TreeNode comment

--- src/_TreeNode/TreeNode.js
+++ src/_TreeNode/TreeNode.js
@@ -1,3 +1,7 @@
+/**
+ * @file TreeNode component
+ * @author sunday(sunday.wei@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
--- src/_TreeNode/index.js
+++ src/_TreeNode/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file TreeNode
+ * @author sunday(sunday.wei@derbysoft.com)
+ */
 export default from './TreeNode';
