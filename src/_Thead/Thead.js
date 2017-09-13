a744b7447ed87d3e9b7d22b95739af663971893f
Liangxiaojun <fatalxiao@163.com>
2017-09-13 11:29:51 +0800

    update Thead comment

--- src/_Thead/Thead.js
+++ src/_Thead/Thead.js
@@ -1,3 +1,7 @@
+/**
+ * @file Thead component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
--- src/_Thead/index.js
+++ src/_Thead/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file Thead
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 export default from './Thead';
