be316407324962a1945423de306c55dff17cec49
Liangxiaojun <fatalxiao@163.com>
2017-09-13 11:22:41 +0800

    update ListGroup comment

--- src/_ListGroup/ListGroup.js
+++ src/_ListGroup/ListGroup.js
@@ -1,3 +1,7 @@
+/**
+ * @file ListGroup component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
--- src/_ListGroup/index.js
+++ src/_ListGroup/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file ListGroup
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 export default from './ListGroup';
