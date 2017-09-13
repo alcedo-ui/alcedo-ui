3398bc6439c98f47eabd645381a4d10f056b82da
Liangxiaojun <fatalxiao@163.com>
2017-09-13 14:26:14 +0800

    update LazyImage comment

--- src/LazyImage/LazyImage.js
+++ src/LazyImage/LazyImage.js
@@ -1,3 +1,7 @@
+/**
+ * @file LazyImage component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
