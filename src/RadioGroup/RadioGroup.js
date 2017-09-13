ce577e78aabe0eadac19a93849b6f9c7610511f6
Liangxiaojun <fatalxiao@163.com>
2017-09-13 14:30:00 +0800

    update RadioGroup comment

--- src/RadioGroup/RadioGroup.js
+++ src/RadioGroup/RadioGroup.js
@@ -1,3 +1,7 @@
+/**
+ * @file RadioGroup component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
--- src/RadioGroup/index.js
+++ src/RadioGroup/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file RadioGroup
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 export default from './RadioGroup';
