a71881d9470498a7ef3acd2db4bedb8cdf1a1515
Liangxiaojun <fatalxiao@163.com>
2017-09-13 14:20:54 +0800

    update DraggableGrid comment

--- src/DraggableGrid/DraggableGrid.js
+++ src/DraggableGrid/DraggableGrid.js
@@ -1,3 +1,7 @@
+/**
+ * @file DraggableGrid component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {findDOMNode} from 'react-dom';
--- src/DraggableGrid/index.js
+++ src/DraggableGrid/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file DraggableGrid
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 export default from './DraggableGrid';
