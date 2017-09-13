a92eb980462449fed336246e567fcc1f7780658c
Liangxiaojun <fatalxiao@163.com>
2017-09-13 14:24:54 +0800

    update Grid comment

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,3 +1,7 @@
+/**
+ * @file Grid component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,1 +1,5 @@
+/**
+ * @file Grid
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 export default from './Grid';
