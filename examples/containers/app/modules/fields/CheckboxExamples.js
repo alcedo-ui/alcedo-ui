930ca99025bfa1929671f9649cb4512112b4094c
Liangxiaojun <fatalxiao@163.com>
2017-09-28 10:08:25 +0800

    update Checkbox examples

--- examples/containers/app/modules/fields/CheckboxExamples.js
+++ examples/containers/app/modules/fields/CheckboxExamples.js
@@ -13,10 +13,56 @@ import 'sass/containers/app/modules/fields/CheckboxExamples.scss';
 export default class CheckboxExamples extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.state = {
+            checkboxGroupValue: []
+        };
+
+        this.data = [{
+            id: 1,
+            label: 1,
+            value: 1
+        }, {
+            id: 2,
+            label: 2,
+            value: 2
+        }, {
+            id: 3,
+            label: 3,
+            value: 3
+        }, {
+            id: 4,
+            label: 4,
+            value: 4
+        }, {
+            id: 5,
+            label: 5,
+            value: 5
+        }];
+
+        this.checkboxChangeHandler = ::this.checkboxChangeHandler;
+        this.checkboxGroupChangeHandler = ::this.checkboxGroupChangeHandler;
+
+    }
+
+    checkboxChangeHandler(value) {
+        this.setState({
+            checkboxGroupValue: value ? this.data : []
+        });
+    }
+
+    checkboxGroupChangeHandler(checkboxGroupValue) {
+        this.setState({
+            checkboxGroupValue
+        });
     }
 
     render() {
+
+        const {checkboxGroupValue} = this.state;
+
         return (
             <div className="example checkbox-examples">
 
@@ -82,59 +128,14 @@ export default class CheckboxExamples extends Component {
 
                                 <p>CheckboxGroup simple example.</p>
 
-                                <CheckboxGroup data={[{
-                                    label: 1,
-                                    value: 1
-                                }, {
-                                    label: 2,
-                                    value: 2
-                                }, {
-                                    label: 3,
-                                    value: 3
-                                }, {
-                                    label: 4,
-                                    value: 4
-                                }, {
-                                    label: 5,
-                                    value: 5
-                                }]}/>
+                                <Checkbox label="All"
+                                          value={checkboxGroupValue.length > 0}
+                                          indeterminate={checkboxGroupValue.length != this.data.length}
+                                          onChange={this.checkboxChangeHandler}/>
 
-                            </div>
-
-                        </div>
-                    </div>
-
-                </Widget>
-
-                <Widget>
-
-                    <WidgetHeader className="example-header"
-                                  title="Checkbox Group Disabled Example"/>
-
-                    <div className="widget-content">
-                        <div className="example-content">
-
-                            <div className="examples-wrapper">
-
-                                <p>Set the <code>disabled</code> property to true to disable the CheckboxGroup.</p>
-
-                                <CheckboxGroup data={[{
-                                    label: 1,
-                                    value: 1
-                                }, {
-                                    label: 2,
-                                    value: 2
-                                }, {
-                                    label: 3,
-                                    value: 3
-                                }, {
-                                    label: 4,
-                                    value: 4
-                                }, {
-                                    label: 5,
-                                    value: 5
-                                }]}
-                                               disabled={true}/>
+                                <CheckboxGroup data={this.data}
+                                               value={checkboxGroupValue}
+                                               onChange={this.checkboxGroupChangeHandler}/>
 
                             </div>
 
