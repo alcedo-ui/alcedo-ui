1931ed7c6f9158e0f1f8d879988e044296fce056
Liangxiaojun <fatalxiao@163.com>
2017-10-24 08:42:23 +0800

    update MaterialLocalAutoComplete examples

--- examples/containers/app/modules/materialDesign/MaterialLocalAutoCompleteExamples.js
+++ examples/containers/app/modules/materialDesign/MaterialLocalAutoCompleteExamples.js
@@ -76,7 +76,8 @@ export default class MaterialLocalAutoCompleteExamples extends Component {
 
                                 <MaterialLocalAutoComplete theme={Theme.HIGHLIGHT}
                                                            data={this.data}
-                                                           label="Label"
+                                                           // label="Label"
+                                                           // isLabelAnimated={false}
                                                            placeholder="Please select ..."
                                                            onChange={this.onChange}
                                                            onFilterPressEnter={this.filterPressEnterHandle}
