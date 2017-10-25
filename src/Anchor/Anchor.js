2a0d1422be9e6cc6be1ecb94189e30c6da4c7f8c
Liangxiaojun <fatalxiao@163.com>
2017-10-25 17:26:44 +0800

    add Anchor

---
+++ src/Anchor/Anchor.js
@@ -0,0 +1,196 @@
+/**
+ * @file Anchor component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import CircularLoading from '../CircularLoading';
+import TipProvider from '../TipProvider';
+import TouchRipple from '../TouchRipple';
+import Theme from '../Theme';
+
+import Util from '../_vendors/Util';
+
+export default class Anchor extends Component {
+
+    constructor(props, ...restArgs) {
+
+        super(props, ...restArgs);
+
+        this.state = {
+            focused: false
+        };
+
+        this.touchTapHandler = ::this.touchTapHandler;
+        this.focusHandler = ::this.focusHandler;
+        this.blurHandler = ::this.blurHandler;
+        this.startRipple = ::this.startRipple;
+        this.endRipple = ::this.endRipple;
+
+    }
+
+    touchTapHandler(e) {
+        e.preventDefault();
+        const {disabled, isLoading, onTouchTap} = this.props;
+        !disabled && !isLoading && onTouchTap && onTouchTap(e);
+    }
+
+    focusHandler(e) {
+        this.setState({
+            focused: true
+        }, () => {
+            const {onFocus} = this.props;
+            onFocus && onFocus(e);
+        });
+    }
+
+    blurHandler(e) {
+        this.setState({
+            focused: false
+        }, () => {
+            const {onBlur} = this.props;
+            onBlur && onBlur(e);
+        });
+    }
+
+    startRipple(e) {
+        this.refs.touchRipple.addRipple(e);
+    }
+
+    endRipple() {
+        this.refs.touchRipple.removeRipple();
+    }
+
+    render() {
+
+        const {
+
+                children, className, style, theme, isRounded, isCircular, disableTouchRipple,
+                iconCls, rightIconCls, disabled, readOnly, isLoading, rippleDisplayCenter,
+                tip, tipPosition, onMouseEnter, onMouseLeave,
+
+                ...restProps
+
+            } = this.props,
+            {focused} = this.state,
+
+            buttonClassName = (focused ? ' focused' : '') + (theme ? ` theme-${theme}` : '')
+                + (isCircular ? ' button-circular' : (isRounded ? ' button-rounded' : ''))
+                + (className ? ' ' + className : ''),
+
+            loadingIconPosition = (rightIconCls && !iconCls) ? 'right' : 'left';
+
+        return (
+            <TipProvider text={tip}
+                         position={tipPosition}>
+
+                <a {...restProps}
+                   className={'base-button' + buttonClassName}
+                   style={style}
+                   disabled={disabled || isLoading}
+                   readOnly={readOnly}
+                   onTouchTap={this.touchTapHandler}
+                   onFocus={this.focusHandler}
+                   onBlur={this.blurHandler}
+                   onMouseEnter={onMouseEnter}
+                   onMouseLeave={onMouseLeave}>
+
+                    {
+                        isLoading && loadingIconPosition === 'left' ?
+                            <CircularLoading className="button-icon button-icon-left button-loading-icon"
+                                             size="small"/>
+                            :
+                            (
+                                iconCls ?
+                                    <i className={`button-icon button-icon-left ${iconCls}`}
+                                       aria-hidden="true"></i>
+                                    :
+                                    null
+                            )
+                    }
+
+                    {
+                        isLoading && loadingIconPosition === 'right' ?
+                            <CircularLoading className="button-icon button-icon-right button-loading-icon"
+                                             size="small"/>
+                            :
+                            (
+                                rightIconCls ?
+                                    <i className={`button-icon button-icon-right ${rightIconCls}`}
+                                       aria-hidden="true"></i>
+                                    :
+                                    null
+                            )
+                    }
+
+                    {children}
+
+                    {
+                        disableTouchRipple ?
+                            null
+                            :
+                            <TouchRipple ref="touchRipple"
+                                         className={disabled || isLoading ? 'hidden' : ''}
+                                         displayCenter={rippleDisplayCenter}/>
+                    }
+
+                </a>
+
+            </TipProvider>
+        );
+
+    }
+};
+
+Anchor.propTypes = {
+
+    className: PropTypes.string,
+    style: PropTypes.object,
+
+    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+    isRounded: PropTypes.bool,
+    isCircular: PropTypes.bool,
+
+    disabled: PropTypes.bool,
+    readOnly: PropTypes.bool,
+    isLoading: PropTypes.bool,
+    disableTouchRipple: PropTypes.bool,
+
+    iconCls: PropTypes.string,
+    rightIconCls: PropTypes.string,
+
+    tip: PropTypes.string,
+    tipPosition: PropTypes.string,
+
+    rippleDisplayCenter: PropTypes.bool,
+
+    onTouchTap: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    onMouseEnter: PropTypes.func,
+    onMouseLeave: PropTypes.func
+
+};
+
+Anchor.defaultProps = {
+
+    className: '',
+    style: null,
+
+    theme: Theme.DEFAULT,
+    isRounded: false,
+    isCircular: false,
+
+    disabled: false,
+    readOnly: false,
+    isLoading: false,
+    disableTouchRipple: false,
+
+    rippleDisplayCenter: false,
+
+    iconCls: '',
+    rightIconCls: ''
+
+};
---
+++ src/Anchor/index.js
@@ -0,0 +1,6 @@
+/**
+ * @file IconAnchor
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+export default from './IconAnchor';
