396c0f65d45e54971cac2cfb8f45d0a5a9ec7a8b
Liangxiaojun <fatalxiao@163.com>
2017-10-31 15:08:11 +0800

    add GroupList examples

---
+++ examples/containers/app/modules/layout/GroupListExamples.js
@@ -0,0 +1,283 @@
+import React, {Component} from 'react';
+
+import List from 'src/List';
+import Widget from 'src/Widget';
+import WidgetHeader from 'src/WidgetHeader';
+import Paper from 'src/Paper';
+
+import PropTypeDescTable from '../PropTypeDescTable';
+import ListDoc from 'assets/propTypes/List.json';
+
+import 'sass/containers/app/modules/layout/ListExamples.scss';
+
+export default class ListExamples extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.listData = [{
+            iconCls: 'fa fa-facebook',
+            text: 'Facebook'
+        }, {
+            iconCls: 'fa fa-twitter',
+            text: 'Twitter'
+        }, {
+            iconCls: 'fa fa-google-plus',
+            text: 'Google+'
+        }];
+
+        this.disabledListData = [{
+            iconCls: 'fa fa-firefox',
+            text: 'Firefox',
+            disabled: true
+        }, {
+            iconCls: 'fa fa-chrome',
+            text: 'Chrome'
+        }, {
+            iconCls: 'fa fa-safari',
+            text: 'Safari',
+            disabled: true
+        }];
+
+        this.themeListData = [{
+            text: 'Success',
+            theme: 'success',
+            rightIconCls: 'fa fa-check-circle'
+        }, {
+            text: 'Error',
+            theme: 'error',
+            rightIconCls: 'fa fa-times-circle-o'
+        }, {
+            text: 'Warning',
+            theme: 'warning',
+            rightIconCls: 'fa fa-exclamation-triangle'
+        }, {
+            text: 'Primary',
+            theme: 'primary',
+            rightIconCls: 'fa fa-gratipay'
+        }];
+
+        this.descListData = [{
+            iconCls: 'fa fa-facebook',
+            text: 'Facebook',
+            desc: 'Here is a Facebook Desc.'
+        }, {
+            iconCls: 'fa fa-twitter',
+            text: 'Twitter',
+            desc: 'Here is a Twitter Desc.'
+        }, {
+            iconCls: 'fa fa-google-plus',
+            text: 'Google+',
+            desc: 'Here is a Google+ Desc.'
+        }];
+
+        this.groupedListData = [{
+            name: 'socialNetwork',
+            children: [{
+                iconCls: 'fa fa-facebook',
+                text: 'Facebook',
+                desc: 'Here is a Facebook Desc.',
+                checked: 'true'
+            }, {
+                iconCls: 'fa fa-twitter',
+                text: 'Twitter',
+                desc: 'Here is a Twitter Desc.'
+            }, {
+                iconCls: 'fa fa-google-plus',
+                text: 'Google+',
+                desc: 'Here is a Google+ Desc.'
+            }]
+        }, List.SEPARATOR, {
+            name: 'device',
+            children: [{
+                iconCls: 'fa fa-android',
+                text: 'Android',
+                desc: 'Here is a Android Desc.'
+            }, {
+                iconCls: 'fa fa-apple',
+                text: 'Apple',
+                desc: 'Here is a Apple Desc.'
+            }, {
+                iconCls: 'fa fa-windows',
+                text: 'Windows',
+                desc: 'Here is a Windows Desc.'
+            }]
+        }];
+
+        this.changeHandle = this::this.changeHandle;
+
+    }
+
+    changeHandle(value) {
+        console.log(value);
+    }
+
+    render() {
+        return (
+            <div className="example list-examples">
+
+                <h2 className="example-title">List</h2>
+
+                <p>
+                    <span>List</span> are used to present multiple items vertically as a single continuous
+                    element. They can be configured for many uses such as a contacts list, nested lists, etc.
+                </p>
+
+                <h2 className="example-title">Examples</h2>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="Basic"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <p>A <code>List</code> simple example with left icons.</p>
+
+                                <Paper>
+                                    <List items={this.listData}/>
+                                </Paper>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="With disabled"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <p>Set the <code>disabled</code> property to true for disabled items.</p>
+
+                                <Paper>
+                                    <List items={this.disabledListData}/>
+                                </Paper>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="With theme and rightIconCls"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <p>Set the <code>theme</code> property to "success","error","warning","primary" and set
+                                    the <code>rightIconCls</code> property to get rightIcons.</p>
+
+                                <Paper>
+                                    <List items={this.themeListData}/>
+                                </Paper>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="With desc"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <p>
+                                    A <code>List</code> with desc.
+                                </p>
+
+                                <Paper style={{width: 240}}>
+                                    <List style={{width: 240}}
+                                          items={this.descListData}/>
+                                </Paper>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="With mode"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <p>
+                                    A <code>List</code> with mode.Can be normal,radio,checkbox.
+                                </p>
+
+                                <Paper style={{width: 240}}>
+                                    <List style={{width: 240}}
+                                          mode={List.Mode.RADIO}
+                                          items={this.descListData}
+                                          onChange={this.changeHandle}/>
+                                </Paper>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="With isGrouped"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <p>
+                                    A <code>List</code> with isGrouped.
+                                </p>
+
+                                <Paper style={{width: 240, height: 240}}>
+                                    <List style={{width: 240, height: 240}}
+                                          mode={List.Mode.CHECKBOX}
+                                          isGrouped={true}
+                                          items={this.groupedListData}
+                                          onChange={this.changeHandle}/>
+                                </Paper>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <h2 className="example-title">Properties</h2>
+
+                <PropTypeDescTable data={ListDoc}/>
+
+            </div>
+        );
+    }
+};
