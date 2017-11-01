63abbd21bc097517ffe162c7d54a1df532a28d27
Liangxiaojun <fatalxiao@163.com>
2017-11-01 08:35:41 +0800

    update Calculation

--- src/_vendors/Calculation.js
+++ src/_vendors/Calculation.js
@@ -3,6 +3,8 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
+import Valid from './Valid';
+
 function pageSize(pageSize, pageSizes, defaultValue) {
 
     if (pageSize) {
@@ -22,6 +24,31 @@ function pageSize(pageSize, pageSizes, defaultValue) {
 
 }
 
+function longListDisplayIndex(data, listHeight, itemHeight, scrollTop = 0, buffer = 0) {
+
+    if (!data || !listHeight || !itemHeight) {
+        return {
+            start: 0,
+            stop: 0,
+            startWithCache: 0,
+            stopWithCache: 0
+        };
+    }
+
+    const len = data.length,
+        start = Math.floor(scrollTop / itemHeight),
+        stop = start + Math.ceil(listHeight / itemHeight);
+
+    return {
+        start: Valid.range(start, 0, len - 1),
+        stop: Valid.range(stop, 0, len - 1),
+        startWithCache: Valid.range(start - buffer, 0, len - 1),
+        stopWithCache: Valid.range(stop + buffer, 0, len - 1)
+    };
+
+}
+
 export default {
-    pageSize
+    pageSize,
+    longListDisplayIndex
 };
