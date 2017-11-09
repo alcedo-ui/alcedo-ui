540ee25c5bc8b913688bfa48e6cb69c6846be4df
Liangxiaojun <fatalxiao@163.com>
2017-11-09 10:55:47 +0800

    update DropdownFilter examples

--- examples/containers/app/modules/filter/DropdownFilterExamples.js
+++ examples/containers/app/modules/filter/DropdownFilterExamples.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import LocalAutoComplete from 'src/LocalAutoComplete';
+import DropdownFilter from 'src/DropdownFilter';
 import Widget from 'src/Widget';
 import WidgetHeader from 'src/WidgetHeader';
 
@@ -52,10 +52,10 @@ export default class DropdownFilterExamples extends Component {
         return (
             <div className="example auto-complete-examples">
 
-                <h2 className="example-title">LocalAutoComplete</h2>
+                <h2 className="example-title">DropdownFilter</h2>
 
                 <p>
-                    A <span>LocalAutoComplete</span> is an interface to help users to complete their input.
+                    A <span>DropdownFilter</span> is a filter.
                 </p>
 
                 <h2 className="example-title">Examples</h2>
@@ -69,89 +69,14 @@ export default class DropdownFilterExamples extends Component {
 
                             <div className="examples-wrapper">
 
-                                <p><code>LocalAutoComplete</code> simple example.</p>
+                                <p><code>DropdownFilter</code> simple example.</p>
 
-                                <LocalAutoComplete data={this.data}
-                                                   placeholder="Please select ..."
-                                                   onChange={this.onChange}
-                                                   onFilterPressEnter={this.filterPressEnterHandle}
-                                                   onFilterClear={this.filterClearHandle}/>
-
-                            </div>
-
-                        </div>
-                    </div>
-
-                </Widget>
-
-                <Widget>
-
-                    <WidgetHeader className="example-header" title="With mode"/>
-
-                    <div className="widget-content">
-                        <div className="example-content">
-
-                            <div className="examples-wrapper">
-
-                                <p>
-                                    <code>LocalAutoComplete</code> with default mode, <code> normal</code>
-                                    , <code>checkbox</code>, <code>radio</code> applied.
-                                </p>
-
-                                <LocalAutoComplete popupStyle={{maxHeight: 300}}
-                                                   mode="checkbox"
-                                                   data={this.data}
-                                                   placeholder="Please select ..."
-                                                   onChange={this.onChange}/>
-
-                            </div>
-
-                        </div>
-                    </div>
-
-                </Widget>
-
-                <Widget>
-
-                    <WidgetHeader className="example-header" title="With mode"/>
-
-                    <div className="widget-content">
-                        <div className="example-content">
-
-                            <div className="examples-wrapper">
-
-                                <p>Set the <code>disabled</code> property to true,the <code>LocalAutoComplete</code>
-                                    will disabled.</p>
-
-                                <LocalAutoComplete popupStyle={{maxHeight: 300}}
-                                                   disabled={true}
-                                                   data={this.data}
-                                                   placeholder="Please select ..."
-                                                   onChange={this.onChange}/>
-
-                            </div>
-
-                        </div>
-                    </div>
-
-                </Widget>
-
-                <Widget>
-
-                    <WidgetHeader className="example-header" title="With mode"/>
-
-                    <div className="widget-content">
-                        <div className="example-content">
-
-                            <div className="examples-wrapper">
-
-                                <p>Set the <code>noMatchedMsg</code> property for no matched message.</p>
-
-                                <LocalAutoComplete popupStyle={{maxHeight: 300}}
-                                                   noMatchedMsg="There have no matched value."
-                                                   data={this.data}
-                                                   placeholder="Please select ..."
-                                                   onChange={this.onChange}/>
+                                <DropdownFilter data={this.data}
+                                                mode={DropdownFilter.Mode.CHECKBOX}
+                                                placeholder="Please select ..."
+                                                onChange={this.onChange}
+                                                onFilterPressEnter={this.filterPressEnterHandle}
+                                                onFilterClear={this.filterClearHandle}/>
 
                             </div>
 
