3e67b70f521f33ce6fce8df53c231b15fabe5d0b
Liangxiaojun <fatalxiao@163.com>
2017-11-17 10:59:21 +0800

    update Tip examples

--- examples/containers/app/modules/message/TipExamples.js
+++ examples/containers/app/modules/message/TipExamples.js
@@ -1,119 +1,288 @@
 import React, {Component} from 'react';
 
-import IconButton from 'src/IconButton';
+import RaisedButton from 'src/RaisedButton';
+import Tip from 'src/Tip';
 import Widget from 'src/Widget';
 import WidgetHeader from 'src/WidgetHeader';
-import Tip from 'src/Tip';
 
-import TipProvider from 'src/TipProvider';
-import RaisedButton from 'src/RaisedButton';
 import PropTypeDescTable from '../PropTypeDescTable';
-import TipDoc from 'assets/propTypes/TipBody.json';
+import MenuDoc from 'assets/propTypes/MenuBody.json';
 
-import 'sass/containers/app/modules/message/TipExamples.scss';
+import 'sass/containers/app/modules/popup/TipExamples.scss';
 
 export default class TipExamples extends Component {
 
     constructor(props) {
+
         super(props);
-        this.text = 'This is a icon button';
+
+        this.state = {
+            tipVisible4: false,
+            tipVisible5: false,
+            tipVisible6: false,
+            tipVisible7: false,
+            tipVisible8: false,
+            tipVisible9: false,
+            tipVisible10: false,
+            tipVisible11: false,
+            tipVisible12: false,
+            tipVisible13: false,
+            tipVisible14: false,
+            tipVisible15: false,
+            triggerEl4: null,
+            triggerEl5: null,
+            triggerEl6: null,
+            triggerEl7: null,
+            triggerEl8: null,
+            triggerEl9: null,
+            triggerEl10: null,
+            triggerEl11: null,
+            triggerEl12: null,
+            triggerEl13: null,
+            triggerEl14: null,
+            triggerEl15: null
+        };
+
+        this.showTip = this::this.showTip;
+        this.hideTip = this::this.hideTip;
+
+    }
+
+    showTip(e, id) {
+        this.setState({
+            [`tipVisible${id}`]: true,
+            [`triggerEl${id}`]: e.currentTarget
+        });
+    }
+
+    hideTip(id) {
+        this.setState({
+            [`tipVisible${id}`]: false
+        });
     }
 
     render() {
+
+        const {
+            tipVisible4, tipVisible5, tipVisible6,
+            tipVisible7, tipVisible8, tipVisible9, tipVisible10, tipVisible11, tipVisible12,
+            tipVisible13, tipVisible14, tipVisible15,
+            triggerEl4, triggerEl5, triggerEl6,
+            triggerEl7, triggerEl8, triggerEl9, triggerEl10, triggerEl11,
+            triggerEl12, triggerEl13, triggerEl14, triggerEl15
+        } = this.state;
+
         return (
-            <div className="example tip-examples">
+            <div className="example menu-examples">
 
                 <h2 className="example-title">Tip</h2>
 
                 <p>
-                    <span>Tip</span>is a simple text bubble box.Move the mouse to display the prompt, remove the
-                    disappearance, bubble floating layer does not carry the complex text and operation.
+                    A <span>Tip</span> can be used as an alternative to a Drop Down Tip that can contain elements
+                    inside.
                 </p>
 
                 <h2 className="example-title">Examples</h2>
 
                 <Widget>
 
-                    <WidgetHeader className="example-header" title="With theme and tipPosition"/>
+                    <WidgetHeader className="example-header"
+                                  title="With value"/>
 
                     <div className="widget-content">
                         <div className="example-content">
 
-                            <div className="examples-wrapper">
+                            <div className="popup-example-wrapper">
 
-                                <p>Set <code>theme</code> property to different theme and <code>tipPosition</code> to
-                                    different position.</p>
+                                <p>
+                                    Set the <code>value</code> property to show different position of <code>Tip</code>.
+                                </p>
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="success"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.TOP}/>
+                                <div className="button-group-wrapper">
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="error"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.RIGHT}/>
+                                    <div className="button-group top">
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Top Left"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 7);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Top"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 8);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Top right"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 9);
+                                                      }}/>
+                                    </div>
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="primary"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.BOTTOM}/>
+                                    <div className="button-group right">
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Right Top"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 13);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Right"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 14);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Right Bottom"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 15);
+                                                      }}/>
+                                    </div>
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="highlight"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.LEFT}/>
+                                    <div className="button-group bottom">
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Bottom Left"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 4);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Bottom"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 5);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Bottom Right"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 6);
+                                                      }}/>
+                                    </div>
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="warning"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.TOP_LEFT}/>
+                                    <div className="button-group left">
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Left Top"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 10);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Left"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 11);
+                                                      }}/>
+                                        <RaisedButton className="trigger-position-button"
+                                                      value="Left Bottom"
+                                                      onMouseEnter={(e) => {
+                                                          this.showTip(e, 12);
+                                                      }}/>
+                                    </div>
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="success"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.TOP_RIGHT}/>
+                                </div>
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="success"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.BOTTOM_RIGHT}/>
+                                <Tip visible={tipVisible4}
+                                     triggerEl={triggerEl4}
+                                     position={Tip.Position.BOTTOM_LEFT}
+                                     onRequestClose={() => {
+                                         this.hideTip(4);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible5}
+                                     triggerEl={triggerEl5}
+                                     position={Tip.Position.BOTTOM}
+                                     onRequestClose={() => {
+                                         this.hideTip(5);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible6}
+                                     triggerEl={triggerEl6}
+                                     position={Tip.Position.BOTTOM_RIGHT}
+                                     onRequestClose={() => {
+                                         this.hideTip(6);
+                                     }}>
+                                    Tip
+                                </Tip>
 
-                                <IconButton iconCls="fa fa-plus"
-                                            theme="success"
-                                            tip={this.text}
-                                            tipPosition={Tip.Position.BOTTOM_LEFT}/>
+                                <Tip visible={tipVisible7}
+                                     triggerEl={triggerEl7}
+                                     position={Tip.Position.TOP_LEFT}
+                                     onRequestClose={() => {
+                                         this.hideTip(7);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible8}
+                                     triggerEl={triggerEl8}
+                                     position={Tip.Position.TOP}
+                                     onRequestClose={() => {
+                                         this.hideTip(8);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible9}
+                                     triggerEl={triggerEl9}
+                                     position={Tip.Position.TOP_RIGHT}
+                                     onRequestClose={() => {
+                                         this.hideTip(9);
+                                     }}>
+                                    Tip
+                                </Tip>
+
+                                <Tip visible={tipVisible10}
+                                     triggerEl={triggerEl10}
+                                     position={Tip.Position.LEFT_TOP}
+                                     onRequestClose={() => {
+                                         this.hideTip(10);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible11}
+                                     triggerEl={triggerEl11}
+                                     position={Tip.Position.LEFT}
+                                     onRequestClose={() => {
+                                         this.hideTip(11);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible12}
+                                     triggerEl={triggerEl12}
+                                     position={Tip.Position.LEFT_BOTTOM}
+                                     onRequestClose={() => {
+                                         this.hideTip(12);
+                                     }}>
+                                    Tip
+                                </Tip>
+
+                                <Tip visible={tipVisible13}
+                                     triggerEl={triggerEl13}
+                                     position={Tip.Position.RIGHT_TOP}
+                                     onRequestClose={() => {
+                                         this.hideTip(13);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible14}
+                                     triggerEl={triggerEl14}
+                                     position={Tip.Position.RIGHT}
+                                     onRequestClose={() => {
+                                         this.hideTip(14);
+                                     }}>
+                                    Tip
+                                </Tip>
+                                <Tip visible={tipVisible15}
+                                     triggerEl={triggerEl15}
+                                     position={Tip.Position.RIGHT_BOTTOM}
+                                     onRequestClose={() => {
+                                         this.hideTip(15);
+                                     }}>
+                                    Tip
+                                </Tip>
 
                             </div>
 
                         </div>
                     </div>
-
-                </Widget>
-
-                <Widget>
-
-                    <WidgetHeader className="example-header" title="Tip Container"/>
-
-                    <div className="widget-content">
-                        <div className="example-content">
-
-                            <div className="examples-wrapper">
-                                <TipProvider text="Here is a tip.">
-                                    <RaisedButton className="tip-button"
-                                                  value="Show me a tip">
-                                    </RaisedButton>
-                                </TipProvider>
-                            </div>
-
-                        </div>
-                    </div>
-
                 </Widget>
 
                 <h2 className="example-title">Properties</h2>
 
-                <PropTypeDescTable data={TipDoc}/>
+                <PropTypeDescTable data={MenuDoc}/>
 
             </div>
         );
