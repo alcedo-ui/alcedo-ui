f3c3ac88a0c2e8fa0c994a8295fe3b47af2db3ce
Liangxiaojun <fatalxiao@163.com>
2017-11-23 11:30:04 +0800

    add Position static

---
+++ src/_statics/Position.js
@@ -0,0 +1,24 @@
+/**
+ * @file Position static
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+export default {
+
+    TOP_LEFT: 'top-left',
+    TOP: 'top',
+    TOP_RIGHT: 'top-right',
+
+    BOTTOM_LEFT: 'bottom-left',
+    BOTTOM: 'bottom',
+    BOTTOM_RIGHT: 'bottom-right',
+
+    LEFT_TOP: 'left-top',
+    LEFT: 'left',
+    LEFT_BOTTOM: 'left-bottom',
+
+    RIGHT_TOP: 'right-top',
+    RIGHT: 'right',
+    RIGHT_BOTTOM: 'right-bottom'
+
+};
