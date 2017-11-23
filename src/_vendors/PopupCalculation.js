2f8ba7e7b75342eab745f4838068aa7cc77c6d08
Liangxiaojun <fatalxiao@163.com>
2017-11-23 13:01:56 +0800

    update PopupCalculation

--- src/_vendors/PopupCalculation.js
+++ src/_vendors/PopupCalculation.js
@@ -3,6 +3,9 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
+import Dom from './Dom';
+import Position from '../_statics/Position';
+
 function topVerticalBottom(triggerEl, triggerOffset) {
     return triggerOffset.top + triggerEl.offsetHeight;
 }
@@ -47,6 +50,82 @@ function leftHorizontalRight(triggerEl, triggerOffset) {
     return triggerOffset.left + triggerEl.offsetWidth;
 }
 
+function getStyle(triggerEl, popupEl, position) {
+
+    if (!triggerEl || !popupEl) {
+        return;
+    }
+
+    const triggerOffset = Dom.getOffset(triggerEl);
+    let left, top;
+
+    switch (position) {
+        case Position.TOP_LEFT: {
+            left = leftVerticalLeft(triggerOffset);
+            top = topVerticalTop(triggerOffset, popupEl);
+            break;
+        }
+        case Position.TOP: {
+            left = leftVerticalCenter(triggerEl, triggerOffset, popupEl);
+            top = topVerticalTop(triggerOffset, popupEl);
+            break;
+        }
+        case Position.TOP_RIGHT: {
+            left = leftVerticalRight(triggerEl, triggerOffset, popupEl);
+            top = topVerticalTop(triggerOffset, popupEl);
+            break;
+        }
+        case Position.BOTTOM_LEFT: {
+            left = leftVerticalLeft(triggerOffset);
+            top = topVerticalBottom(triggerEl, triggerOffset);
+            break;
+        }
+        case Position.BOTTOM: {
+            left = leftVerticalCenter(triggerEl, triggerOffset, popupEl);
+            top = topVerticalBottom(triggerEl, triggerOffset);
+            break;
+        }
+        case Position.BOTTOM_RIGHT: {
+            left = leftVerticalRight(triggerEl, triggerOffset, popupEl);
+            top = topVerticalBottom(triggerEl, triggerOffset);
+            break;
+        }
+        case Position.LEFT_TOP: {
+            left = leftHorizontalLeft(triggerOffset, popupEl);
+            top = topHorizontalTop(triggerOffset);
+            break;
+        }
+        case Position.LEFT: {
+            left = leftHorizontalLeft(triggerOffset, popupEl);
+            top = topHorizontalMiddle(triggerEl, triggerOffset, popupEl);
+            break;
+        }
+        case Position.LEFT_BOTTOM: {
+            left = leftHorizontalLeft(triggerOffset, popupEl);
+            top = topHorizontalBottom(triggerEl, triggerOffset, popupEl);
+            break;
+        }
+        case Position.RIGHT_TOP: {
+            left = leftHorizontalRight(triggerEl, triggerOffset);
+            top = topHorizontalTop(triggerOffset);
+            break;
+        }
+        case Position.RIGHT: {
+            left = leftHorizontalRight(triggerEl, triggerOffset);
+            top = topHorizontalMiddle(triggerEl, triggerOffset, popupEl);
+            break;
+        }
+        case Position.RIGHT_BOTTOM: {
+            left = leftHorizontalRight(triggerEl, triggerOffset);
+            top = topHorizontalBottom(triggerEl, triggerOffset, popupEl);
+            break;
+        }
+    }
+
+    return {left, top};
+
+}
+
 export default {
     topVerticalBottom,
     topVerticalTop,
@@ -57,5 +136,6 @@ export default {
     leftVerticalCenter,
     leftVerticalRight,
     leftHorizontalLeft,
-    leftHorizontalRight
+    leftHorizontalRight,
+    getStyle
 };
