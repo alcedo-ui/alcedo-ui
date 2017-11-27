b6e8c7cea21abcb7adff016785d8b691ce21cdf9
Liangxiaojun <fatalxiao@163.com>
2017-11-27 14:39:03 +0800

    optimize Popup

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -8,11 +8,12 @@ import React, {Component} from 'react';
 import Portal from '../Portal';
 import PopupBody from '../_PopupBody';
 import Position from '../_statics/Position';
+import TriggerMode from '../_statics/TriggerMode';
 
 export default class Popup extends Component {
 
     static Position = Position;
-    static TriggerMode = PopupBody.TriggerMode;
+    static TriggerMode = TriggerMode;
 
     constructor(props, ...restArgs) {
         super(props, ...restArgs);
