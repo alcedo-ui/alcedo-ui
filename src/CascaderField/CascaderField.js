df3d17e40f0b249bbc7a993fb0b29a67f8a6700d
Liangxiaojun <fatalxiao@163.com>
2017-12-18 09:36:53 +0800

    optimize CascaderField

--- src/CascaderField/CascaderField.js
+++ src/CascaderField/CascaderField.js
@@ -18,6 +18,8 @@ import DropdownCalculation from '../_vendors/DropdownCalculation';
 
 export default class CascaderField extends Component {
 
+    static Theme = Theme;
+
     constructor(props, ...restArgs) {
 
         super(props, ...restArgs);
