2c55a6072fc5c54d270452b4bdc579f719c2f74a
Liangxiaojun <fatalxiao@163.com>
2017-12-18 11:04:20 +0800

    update DropdownSelect

--- src/DropdownSelect/DropdownSelect.js
+++ src/DropdownSelect/DropdownSelect.js
@@ -237,7 +237,7 @@ export default class DropdownSelect extends Component {
                             isMultiSelect && useSelectAll ?
                                 <div className="list-item dropdown-select-all-wrapper"
                                      onTouchTap={this.selectAllTouchTapHandler}>
-                                    <Checkbox className="list-item-checkbox"
+                                    <Checkbox className="list-item-select"
                                               checked={data && value && value.length === data.length}
                                               indeterminate={data && value && value.length > 0 && value.length < data.length}/>
                                     Select All
