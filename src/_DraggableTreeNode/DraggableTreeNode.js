d299484ba7b8ebe8e7f472c3a338e468047bf04d
Liangxiaojun <fatalxiao@163.com>
2017-12-18 14:55:18 +0800

    update DraggableTreeNode

--- src/_DraggableTreeNode/DraggableTreeNode.js
+++ src/_DraggableTreeNode/DraggableTreeNode.js
@@ -20,6 +20,7 @@ import SelectMode from '../_statics/SelectMode';
 
 export default class DraggableTreeNode extends Component {
 
+    static SelectMode = SelectMode;
     static Theme = Theme;
 
     constructor(props, ...restArgs) {
