8264d413ce77e2d22b820ff106ba493817470e69
Liangxiaojun <fatalxiao@163.com>
2017-12-18 09:39:29 +0800

    optimize EditableSelect

--- src/EditableSelect/EditableSelect.js
+++ src/EditableSelect/EditableSelect.js
@@ -17,6 +17,8 @@ import DropdownCalculation from '../_vendors/DropdownCalculation';
 
 export default class EditableSelect extends Component {
 
+    static Theme = Theme;
+
     constructor(props, ...restArgs) {
 
         super(props, ...restArgs);
