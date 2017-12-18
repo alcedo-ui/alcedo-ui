f2f77fa7a2e122de9cf9fcd815093562dd8ea2e4
Liangxiaojun <fatalxiao@163.com>
2017-12-18 14:48:01 +0800

    add DraggableTree

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ export DotStep from './DotStep';
 export DownloadField from './DownloadField';
 export DraggableGrid from './DraggableGrid';
 export DraggableList from './DraggableList';
+export DraggableTree from './DraggableTree';
 export Dropdown from './Dropdown';
 export DropdownFilter from './DropdownFilter';
 export DropdownSelect from './DropdownSelect';
@@ -83,4 +84,4 @@ export Waterfall from './Waterfall';
 export Widget from './Widget';
 export WidgetHeader from './WidgetHeader';
 
-export const COMPONENTS_TOTAL_COUNT = 83;
+export const COMPONENTS_TOTAL_COUNT = 84;
