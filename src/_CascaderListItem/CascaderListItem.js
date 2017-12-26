3f593e05c826ea6dbe4c7f99d9221e28023f70c7
Liangxiaojun <fatalxiao@163.com>
2017-12-26 16:22:13 +0800

    update CascaderListItem

--- src/_CascaderListItem/CascaderListItem.js
+++ src/_CascaderListItem/CascaderListItem.js
@@ -57,10 +57,10 @@ export default class CascaderListItem extends Component {
 
     render() {
 
-        const {listWidth, listData, valueField, displayField, path, depth} = this.props,
+        const {listWidth, data, valueField, displayField, path, depth} = this.props,
 
             currDepth = this.props.currDepth || 0,
-            activatedNode = currDepth in path ? listData[path[currDepth].index] : null,
+            activatedNode = currDepth in path ? data[path[currDepth].index] : null,
             hasChildren = activatedNode && activatedNode.children && activatedNode.children.length > 0,
 
             listStyle = currDepth === 0 ?
@@ -80,7 +80,7 @@ export default class CascaderListItem extends Component {
 
                 <List className={'cascader-popup-list' + popupListClassName}
                       style={popupListStyle}
-                      data={this.formatData(listData)}
+                      data={this.formatData(data)}
                       value={activatedNode}
                       valueField={valueField}
                       displayField={displayField}
@@ -89,7 +89,7 @@ export default class CascaderListItem extends Component {
                 {
                     hasChildren ?
                         <CascaderListItem {...this.props}
-                                          listData={activatedNode.children}
+                                          data={activatedNode.children}
                                           currDepth={currDepth + 1}/>
                         :
                         null
@@ -111,7 +111,7 @@ CascaderListItem.propTypes = {
     /**
      * The data of cascader-list.
      */
-    listData: PropTypes.array,
+    data: PropTypes.array,
 
     /**
      *
@@ -156,7 +156,7 @@ CascaderListItem.defaultProps = {
 
     listWidth: 200,
 
-    listData: [],
+    data: [],
 
     path: [],
 
