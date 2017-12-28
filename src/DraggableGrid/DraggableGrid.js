4e2b6520bae1268f76a75464e4c1dacb03807077
Liangxiaojun <fatalxiao@163.com>
2017-12-28 11:15:40 +0800

    update DraggableGrid

--- src/DraggableGrid/DraggableGrid.js
+++ src/DraggableGrid/DraggableGrid.js
@@ -6,7 +6,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import withScrolling, {createVerticalStrength, createHorizontalStrength} from 'react-dnd-scrollzone';
 
 import DraggableGridItem from '../_DraggableGridItem';
 import Tip from '../Tip';
@@ -16,8 +15,6 @@ import Util from '../_vendors/Util';
 import Event from '../_vendors/Event';
 import Calculation from '../_vendors/Calculation';
 import SelectMode from '../_statics/SelectMode';
-
-const ScrollingComponent = withScrolling('div');
 
 export default class DraggableGrid extends Component {
 
@@ -29,29 +26,11 @@ export default class DraggableGrid extends Component {
         super(props, ...restArgs);
 
         this.state = {
-            data: props.data,
             value: Calculation.getInitValue(props)
         };
 
-        this.listItemMoveHandler = ::this.listItemMoveHandler;
         this.listItemSelectHandler = ::this.listItemSelectHandler;
         this.listItemDeselectHandler = ::this.listItemDeselectHandler;
-
-    }
-
-    listItemMoveHandler(dragIndex, hoverIndex, props) {
-
-        const {data} = this.state,
-            dragItem = data.splice(dragIndex, 1);
-
-        data.splice(hoverIndex, 0, ...dragItem);
-
-        this.setState({
-            data
-        }, () => {
-            const {onSequenceChange} = this.props;
-            onSequenceChange && onSequenceChange(data);
-        });
 
     }
 
@@ -114,52 +93,36 @@ export default class DraggableGrid extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-
-        let state;
-
-        if (nextProps.data !== this.state.data) {
-            state = state ? state : {};
-            state.data = nextProps.data;
-        }
         if (nextProps.value !== this.state.value) {
-            state = state ? state : {};
-            state.value = Calculation.getInitValue(nextProps);
+            this.setState({
+                value: Calculation.getInitValue(nextProps)
+            });
         }
-
-        if (state) {
-            this.setState(state);
-        }
-
     }
 
     render() {
 
         const {
 
-                children, className, style, theme, itemHeight, col,
+                children, className, style, theme, data, itemHeight, col,
 
                 selectTheme, selectMode, radioUncheckedIconCls, radioCheckedIconCls,
                 checkboxUncheckedIconCls, checkboxCheckedIconCls, checkboxIndeterminateIconCls,
 
-                idField, valueField, displayField, descriptionField, disabled, isLoading, renderer, onItemTouchTap,
-
-                scrollSpeed, scrollBuffer
+                idField, valueField, displayField, descriptionField, disabled, isLoading, renderer, onItemTouchTap
 
             } = this.props,
-            {data, value} = this.state,
+            {value} = this.state,
 
-            gridClassName = (className ? ' ' + className : '');
+            listClassName = (className ? ' ' + className : '');
 
         return (
-            <ScrollingComponent className={'draggable-grid' + gridClassName}
-                                disabled={disabled}
-                                style={style}
-                                strengthMultiplier={scrollSpeed}
-                                verticalStrength={createVerticalStrength(scrollBuffer)}
-                                horizontalStrength={createHorizontalStrength(scrollBuffer)}
-                                onWheel={e => {
-                                    Event.wheelHandler(e, this.props);
-                                }}>
+            <div className={'draggable-grid' + listClassName}
+                 disabled={disabled}
+                 style={style}
+                 onWheel={e => {
+                     Event.wheelHandler(e, this.props);
+                 }}>
 
                 {
                     _.isArray(data) && data.length > 0 ?
@@ -189,7 +152,6 @@ export default class DraggableGrid extends Component {
                                                            isLoading={isLoading || item.isLoading}
                                                            selectMode={selectMode}
                                                            renderer={renderer}
-                                                           onMove={this.listItemMoveHandler}
                                                            onTouchTap={e => {
                                                                onItemTouchTap && onItemTouchTap(item, index, e);
                                                                item.onTouchTap && item.onTouchTap(e);
@@ -222,7 +184,6 @@ export default class DraggableGrid extends Component {
                                                            isLoading={isLoading}
                                                            selectMode={selectMode}
                                                            renderer={renderer}
-                                                           onMove={this.listItemMoveHandler}
                                                            onTouchTap={e => {
                                                                onItemTouchTap && onItemTouchTap(item, index, e);
                                                            }}
@@ -242,7 +203,7 @@ export default class DraggableGrid extends Component {
 
                 {children}
 
-            </ScrollingComponent>
+            </div>
         );
     }
 };
@@ -393,16 +354,6 @@ DraggableGrid.propTypes = {
      */
     isLoading: PropTypes.bool,
 
-    /**
-     * The speed of scroll bar.
-     */
-    scrollSpeed: PropTypes.number,
-
-    /**
-     * The number of overflows.
-     */
-    scrollBuffer: PropTypes.number,
-
     shouldPreventContainerScroll: PropTypes.bool,
 
     radioUncheckedIconCls: PropTypes.string,
@@ -439,11 +390,6 @@ DraggableGrid.propTypes = {
     onChange: PropTypes.func,
 
     /**
-     * Callback function fired when select item sequence changed.
-     */
-    onSequenceChange: PropTypes.func,
-
-    /**
      * Callback function fired when wrapper wheeled.
      */
     onWheel: PropTypes.func
@@ -466,9 +412,6 @@ DraggableGrid.defaultProps = {
     displayField: 'text',
     descriptionField: 'desc',
     disabled: false,
-
-    scrollSpeed: 20,
-    scrollBuffer: 40,
     shouldPreventContainerScroll: true,
 
     radioUncheckedIconCls: null,
