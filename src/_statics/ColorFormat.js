8bcf664711db5475543528f2d37e1ba23d6e774c
Liangxiaojun <fatalxiao@163.com>
2018-01-11 14:19:15 +0800

    add ColorFormat static

---
+++ src/_statics/ColorFormat.js
@@ -0,0 +1,14 @@
+/**
+ * @file ColorFormat static
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+export default {
+
+    HEX: Symbol('HEX'),
+    RGB: Symbol('RGB'),
+    CMYK: Symbol('CMYK'),
+    LAB: Symbol('LAB'),
+    HSB: Symbol('HSB')
+
+};
