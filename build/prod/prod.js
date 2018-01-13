7221ae9ed290cc445cca29c9fcaed61ebb370485
Liangxiaojun <fatalxiao@163.com>
2018-01-13 12:20:47 +0800

    update prod build

--- build/prod/prod.js
+++ build/prod/prod.js
@@ -2,24 +2,24 @@ delete process.env['DEBUG_FD'];
 
 process.env.NODE_ENV = '"production"';
 
-var ora = require('ora');
-var chalk = require('chalk');
-var rm = require('rimraf');
-var mkdirp = require('mkdirp');
-var webpack = require('webpack');
-var config = require('../../config/index');
-var webpackConfig = require('./webpack.config.prod.js');
+const ora = require('ora'),
+    chalk = require('chalk'),
+    rm = require('rimraf'),
+    mkdirp = require('mkdirp'),
+    webpack = require('webpack'),
+    config = require('../../config/index'),
+    webpackConfig = require('./webpack.config.prod.js'),
+    spinner = ora('building for production...');
 
-var spinner = ora('building for production...');
 spinner.start();
 
-mkdirp(config.build.assetsSubDirectory, function (err) {
+mkdirp(config.build.assetsSubDirectory, err => {
 
     if (err) {
         throw err;
     }
 
-    webpack(webpackConfig, function (err, stats) {
+    webpack(webpackConfig, (err, stats) => {
 
         spinner.stop();
 
@@ -28,16 +28,16 @@ mkdirp(config.build.assetsSubDirectory, function (err) {
         }
 
         process.stdout.write(stats.toString({
-                colors: true,
-                modules: false,
-                children: false,
-                chunks: false,
-                chunkModules: false
-            }) + '\n\n');
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+        }) + '\n\n');
 
         console.log(chalk.cyan('Build complete.'));
 
-        rm(config.build.assetsSubDirectory, function (err) {
+        rm(config.build.assetsSubDirectory, err => {
             if (err) {
                 throw err;
             }
