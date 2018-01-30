7e77b1fa7c2bdaa16ce5687d14f1038185ee8a02
Liangxiaojun <fatalxiao@163.com>
2018-01-30 09:46:31 +0800

    update AnchorButton

--- src/AnchorButton/AnchorButton.js
+++ src/AnchorButton/AnchorButton.js
@@ -17,6 +17,111 @@ export default class AnchorButton extends Component {
 
     static Theme = Theme;
     static TipPosition = Position;
+    static propTypes = {
+
+        /**
+         * The CSS class name of the root element.
+         */
+        className: PropTypes.string,
+
+        /**
+         * Override the styles of the root element.
+         */
+        style: PropTypes.object,
+
+        /**
+         * The button theme.Can be primary,highlight,success,warning,error.
+         */
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+        /**
+         * If true,the button will have rounded corners.
+         */
+        isRounded: PropTypes.bool,
+
+        /**
+         * If true,the button will be round.
+         */
+        isCircular: PropTypes.bool,
+
+        /**
+         * The text of the button.
+         */
+        value: PropTypes.any,
+
+        /**
+         * The type of button.Can be reset,submit or button.
+         */
+        type: PropTypes.string,
+
+        /**
+         * Disables the button if set to true.
+         */
+        disabled: PropTypes.bool,
+
+        /**
+         * If true,the button will be have loading effect.
+         */
+        isLoading: PropTypes.bool,
+
+        /**
+         * If true,the element's ripple effect will be disabled.
+         */
+        disableTouchRipple: PropTypes.bool,
+
+        /**
+         * Use this property to display an icon.It will display on the left.
+         */
+        iconCls: PropTypes.string,
+
+        /**
+         * Use this property to display an icon.It will display on the right.
+         */
+        rightIconCls: PropTypes.string,
+
+        /**
+         * If true,the ripple effect will be display centered.
+         */
+        rippleDisplayCenter: PropTypes.bool,
+
+        tip: PropTypes.string,
+        tipPosition: PropTypes.oneOf(Util.enumerateValue(TipProvider.Position)),
+
+        /**
+         * You can create a complicated renderer callback instead of value prop.
+         */
+        renderer: PropTypes.func,
+
+        /**
+         * Callback function fired when the button is touch-tapped.
+         */
+        onTouchTap: PropTypes.func
+
+    };
+    static defaultProps = {
+
+        className: '',
+        style: null,
+
+        theme: Theme.DEFAULT,
+        isRounded: false,
+        isCircular: false,
+
+        value: '',
+        disabled: false,
+        type: 'button',
+        isLoading: false,
+        disableTouchRipple: false,
+
+        rippleDisplayCenter: false,
+
+        iconCls: '',
+        rightIconCls: '',
+
+        tip: null,
+        tipPosition: TipProvider.Position.BOTTOM
+
+    };
 
     constructor(props, ...restArgs) {
 
@@ -48,111 +153,4 @@ export default class AnchorButton extends Component {
         );
 
     }
-};
-
-AnchorButton.propTypes = {
-
-    /**
-     * The CSS class name of the root element.
-     */
-    className: PropTypes.string,
-
-    /**
-     * Override the styles of the root element.
-     */
-    style: PropTypes.object,
-
-    /**
-     * The button theme.Can be primary,highlight,success,warning,error.
-     */
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-    /**
-     * If true,the button will have rounded corners.
-     */
-    isRounded: PropTypes.bool,
-
-    /**
-     * If true,the button will be round.
-     */
-    isCircular: PropTypes.bool,
-
-    /**
-     * The text of the button.
-     */
-    value: PropTypes.any,
-
-    /**
-     * The type of button.Can be reset,submit or button.
-     */
-    type: PropTypes.string,
-
-    /**
-     * Disables the button if set to true.
-     */
-    disabled: PropTypes.bool,
-
-    /**
-     * If true,the button will be have loading effect.
-     */
-    isLoading: PropTypes.bool,
-
-    /**
-     * If true,the element's ripple effect will be disabled.
-     */
-    disableTouchRipple: PropTypes.bool,
-
-    /**
-     * Use this property to display an icon.It will display on the left.
-     */
-    iconCls: PropTypes.string,
-
-    /**
-     * Use this property to display an icon.It will display on the right.
-     */
-    rightIconCls: PropTypes.string,
-
-    /**
-     * If true,the ripple effect will be display centered.
-     */
-    rippleDisplayCenter: PropTypes.bool,
-
-    tip: PropTypes.string,
-    tipPosition: PropTypes.oneOf(Util.enumerateValue(TipProvider.Position)),
-
-    /**
-     * You can create a complicated renderer callback instead of value prop.
-     */
-    renderer: PropTypes.func,
-
-    /**
-     * Callback function fired when the button is touch-tapped.
-     */
-    onTouchTap: PropTypes.func
-
-};
-
-AnchorButton.defaultProps = {
-
-    className: '',
-    style: null,
-
-    theme: Theme.DEFAULT,
-    isRounded: false,
-    isCircular: false,
-
-    value: '',
-    disabled: false,
-    type: 'button',
-    isLoading: false,
-    disableTouchRipple: false,
-
-    rippleDisplayCenter: false,
-
-    iconCls: '',
-    rightIconCls: '',
-
-    tip: null,
-    tipPosition: TipProvider.Position.BOTTOM
-
 };
