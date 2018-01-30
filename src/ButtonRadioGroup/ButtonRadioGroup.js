d2d2e1be91e408949897ee8820fe4dcdafe17592
Liangxiaojun <fatalxiao@163.com>
2018-01-30 09:48:30 +0800

    update ButtonRadioGroup

--- src/ButtonRadioGroup/ButtonRadioGroup.js
+++ src/ButtonRadioGroup/ButtonRadioGroup.js
@@ -14,6 +14,94 @@ import Util from '../_vendors/Util';
 export default class ButtonRadioGroup extends Component {
 
     static Theme = Theme;
+    static propTypes = {
+
+        /**
+         * The CSS class name of the root element.
+         */
+        className: PropTypes.string,
+
+        /**
+         * Override the styles of the root element.
+         */
+        style: PropTypes.object,
+
+        /**
+         * The ButtonCheckbox theme.
+         */
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+        /**
+         * The ButtonCheckbox activated theme.
+         */
+        activatedTheme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+        /**
+         * The hidden input name for form submit.
+         */
+        name: PropTypes.string,
+
+        /**
+         * Data for ButtonRadioGroup.
+         */
+        data: PropTypes.arrayOf(PropTypes.shape({
+
+            /**
+             * The className of RaisedButton.
+             */
+            className: PropTypes.string,
+
+            /**
+             * The style of RaisedButton.
+             */
+            style: PropTypes.object,
+
+            /**
+             * The label of RaisedButton.
+             */
+            label: PropTypes.any,
+
+            /**
+             * The value of RaisedButton.
+             */
+            value: PropTypes.any,
+
+            /**
+             * If true, the RaisedButton will be disabled.
+             */
+            disabled: PropTypes.bool
+
+        })).isRequired,
+
+        /**
+         * Set one of the button activation.
+         */
+        value: PropTypes.any,
+
+        /**
+         * If true, the ButtonRadioGroup will be disabled.
+         */
+        disabled: PropTypes.bool,
+
+        /**
+         * Callback function fired when click RaisedButton.
+         */
+        onChange: PropTypes.func
+
+    };
+    static defaultProps = {
+
+        className: '',
+        style: null,
+        theme: Theme.DEFAULT,
+        activatedTheme: Theme.PRIMARY,
+
+        name: '',
+        data: [],
+        value: '',
+        disabled: false
+
+    };
 
     constructor(props, ...restArgs) {
 
@@ -87,94 +175,4 @@ export default class ButtonRadioGroup extends Component {
         );
 
     }
-};
-
-ButtonRadioGroup.propTypes = {
-
-    /**
-     * The CSS class name of the root element.
-     */
-    className: PropTypes.string,
-
-    /**
-     * Override the styles of the root element.
-     */
-    style: PropTypes.object,
-
-    /**
-     * The ButtonCheckbox theme.
-     */
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-    /**
-     * The ButtonCheckbox activated theme.
-     */
-    activatedTheme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-    /**
-     * The hidden input name for form submit.
-     */
-    name: PropTypes.string,
-
-    /**
-     * Data for ButtonRadioGroup.
-     */
-    data: PropTypes.arrayOf(PropTypes.shape({
-
-        /**
-         * The className of RaisedButton.
-         */
-        className: PropTypes.string,
-
-        /**
-         * The style of RaisedButton.
-         */
-        style: PropTypes.object,
-
-        /**
-         * The label of RaisedButton.
-         */
-        label: PropTypes.any,
-
-        /**
-         * The value of RaisedButton.
-         */
-        value: PropTypes.any,
-
-        /**
-         * If true, the RaisedButton will be disabled.
-         */
-        disabled: PropTypes.bool
-
-    })).isRequired,
-
-    /**
-     * Set one of the button activation.
-     */
-    value: PropTypes.any,
-
-    /**
-     * If true, the ButtonRadioGroup will be disabled.
-     */
-    disabled: PropTypes.bool,
-
-    /**
-     * Callback function fired when click RaisedButton.
-     */
-    onChange: PropTypes.func
-
-};
-
-ButtonRadioGroup.defaultProps = {
-
-    className: '',
-    style: null,
-    theme: Theme.DEFAULT,
-    activatedTheme: Theme.PRIMARY,
-
-    name: '',
-    data: [],
-    value: '',
-    disabled: false
-
 };
