67c2760b5bedb0b6691d3038d7e528fb21a3384c
Liangxiaojun <fatalxiao@163.com>
2018-01-30 11:27:35 +0800

    update TimeLineItem

--- src/_TimeLineItem/TimeLineItem.js
+++ src/_TimeLineItem/TimeLineItem.js
@@ -6,28 +6,11 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-export default class TimeLineItem extends Component {
+class TimeLineItem extends Component {
 
     static Type = {
         HEADER: 'header',
         TITLE: 'title'
-    };
-    static propTypes = {
-
-        /**
-         * The CSS class name of the root element.
-         */
-        className: PropTypes.string,
-
-        /**
-         * Override the styles of the root element.
-         */
-        style: PropTypes.object
-
-    };
-    static defaultProps = {
-        className: '',
-        style: null
     };
 
     constructor(props, ...restArgs) {
@@ -73,3 +56,24 @@ export default class TimeLineItem extends Component {
         );
     }
 }
+
+TimeLineItem.propTypes = {
+
+    /**
+     * The CSS class name of the root element.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Override the styles of the root element.
+     */
+    style: PropTypes.object
+
+};
+
+TimeLineItem.defaultProps = {
+    className: '',
+    style: null
+};
+
+export default TimeLineItem;
