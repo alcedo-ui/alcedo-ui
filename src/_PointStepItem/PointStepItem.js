57e6f41c1e6f9e977a93f039a1a51cb0f699cc30
Liangxiaojun <fatalxiao@163.com>
2018-01-30 09:26:12 +0800

    update PointStepItem

--- src/_PointStepItem/PointStepItem.js
+++ src/_PointStepItem/PointStepItem.js
@@ -8,6 +8,45 @@ import PropTypes from 'prop-types';
 
 export default class PointStepItem extends Component {
 
+    static propTypes = {
+
+        className: PropTypes.string,
+        style: PropTypes.object,
+
+        activatedStep: PropTypes.number,
+        finishedStep: PropTypes.number,
+        index: PropTypes.number,
+        value: PropTypes.object,
+        isFirst: PropTypes.bool,
+        isLast: PropTypes.bool,
+
+        showFinishedStepIcon: PropTypes.bool,
+        finishedStepIconCls: PropTypes.string,
+
+        disabled: PropTypes.bool,
+
+        onTouchTap: PropTypes.func
+
+    };
+    static defaultProps = {
+
+        className: '',
+        style: null,
+
+        activatedStep: 0,
+        finishedStep: 0,
+        index: 0,
+        value: {},
+        isFirst: true,
+        isLast: false,
+
+        showFinishedStepIcon: true,
+        finishedStepIconCls: 'fa fa-check',
+
+        disabled: false
+
+    };
+
     constructor(props, ...restArgs) {
 
         super(props, ...restArgs);
@@ -68,44 +107,4 @@ export default class PointStepItem extends Component {
         );
 
     }
-};
-
-PointStepItem.propTypes = {
-
-    className: PropTypes.string,
-    style: PropTypes.object,
-
-    activatedStep: PropTypes.number,
-    finishedStep: PropTypes.number,
-    index: PropTypes.number,
-    value: PropTypes.object,
-    isFirst: PropTypes.bool,
-    isLast: PropTypes.bool,
-
-    showFinishedStepIcon: PropTypes.bool,
-    finishedStepIconCls: PropTypes.string,
-
-    disabled: PropTypes.bool,
-
-    onTouchTap: PropTypes.func
-
-};
-
-PointStepItem.defaultProps = {
-
-    className: '',
-    style: null,
-
-    activatedStep: 0,
-    finishedStep: 0,
-    index: 0,
-    value: {},
-    isFirst: true,
-    isLast: false,
-
-    showFinishedStepIcon: true,
-    finishedStepIconCls: 'fa fa-check',
-
-    disabled: false
-
 };
