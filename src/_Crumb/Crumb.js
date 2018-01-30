b69ac79a5005059d88eea0d81fe6cb6053bc3693
Liangxiaojun <fatalxiao@163.com>
2018-01-30 08:37:59 +0800

    update Crumb

--- src/_Crumb/Crumb.js
+++ src/_Crumb/Crumb.js
@@ -16,6 +16,103 @@ export default class Crumb extends Component {
 
     static Theme = Theme;
 
+    static propTypes = {
+
+        /**
+         * The CSS class name of the list button.
+         */
+        className: PropTypes.string,
+
+        /**
+         * Override the styles of the list button.
+         */
+        style: PropTypes.object,
+
+        /**
+         * The theme of the list button.
+         */
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+        /**
+         * The url of crumb.
+         */
+        href: PropTypes.string,
+
+        /**
+         * The text value of the list button.Type can be string or number.
+         */
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+        /**
+         * The list item's display text. Type can be string, number or bool.
+         */
+        text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+        /**
+         * The desc value of the list button. Type can be string or number.
+         */
+        desc: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+        /**
+         * If true,the list item will be disabled.
+         */
+        disabled: PropTypes.bool,
+
+        /**
+         * If true,the button will be have loading effect.
+         */
+        isLoading: PropTypes.bool,
+
+        /**
+         * If true,the element's ripple effect will be disabled.
+         */
+        disableTouchRipple: PropTypes.bool,
+
+        /**
+         * Use this property to display an icon. It will display on the left.
+         */
+        iconCls: PropTypes.string,
+
+        /**
+         * Use this property to display an icon. It will display on the right.
+         */
+        rightIconCls: PropTypes.string,
+
+        /**
+         * The message of tip.
+         */
+        tip: PropTypes.string,
+
+        /**
+         * The position of tip.
+         */
+        tipPosition: PropTypes.oneOf(Util.enumerateValue(Tip.Position)),
+
+        /**
+         * If true,the item will have center displayed ripple effect.
+         */
+        rippleDisplayCenter: PropTypes.bool,
+
+        /**
+         * You can create a complicated renderer callback instead of value and desc prop.
+         */
+        itemRenderer: PropTypes.func,
+
+        /**
+         * Callback function fired when a list item touch-tapped.
+         */
+        onTouchTap: PropTypes.func
+
+    };
+
+    static defaultProps = {
+
+        className: '',
+        style: null,
+        theme: Theme.DEFAULT
+
+    };
+
     constructor(props, ...restArgs) {
         super(props, ...restArgs);
     }
@@ -35,101 +132,4 @@ export default class Crumb extends Component {
         );
 
     }
-};
-
-Crumb.propTypes = {
-
-    /**
-     * The CSS class name of the list button.
-     */
-    className: PropTypes.string,
-
-    /**
-     * Override the styles of the list button.
-     */
-    style: PropTypes.object,
-
-    /**
-     * The theme of the list button.
-     */
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-    /**
-     * The url of crumb.
-     */
-    href: PropTypes.string,
-
-    /**
-     * The text value of the list button.Type can be string or number.
-     */
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-
-    /**
-     * The list item's display text. Type can be string, number or bool.
-     */
-    text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-
-    /**
-     * The desc value of the list button. Type can be string or number.
-     */
-    desc: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-
-    /**
-     * If true,the list item will be disabled.
-     */
-    disabled: PropTypes.bool,
-
-    /**
-     * If true,the button will be have loading effect.
-     */
-    isLoading: PropTypes.bool,
-
-    /**
-     * If true,the element's ripple effect will be disabled.
-     */
-    disableTouchRipple: PropTypes.bool,
-
-    /**
-     * Use this property to display an icon. It will display on the left.
-     */
-    iconCls: PropTypes.string,
-
-    /**
-     * Use this property to display an icon. It will display on the right.
-     */
-    rightIconCls: PropTypes.string,
-
-    /**
-     * The message of tip.
-     */
-    tip: PropTypes.string,
-
-    /**
-     * The position of tip.
-     */
-    tipPosition: PropTypes.oneOf(Util.enumerateValue(Tip.Position)),
-
-    /**
-     * If true,the item will have center displayed ripple effect.
-     */
-    rippleDisplayCenter: PropTypes.bool,
-
-    /**
-     * You can create a complicated renderer callback instead of value and desc prop.
-     */
-    itemRenderer: PropTypes.func,
-
-    /**
-     * Callback function fired when a list item touch-tapped.
-     */
-    onTouchTap: PropTypes.func
-
-};
-
-Crumb.defaultProps = {
-
-    className: '',
-    style: null,
-    theme: Theme.DEFAULT
-
 };
