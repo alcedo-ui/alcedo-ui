bb74ff1d275acc1f9fb2c6685e9d395ba5634050
Liangxiaojun <fatalxiao@163.com>
2018-01-30 11:12:00 +0800

    update MenuBody

--- src/_MenuBody/MenuBody.js
+++ src/_MenuBody/MenuBody.js
@@ -17,96 +17,10 @@ import PopupCalculation from '../_vendors/PopupCalculation';
 
 import Position from '../_statics/Position';
 
-export default class MenuBody extends Component {
+class MenuBody extends Component {
 
     static Position = Position;
     static Theme = Theme;
-    static propTypes = {
-
-        /**
-         * The CSS class name of the root element.
-         */
-        className: PropTypes.string,
-
-        /**
-         * Override the styles of the root element.
-         */
-        style: PropTypes.object,
-
-        /**
-         * This is the DOM element that will be used to set the position of the popover.
-         */
-        triggerEl: PropTypes.object,
-
-        /**
-         * If true,the popover is visible.
-         */
-        visible: PropTypes.bool,
-
-        /**
-         * If true,the popover will have a triangle on the top of the DOM element.
-         */
-        hasTriangle: PropTypes.bool,
-
-        triangle: PropTypes.element,
-
-        /**
-         * The popover theme.Can be primary,highlight,success,warning,error.
-         */
-        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-        /**
-         * The popover alignment.The value can be Menu.Position.LEFT or Menu.Position.RIGHT.
-         */
-        position: PropTypes.oneOf(Util.enumerateValue(Position)),
-
-        /**
-         * If true, menu will have animation effects.
-         */
-        isAnimated: PropTypes.bool,
-
-        shouldPreventContainerScroll: PropTypes.bool,
-
-        /**
-         * The depth of Paper component.
-         */
-        depth: PropTypes.number,
-
-        isTriggerPositionFixed: PropTypes.bool,
-
-        /**
-         * The function of menu render.
-         */
-        onRender: PropTypes.func,
-
-        /**
-         * Callback function fired when the popover is requested to be closed.
-         */
-        onRequestClose: PropTypes.func,
-
-        /**
-         * Callback function fired when wrapper wheeled.
-         */
-        onWheel: PropTypes.func
-
-    };
-    static defaultProps = {
-
-        className: '',
-        style: null,
-        theme: Theme.DEFAULT,
-
-        triggerEl: null,
-        visible: false,
-        hasTriangle: true,
-        triangle: <div className="menu-triangle"></div>,
-        position: Position.BOTTOM_LEFT,
-        isAnimated: true,
-        depth: 6,
-        shouldPreventContainerScroll: true,
-        isTriggerPositionFixed: false
-
-    };
 
     constructor(props, ...restArgs) {
 
@@ -264,3 +178,93 @@ export default class MenuBody extends Component {
 
     }
 };
+
+MenuBody.propTypes = {
+
+    /**
+     * The CSS class name of the root element.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Override the styles of the root element.
+     */
+    style: PropTypes.object,
+
+    /**
+     * This is the DOM element that will be used to set the position of the popover.
+     */
+    triggerEl: PropTypes.object,
+
+    /**
+     * If true,the popover is visible.
+     */
+    visible: PropTypes.bool,
+
+    /**
+     * If true,the popover will have a triangle on the top of the DOM element.
+     */
+    hasTriangle: PropTypes.bool,
+
+    triangle: PropTypes.element,
+
+    /**
+     * The popover theme.Can be primary,highlight,success,warning,error.
+     */
+    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+    /**
+     * The popover alignment.The value can be Menu.Position.LEFT or Menu.Position.RIGHT.
+     */
+    position: PropTypes.oneOf(Util.enumerateValue(Position)),
+
+    /**
+     * If true, menu will have animation effects.
+     */
+    isAnimated: PropTypes.bool,
+
+    shouldPreventContainerScroll: PropTypes.bool,
+
+    /**
+     * The depth of Paper component.
+     */
+    depth: PropTypes.number,
+
+    isTriggerPositionFixed: PropTypes.bool,
+
+    /**
+     * The function of menu render.
+     */
+    onRender: PropTypes.func,
+
+    /**
+     * Callback function fired when the popover is requested to be closed.
+     */
+    onRequestClose: PropTypes.func,
+
+    /**
+     * Callback function fired when wrapper wheeled.
+     */
+    onWheel: PropTypes.func
+
+};
+
+MenuBody.defaultProps = {
+
+    className: '',
+    style: null,
+    theme: Theme.DEFAULT,
+
+    triggerEl: null,
+    visible: false,
+    hasTriangle: true,
+    triangle: <div className="menu-triangle"></div>,
+    position: Position.BOTTOM_LEFT,
+    isAnimated: true,
+    depth: 6,
+    shouldPreventContainerScroll: true,
+    isTriggerPositionFixed: false
+
+};
+
+export default MenuBody;
