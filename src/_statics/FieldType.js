82569bc63878fd853656e29418f9ed854314d098
Liangxiaojun <fatalxiao@163.com>
2018-01-31 09:36:21 +0800

    update FieldType

---
+++ src/_statics/FieldType.js
@@ -0,0 +1,17 @@
+/**
+ * @file FieldType static
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+export default {
+    TEXT: 'text',
+    PASSWORD: 'password',
+    NUMBER: 'number',
+    INTEGER: 'integer',
+    POSITIVE_INTEGER: 'positiveInteger',
+    NONNEGATIVE_INTEGER: 'nonnegativeInteger',
+    NEGATIVE_INTEGER: 'negativeInteger',
+    NONPOSITIVE_INTEGER: 'nonpositiveInteger',
+    EMAIL: 'email',
+    URL: 'url'
+};
