0d76f54e0023d3962d9348237b192b68d16c3128
Liangxiaojun <fatalxiao@163.com>
2018-02-01 14:56:29 +0800

    update Thead

--- src/_Thead/Thead.js
+++ src/_Thead/Thead.js
@@ -7,6 +7,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import TableHeader from '../_TableHeader';
+import classNames from 'classnames';
 
 class Thead extends Component {
 
@@ -16,10 +17,15 @@ class Thead extends Component {
 
     render() {
 
-        const {className, style, columns, sort, sortAscIconCls, sortDescIconCls, hidden, onSort} = this.props;
+        const {className, style, columns, sort, sortAscIconCls, sortDescIconCls, hidden, onSort} = this.props,
+
+            theadClassName = classNames('thead', {
+                hidden: hidden,
+                [className]: className
+            });
 
         return (
-            <thead className={'thead' + (hidden ? ' hidden' : '') + (className ? ' ' + className : '')}
+            <thead className={theadClassName}
                    style={style}>
                 <tr>
                     {
