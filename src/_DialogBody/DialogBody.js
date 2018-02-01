941b127e8204f8905e6123599d517652b4da9080
Liangxiaojun <fatalxiao@163.com>
2018-02-01 13:21:59 +0800

    update DialogBody

--- src/_DialogBody/DialogBody.js
+++ src/_DialogBody/DialogBody.js
@@ -6,6 +6,7 @@
 import React, {Component, Children, cloneElement} from 'react';
 import {findDOMNode} from 'react-dom';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import Paper from '../Paper';
 import FlatButton from '../FlatButton';
@@ -185,22 +186,29 @@ class DialogBody extends Component {
             } = this.props,
             {visible} = this.state,
 
-            modalClass = (visible ? '' : ' hidden') + (modalClassName ? ' ' + modalClassName : ''),
-            dialogClassName = (visible ? '' : ' hidden') + (className ? ' ' + className : '');
+            modalClass = classNames('dialog-modal', {
+                hidden: !visible,
+                [modalClassName]: modalClassName
+            }),
+
+            dialogClassName = classNames('dialog-wrapper', {
+                hidden: !visible,
+                [className]: className
+            });
 
         return (
             <div>
 
                 {
                     showModal ?
-                        <div className={'dialog-modal' + modalClass}></div>
+                        <div className={modalClass}></div>
                         :
                         null
                 }
 
                 <Paper {...restProps}
                        ref="dialog"
-                       className={'dialog-wrapper' + dialogClassName}
+                       className={dialogClassName}
                        depth={6}>
 
                     <div className="dialog-title">
