8c3e15057ff1fd01433cec17758b7c309510ea1c
Liangxiaojun <fatalxiao@163.com>
2018-02-03 14:39:47 +0800

    update gulp-component-prop-type-json

--- scripts/gulp-component-prop-type-json.js
+++ scripts/gulp-component-prop-type-json.js
@@ -6,7 +6,7 @@ const gutil = require('gulp-util'),
     generateComponentPropTypeJson = require('./generateComponentPropTypeJson');
 
 module.exports = () => {
-    return through.obj((chunk, encoding, callback) => {
+    return through.obj(function (chunk, encoding, callback) {
 
         if (chunk.isNull()) {
             callback(null, chunk);
