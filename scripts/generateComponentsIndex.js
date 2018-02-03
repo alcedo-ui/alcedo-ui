447bf3a8a9d33f6a83309f6f16c8ad66a6f4dd55
Liangxiaojun <fatalxiao@163.com>
2018-02-03 14:37:51 +0800

    update generateComponentsIndex

--- scripts/generateComponentsIndex.js
+++ scripts/generateComponentsIndex.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var os = require('os'),
+const os = require('os'),
     fs = require('fs'),
     path = require('path');
 
@@ -12,7 +12,7 @@ function isDirectory(path) {
     }
 }
 
-fs.readdir('./src', function (err, files) {
+fs.readdir('./src', (err, files) => {
 
     if (err) {
         console.log('read src error');
@@ -23,15 +23,15 @@ fs.readdir('./src', function (err, files) {
             let indexArray = [],
                 totalCount = 0;
 
-            files.forEach(function (item) {
-                if (item.slice(0, 1) !== '_' && isDirectory('./src/' + item)) {
-                    indexArray.push('export ' + item + ' from \'./' + item + '\';');
+            files.forEach(item => {
+                if (item.slice(0, 1) !== '_' && isDirectory(`./src/${item}`)) {
+                    indexArray.push(`export ${item} from \'./${item}\';`);
                     totalCount++;
                 }
             });
             indexArray.push('', `export const COMPONENTS_TOTAL_COUNT = ${totalCount - 1};`);
 
-            fs.writeFile(path.join(__dirname, '../src/index.js'), indexArray.join(os.EOL), function (error) {
+            fs.writeFile(path.join(__dirname, '../src/index.js'), indexArray.join(os.EOL), error => {
                 if (error) {
                     throw error;
                 }
