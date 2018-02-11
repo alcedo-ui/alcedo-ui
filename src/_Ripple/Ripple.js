9218cc0d881ee5bd62e79f01ebe9871809f79b1e
Liangxiaojun <fatalxiao@163.com>
2018-02-11 15:08:58 +0800

    update Ripple

--- src/_Ripple/Ripple.js
+++ src/_Ripple/Ripple.js
@@ -5,57 +5,25 @@
 
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 class Ripple extends Component {
 
     constructor(props, ...restArgs) {
-
         super(props, ...restArgs);
-
-        this.state = {
-            visible: false,
-            leave: false
-        };
-
-        this.exithandler = ::this.exithandler;
-
-    }
-
-    exithandler() {
-        this.setState({
-            leave: true
-        }, () => {
-            // debugger;
-        });
-    }
-
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                visible: true
-            });
-        });
     }
 
     render() {
 
-        const {style, duration, ...restProps} = this.props,
-            {visible, leave} = this.state,
-
-            wrapperClassName = classNames('ripple', {
-                visible,
-                leave
-            });
+        const {style, duration, ...restProps} = this.props;
 
         return (
-            <Transition {...restProps}
-                        timeout={{enter: duration * 2, exit: duration * 4}}
-                        onExit={this.exithandler}>
-                <div className={wrapperClassName}
+            <CSSTransition {...restProps}
+                           classNames="ripple"
+                           timeout={{enter: duration, exit: duration * 2}}>
+                <div className="ripple"
                      style={style}></div>
-            </Transition>
+            </CSSTransition>
         );
 
     }
@@ -73,7 +41,7 @@ Ripple.defaultProps = {
 
     style: null,
 
-    duration: 250
+    duration: 500
 
 };
 
