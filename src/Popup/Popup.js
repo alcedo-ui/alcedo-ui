d9aa1955aadc638d50ac5ddf5a090ab696f6e4bd
Liangxiaojun <fatalxiao@163.com>
2018-02-14 11:15:04 +0800

    update Popup

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -4,6 +4,7 @@
  */
 
 import React, {Component} from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import Portal from '../Portal';
 import PopupBody from '../_PopupBody';
@@ -19,21 +20,48 @@ class Popup extends Component {
     static Theme = Theme;
 
     constructor(props, ...restArgs) {
+
         super(props, ...restArgs);
+
+        this.state = {
+            exited: true
+        };
+
+        this.exitedHandler = ::this.exitedHandler;
+
     }
 
     reDraw() {
         this.forceUpdate();
     }
 
+    exitedHandler() {
+        this.setState({
+            exited: true
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.visible) {
+            this.setState({
+                exited: false
+            });
+        }
+    }
+
     render() {
 
-        const {visible} = this.props;
+        const {visible} = this.props,
+            {exited} = this.state;
 
         return (
-            <Portal visible={visible}>
-                <PopupBody {...this.props}/>
-            </Portal>
+            <Transition in={visible}
+                        timeout={250}
+                        onExited={this.exitedHandler}>
+                <Portal visible={!exited}>
+                    <PopupBody {...this.props}/>
+                </Portal>
+            </Transition>
         );
 
     }
