6043e8c508c6301d84286b907061982ad3e58f2b
Liangxiaojun <fatalxiao@163.com>
2018-02-15 12:47:07 +0800

    update prod build

--- build/prod/prod.js
+++ build/prod/prod.js
@@ -4,45 +4,28 @@ process.env.NODE_ENV = '"production"';
 
 const ora = require('ora'),
     chalk = require('chalk'),
-    rm = require('rimraf'),
-    mkdirp = require('mkdirp'),
     webpack = require('webpack'),
-    config = require('../../config/index'),
     webpackConfig = require('./webpack.config.prod.js'),
     spinner = ora('building for production...');
 
 spinner.start();
 
-mkdirp(config.build.assetsSubDirectory, err => {
+webpack(webpackConfig, (err, stats) => {
+
+    spinner.stop();
 
     if (err) {
         throw err;
     }
 
-    webpack(webpackConfig, (err, stats) => {
+    process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+    }) + '\n\n');
 
-        spinner.stop();
-
-        if (err) {
-            throw err;
-        }
-
-        process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false,
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n');
-
-        console.log(chalk.cyan('Build complete.'));
-
-        rm(config.build.assetsSubDirectory, err => {
-            if (err) {
-                throw err;
-            }
-        });
-
-    });
+    console.log(chalk.cyan('Build complete.'));
 
 });
