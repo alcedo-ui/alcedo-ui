dba7d4ddd77701e5a06112944718545842a141d0
Liangxiaojun <fatalxiao@163.com>
2018-02-17 14:01:52 +0800

    update Popup

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -4,22 +4,19 @@
  */
 
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {findDOMNode} from 'react-dom';
 import Transition from 'react-transition-group/Transition';
 import classNames from 'classnames';
 
-import Portal from '../Portal';
-import Paper from '../Paper';
+import BasePopup from '../_BasePopup';
 import Theme from '../Theme';
 
 import Position from '../_statics/Position';
 import TriggerMode from '../_statics/TriggerMode';
 import Event from '../_vendors/Event';
-import PopupCalculation from '../_vendors/PopupCalculation';
-import PopupManagement from '../_vendors/PopupManagement';
 import Dom from '../_vendors/Dom';
 import Util from '../_vendors/Util';
-import PropTypes from 'prop-types';
 
 class Popup extends Component {
 
@@ -31,42 +28,8 @@ class Popup extends Component {
 
         super(props, ...restArgs);
 
-        this.state = {
-            enter: false,
-            exited: true
-        };
-
-        this.enterHandler = ::this.enterHandler;
-        this.exitHandler = ::this.exitHandler;
-        this.exitedHandler = ::this.exitedHandler;
         this.mouseDownHandler = ::this.mouseDownHandler;
-        this.resizeHandler = ::this.resizeHandler;
 
-    }
-
-    enterHandler(el) {
-
-        this.transitionEl = el;
-
-        const {triggerEl, position, isTriggerPositionFixed} = this.props;
-        PopupCalculation.setStyle(triggerEl, el, position, isTriggerPositionFixed);
-
-        this.setState({
-            enter: true
-        });
-
-    }
-
-    exitHandler() {
-        this.setState({
-            enter: false
-        });
-    }
-
-    exitedHandler() {
-        this.setState({
-            exited: true
-        });
     }
 
     triggerHandler(el, triggerEl, popupEl, triggerMode, currentVisible, isAutoClose) {
@@ -102,102 +65,22 @@ class Popup extends Component {
 
     }
 
-    resizeHandler = _.debounce(() => {
-        const {triggerEl, position, isTriggerPositionFixed} = this.props;
-        PopupCalculation.setStyle(triggerEl, this.transitionEl, position, isTriggerPositionFixed);
-    }, 250);
-
     componentDidMount() {
 
-        this.popupEl = findDOMNode(this.refs.popup);
+        // this.popupEl = findDOMNode(this.refs.popup);
 
-        Event.addEvent(document, 'mousedown', this.mouseDownHandler);
-        Event.addEvent(window, 'resize', this.resizeHandler);
-
-        this.props.isEscClose && PopupManagement.push(this);
+        // Event.addEvent(document, 'mousedown', this.mouseDownHandler);
 
     }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.visible) {
-            this.setState({
-                exited: !nextProps.visible
-            });
-        }
-    }
-
-    // componentDidUpdate() {
-    //     const {visible, onRender} = this.props;
-    //     visible && onRender && onRender(this.popupEl, this.props.triggerEl);
-    // }
 
     componentWillUnmount() {
-        Event.removeEvent(document, 'mousedown', this.mouseDownHandler);
-        Event.removeEvent(window, 'resize', this.resizeHandler);
+        // Event.removeEvent(document, 'mousedown', this.mouseDownHandler);
     }
 
     render() {
-
-        const {
-
-                children,
-
-                className, style, theme, hasTriangle, triangle, position, isAnimated, visible,
-
-                // not passing down these props
-                isEscClose, isAutoClose, shouldPreventContainerScroll, triggerMode, triggerEl, isTriggerPositionFixed,
-                onRender, onRequestClose, triggerHandler,
-
-                ...restProps
-
-            } = this.props,
-            {enter, exited} = this.state,
-
-            popupClassName = classNames('popup', {
-                hidden: !enter,
-                'popup-has-triangle': hasTriangle,
-                [`theme-${theme}`]: theme,
-                [`popup-position-${position}`]: position,
-                'popup-animated': isAnimated,
-                [className]: className
-            });
-
         return (
-            <Portal visible={!exited}>
-                <Transition appear
-                            in={visible}
-                            timeout={250}
-                            onEnter={this.enterHandler}
-                            onExit={this.exitHandler}
-                            onExited={this.exitedHandler}>
-                    <Paper {...restProps}
-                           className={popupClassName}
-                           style={style}
-                           onWheel={e => {
-                               Event.wheelHandler(e, this.props);
-                           }}>
-
-                        {
-                            hasTriangle ?
-                                <div className="popup-triangle-wrapper">
-                                    {triangle}
-                                </div>
-                                :
-                                null
-                        }
-
-                        <div className="popup-content"
-                             onWheel={e => {
-                                 Event.wheelHandler(e, this.props);
-                             }}>
-                            {children}
-                        </div>
-
-                    </Paper>
-                </Transition>
-            </Portal>
+            <BasePopup {...this.props}/>
         );
-
     }
 
 };
@@ -292,7 +175,6 @@ Popup.defaultProps = {
     triggerEl: null,
     visible: false,
     hasTriangle: true,
-    triangle: <div className="popup-triangle"></div>,
     theme: Theme.DEFAULT,
     position: Position.BOTTOM_LEFT,
     isAnimated: true,
