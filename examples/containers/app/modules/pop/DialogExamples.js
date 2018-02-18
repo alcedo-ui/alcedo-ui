a6d90c36ecccc3b7c829233ba22ead7ac5a1a645
Liangxiaojun <fatalxiao@163.com>
2018-02-18 13:32:32 +0800

    update Dialog examples

--- examples/containers/app/modules/pop/DialogExamples.js
+++ examples/containers/app/modules/pop/DialogExamples.js
@@ -8,7 +8,7 @@ import WidgetHeader from 'src/WidgetHeader';
 import PropTypeDescTable from 'components/PropTypeDescTable';
 import doc from 'assets/propTypes/DialogBody.json';
 
-import 'sass/containers/app/modules/popup/DialogExamples.scss';
+import 'sass/containers/app/modules/pop/DialogExamples.scss';
 
 export default class DialogExamples extends Component {
 
