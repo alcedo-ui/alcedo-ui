98bbc9ca14b31431efc406fc1e5db7d8c4c5c05f
Liangxiaojun <fatalxiao@163.com>
2018-03-02 16:19:18 +0800

    optimize GhostButton

--- src/GhostButton/GhostButton.js
+++ src/GhostButton/GhostButton.js
@@ -38,14 +38,14 @@ class GhostButton extends Component {
 
     render() {
 
-        const {children, className} = this.props,
+        const {children, className, ...restProps} = this.props,
 
             buttonClassName = classNames('ghost-button', {
                 [className]: className
             });
 
         return (
-            <BaseButton {...this.props}
+            <BaseButton {...restProps}
                         ref="baseButton"
                         className={buttonClassName}>
                 {children}
