d30ae89931b5cc0db9544985cb98eca9048871e6
Liangxiaojun <fatalxiao@163.com>
2018-03-07 16:16:21 +0800

    update MaterialTextField

--- src/MaterialTextField/MaterialTextField.js
+++ src/MaterialTextField/MaterialTextField.js
@@ -50,7 +50,7 @@ class MaterialTextField extends Component {
     render() {
 
         const {
-                className, style, theme, label, isLabelAnimate, required,
+                className, style, theme, label, isLabelAnimate, disabled, required,
                 ...restProps
             } = this.props,
             {value} = this.state,
@@ -66,11 +66,13 @@ class MaterialTextField extends Component {
                               label={label}
                               isLabelAnimate={isLabelAnimate}
                               hasValue={!!value}
+                              disabled={disabled}
                               required={required}>
 
                 <TextField {...restProps}
                            theme={theme}
                            value={value}
+                           disabled={disabled}
                            required={required}
                            onChange={this.triggerChangeHandler}/>
 
