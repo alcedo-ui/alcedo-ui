76483256e0eb61d3464de83716fa7bda7bea2e37
Liangxiaojun <fatalxiao@163.com>
2018-03-07 16:21:47 +0800

    update MaterialTextField examples

--- examples/containers/app/modules/fields/MaterialTextFieldExamples.js
+++ examples/containers/app/modules/fields/MaterialTextFieldExamples.js
@@ -79,6 +79,25 @@ export default class MaterialTextFieldExamples extends Component {
 
                 </Widget>
 
+                <Widget>
+
+                    <WidgetHeader className="example-header"
+                                  title="Disabled"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+                            <div className="examples-wrapper">
+                                <div className="field-group">
+                                    <MaterialTextField label="Username"
+                                                       value="John"
+                                                       disabled={true}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </Widget>
+
                 <h2 className="example-title">Properties</h2>
 
                 <PropTypeDescTable data={doc}/>
