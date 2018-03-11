f6406cf7b86889d2deb04820a6a856ba8876f5df
Liangxiaojun <fatalxiao@163.com>
2018-03-11 22:57:41 +0800

    update FieldMsg

--- src/FieldMsg/FieldMsg.js
+++ src/FieldMsg/FieldMsg.js
@@ -7,143 +7,41 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import Popover from '../Popover';
+
 import Util from '../_vendors/Util';
+import MsgType from '../_statics/MsgType';
+import Position from '../_statics/Position';
 
 class FieldMsg extends Component {
 
-    static Type = {
-        DEFAULT: '',
-        INFO: 'info',
-        SUCCESS: 'success',
-        WARNING: 'warning',
-        ERROR: 'error'
-    };
+    static Type = MsgTyPropTypespe;
+    static Position = Position;
 
     constructor(props, ...restArgs) {
-
         super(props, ...restArgs);
-
-        this.MAX_WIDTH = 460;
-
-        this.charCount = 100;
-        this.charSize = {};
-
-        this.state = {
-            msg: '',
-            multiLine: false,
-            width: 0
-        };
-
-        this.getCharSize = ::this.getCharSize;
-        this.getStringSize = ::this.getStringSize;
-
-    }
-
-    /**
-     * 获取单个字符的宽度
-     * @param char
-     * @returns {number}
-     */
-    getCharSize(char) {
-
-        if (char in this.charSize) {
-            return this.charSize[char];
-        }
-
-        const component = this.refs.testCharWidthDiv;
-        component.innerHTML = char.repeat(this.charCount);
-
-        return this.charSize[char] = component.getBoundingClientRect().width / this.charCount;
-
-    }
-
-    /**
-     * 获取字符串的宽度
-     * @param string
-     * @returns {number}
-     */
-    getStringSize(string) {
-        let width = 0;
-        if (string) {
-            for (let i = 0, len = string.length; i < len; i++) {
-                width += this.getCharSize(string.charAt(i));
-            }
-        }
-        return width;
-    }
-
-    componentDidMount() {
-        const width = this.getStringSize(this.props.msg);
-        if (width > this.MAX_WIDTH) {
-            this.setState({
-                msg: this.props.msg,
-                multiLine: true,
-                width
-            });
-        } else {
-            this.setState({
-                msg: this.props.msg,
-                multiLine: false,
-                width
-            });
-        }
     }
 
     render() {
 
-        const {type, children, className, style, isHtmlMsg} = this.props,
-            {msg, multiLine, width} = this.state,
-            {MAX_WIDTH} = this,
-
-            lineCount = Math.ceil(width / MAX_WIDTH),
-            msgHeight = multiLine ? (lineCount * 24 + 6) : 'auto',
+        const {children, className, style, type, visible, position, msg} = this.props,
 
             wrapperClassName = classNames('field-msg', {
-                [`theme-${type}`]: type,
                 [className]: className
-            }),
-
-            msgWrapClassName = classNames('msg-wrap', {
-                'multi-line': multiLine
-            }),
-            msgWrapStyle = {
-                width: multiLine ? MAX_WIDTH : 'auto',
-                height: isHtmlMsg ? 'auto' : msgHeight,
-                top: type == 'info' ? (multiLine ? 30 - msgHeight : 0) : 6
-            },
-
-            msgStyle = {
-                width: multiLine ? MAX_WIDTH : 'auto',
-                height: isHtmlMsg ? 'auto' : msgHeight
-            };
+            });
 
         return (
-            <div className={wrapperClassName}
-                 style={style}>
+            <Popover className={wrapperClassName}
+                     style={style}
+                     theme={type}
+                     visible={visible}
+                     position={position}>
 
-                <div className="triangle"></div>
+                {msg}
 
-                <div className={msgWrapClassName}
-                     style={msgWrapStyle}>
+                {children}
 
-                    <div className="msg"
-                         style={msgStyle}>
-                        {
-                            isHtmlMsg ?
-                                <div dangerouslySetInnerHTML={{__html: msg}}></div>
-                                :
-                                msg
-                        }
-                    </div>
-
-                    {children}
-
-                </div>
-
-                <div ref="testCharWidthDiv"
-                     className="test-char-width-div"></div>
-
-            </div>
+            </Popover>
         );
 
     }
@@ -154,21 +52,23 @@ FieldMsg.propTypes = {
 
     className: PropTypes.string,
     style: PropTypes.object,
+    type: PropTypes.oneOf(Util.enumerateValue(MsgType)),
 
-    type: PropTypes.oneOf(Util.enumerateValue(FieldMsg.Type)),
-    msg: PropTypes.any,
-    isHtmlMsg: PropTypes.bool
+    visible: Position.bool,
+    position: PropTypes.oneOf(Util.enumerateValue(Position)),
+    msg: PropTypes.any
 
 };
 
 FieldMsg.defaultProps = {
 
-    className: '',
+    className: null,
     style: null,
+    type: MsgType.DEFAULT,
 
-    type: FieldMsg.Type.DEFAULT,
-    msg: '',
-    isHtmlMsg: false
+    visible: false,
+    position: Position.BOTTOM_LEFT,
+    msg: null
 
 };
 
