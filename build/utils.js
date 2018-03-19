fda778d64d2c909982051a841079fd69a87b2f54
Liangxiaojun <fatalxiao@163.com>
2018-03-19 14:31:44 +0800

    update build

--- build/utils.js
+++ build/utils.js
@@ -1,6 +1,10 @@
 const path = require('path'),
     config = require('../config');
 
+exports.assetsPath = _path => {
+    return path.posix.join(config.assetsDirectory, _path);
+};
+
 exports.assetsSubPath = _path => {
     return path.posix.join(config.assetsSubDirectory, _path);
 };
