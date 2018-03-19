0db6f607dc63cb38b727f447f5ae9eac8888dae4
Liangxiaojun <fatalxiao@163.com>
2018-03-19 12:40:52 +0800

    update webpack

--- build/prod/webpack.config.prod.js
+++ build/prod/webpack.config.prod.js
@@ -33,6 +33,31 @@ module.exports = merge(baseWebpackConfig, {
         chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
     },
 
+    // optimization: {
+    //     // runtimeChunk: {
+    //     //     name: 'manifest'
+    //     // },
+    //     splitChunks: {
+    //         chunks: 'all'
+    //         // minSize: 30000,
+    //         // minChunks: 1,
+    //         // maxAsyncRequests: 5,
+    //         // maxInitialRequests: 3,
+    //         // name: true,
+    //         // cacheGroups: {
+    //         //     default: {
+    //         //         minChunks: 2,
+    //         //         priority: -20,
+    //         //         reuseExistingChunk: true
+    //         //     },
+    //         //     vendors: {
+    //         //         test: /[\\/]node_modules[\\/]/,
+    //         //         priority: -10
+    //         //     }
+    //         // }
+    //     }
+    // },
+
     plugins: [
 
         new webpack.DefinePlugin({
@@ -42,6 +67,7 @@ module.exports = merge(baseWebpackConfig, {
         // new UglifyJSPlugin(),
 
         new ExtractTextPlugin({
+            allChunks: true,
             filename: utils.assetsPath('css/[name].[contenthash].css')
         }),
 
