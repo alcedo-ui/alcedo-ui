c56a8649ae74704aa68538bcc3fc8559f54fcd19
Liangxiaojun <fatalxiao@163.com>
2018-03-23 16:18:17 +0800

    update build

--- build/demo/demo-server.js
+++ build/demo/demo-server.js
@@ -2,19 +2,23 @@ const express = require('express'),
     history = require('connect-history-api-fallback'),
     opn = require('opn'),
     compression = require('compression'),
+    moment = require('moment'),
 
     config = require('../config.js'),
 
     app = express(),
     uri = 'http://localhost:' + config.demo.port;
 
-app.use(compression());
-
-app.use(history());
-
-app.use(express.static(config.build.assetsRoot));
-
-app.listen(config.demo.port, err => {
+app
+.use(compression())
+.use(history())
+.use(express.static(config.build.assetsRoot, {
+    setHeaders: res => {
+        res.setHeader('Cache-Control', 'max-age=2592000');
+        res.setHeader('Expires', `${moment().add(1, 'months').utc().format('ddd, DD MMM YYYY HH:mm:ss')} GTM`);
+    }
+}))
+.listen(config.demo.port, err => {
 
     if (err) {
         return console.log(err);
