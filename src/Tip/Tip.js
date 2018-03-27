4ee88fbb8ff46fb92014081e37b1a3ed0367d7da
Liangxiaojun <fatalxiao@163.com>
2018-03-27 16:20:31 +0800

    update Tip

--- src/Tip/Tip.js
+++ src/Tip/Tip.js
@@ -54,10 +54,17 @@ Tip.propTypes = {
      */
     contentClassName: PropTypes.string,
 
+    modalClassName: PropTypes.string,
+
     /**
      * Override the styles of the root element.
      */
     style: PropTypes.object,
+
+    /**
+     * The popover theme.Can be primary,highlight,success,warning,error.
+     */
+    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
 
     /**
      * This is the DOM element that will be used to set the position of the popover.
@@ -77,11 +84,6 @@ Tip.propTypes = {
     triangle: PropTypes.element,
 
     /**
-     * The popover theme.Can be primary,highlight,success,warning,error.
-     */
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-    /**
      * The popover alignment.
      */
     position: PropTypes.oneOf(Util.enumerateValue(Position)),
@@ -99,6 +101,7 @@ Tip.propTypes = {
     depth: PropTypes.number,
 
     isTriggerPositionFixed: PropTypes.bool,
+    showModal: PropTypes.bool,
 
     /**
      * The function of tip render.
@@ -131,17 +134,19 @@ Tip.defaultProps = {
 
     className: null,
     contentClassName: null,
+    modalClassName: null,
     style: null,
     theme: Theme.DARK,
+    depth: 6,
 
     triggerEl: null,
     visible: false,
     hasTriangle: true,
     position: Position.BOTTOM,
     isAnimated: true,
-    depth: 6,
     shouldPreventContainerScroll: true,
-    isTriggerPositionFixed: false
+    isTriggerPositionFixed: false,
+    showModal: false
 
 };
 
