22ed4c1964c046550c6653f16ba4c49b27a7ffbb
Liangxiaojun <fatalxiao@163.com>
2018-03-29 14:22:18 +0800

    update PureRender

--- src/_vendors/PureRender.js
+++ src/_vendors/PureRender.js
@@ -3,10 +3,45 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
-import isEqual from 'lodash/isEqual';
+function isObject(value) {
+    return value != null && typeof value === 'object';
+}
+
+function equal(x, y) {
+
+    if (x === y) {
+        return true;
+    }
+
+    if (x == null || y == null || (!isObject(x) && !isObject(y))) {
+        return x !== x && y !== y;
+    }
+
+    const xProps = Object.keys(x),
+        yProps = Object.keys(y),
+        xLen = xProps.length,
+        yLen = yProps.length;
+
+    if (xLen !== yLen) {
+        return false;
+    }
+
+    for (let prop of xProps) {
+        if (hasOwnProperty.call(y, prop)) {
+            if (!equal(x[prop], y[prop])) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+
+}
 
 function shouldComponentUpdate(nextProps, nextState) {
-    return !isEqual(this.props, nextProps) || !isEqual(this.state, nextState);
+    return !equal(this.props, nextProps) || !equal(this.state, nextState);
 }
 
 function PureRender(component) {
