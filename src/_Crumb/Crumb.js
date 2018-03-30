6d6dd1e27175fa2920a8d132aa288ce6cecca172
Liangxiaojun <fatalxiao@163.com>
2018-03-30 20:08:32 +0800

    update Crumb

--- src/_Crumb/Crumb.js
+++ src/_Crumb/Crumb.js
@@ -4,7 +4,6 @@
  */
 
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import FlatButton from '../FlatButton';
@@ -42,94 +41,100 @@ class Crumb extends Component {
     }
 };
 
-process.env.NODE_ENV !== 'production' && (Crumb.propTypes = {
+if (process.env.NODE_ENV === 'development') {
 
-    /**
-     * The CSS class name of the list button.
-     */
-    className: PropTypes.string,
+    const PropTypes = require('prop-types');
 
-    /**
-     * Override the styles of the list button.
-     */
-    style: PropTypes.object,
+    Crumb.propTypes = {
 
-    /**
-     * The theme of the list button.
-     */
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+        /**
+         * The CSS class name of the list button.
+         */
+        className: PropTypes.string,
 
-    /**
-     * The url of crumb.
-     */
-    href: PropTypes.string,
+        /**
+         * Override the styles of the list button.
+         */
+        style: PropTypes.object,
 
-    /**
-     * The text value of the list button.Type can be string or number.
-     */
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        /**
+         * The theme of the list button.
+         */
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
 
-    /**
-     * The list item's display text. Type can be string, number or bool.
-     */
-    text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        /**
+         * The url of crumb.
+         */
+        href: PropTypes.string,
 
-    /**
-     * The desc value of the list button. Type can be string or number.
-     */
-    desc: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        /**
+         * The text value of the list button.Type can be string or number.
+         */
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 
-    /**
-     * If true,the list item will be disabled.
-     */
-    disabled: PropTypes.bool,
+        /**
+         * The list item's display text. Type can be string, number or bool.
+         */
+        text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 
-    /**
-     * If true,the button will be have loading effect.
-     */
-    isLoading: PropTypes.bool,
+        /**
+         * The desc value of the list button. Type can be string or number.
+         */
+        desc: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 
-    /**
-     * If true,the element's ripple effect will be disabled.
-     */
-    disableTouchRipple: PropTypes.bool,
+        /**
+         * If true,the list item will be disabled.
+         */
+        disabled: PropTypes.bool,
 
-    /**
-     * Use this property to display an icon. It will display on the left.
-     */
-    iconCls: PropTypes.string,
+        /**
+         * If true,the button will be have loading effect.
+         */
+        isLoading: PropTypes.bool,
 
-    /**
-     * Use this property to display an icon. It will display on the right.
-     */
-    rightIconCls: PropTypes.string,
+        /**
+         * If true,the element's ripple effect will be disabled.
+         */
+        disableTouchRipple: PropTypes.bool,
 
-    /**
-     * The message of tip.
-     */
-    tip: PropTypes.string,
+        /**
+         * Use this property to display an icon. It will display on the left.
+         */
+        iconCls: PropTypes.string,
 
-    /**
-     * The position of tip.
-     */
-    tipPosition: PropTypes.oneOf(Util.enumerateValue(Tip.Position)),
+        /**
+         * Use this property to display an icon. It will display on the right.
+         */
+        rightIconCls: PropTypes.string,
 
-    /**
-     * If true,the item will have center displayed ripple effect.
-     */
-    rippleDisplayCenter: PropTypes.bool,
+        /**
+         * The message of tip.
+         */
+        tip: PropTypes.string,
 
-    /**
-     * You can create a complicated renderer callback instead of value and desc prop.
-     */
-    itemRenderer: PropTypes.func,
+        /**
+         * The position of tip.
+         */
+        tipPosition: PropTypes.oneOf(Util.enumerateValue(Tip.Position)),
 
-    /**
-     * Callback function fired when a list item touch-tapped.
-     */
-    onTouchTap: PropTypes.func
+        /**
+         * If true,the item will have center displayed ripple effect.
+         */
+        rippleDisplayCenter: PropTypes.bool,
 
-});
+        /**
+         * You can create a complicated renderer callback instead of value and desc prop.
+         */
+        itemRenderer: PropTypes.func,
+
+        /**
+         * Callback function fired when a list item touch-tapped.
+         */
+        onTouchTap: PropTypes.func
+
+    };
+
+}
 
 Crumb.defaultProps = {
     theme: Theme.DEFAULT
