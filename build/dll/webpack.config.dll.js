d50c7fe81a08676f10f23358da78afba99052e16
Liangxiaojun <fatalxiao@163.com>
2018-03-30 19:48:37 +0800

    update webpack.config.dll

--- build/dll/webpack.config.dll.js
+++ build/dll/webpack.config.dll.js
@@ -13,9 +13,8 @@ module.exports = {
 
     entry: {
         'polyfill': ['babel-polyfill'],
-        'react': ['prop-types', 'react', 'react-dom', 'react-redux', 'react-router',
-            'react-router-config', 'react-router-dom', 'react-router-redux', 'redux',
-            'redux-thunk', 'react-tap-event-plugin', 'react-transition-group'],
+        'react': ['react', 'react-dom', 'react-redux', 'react-router', 'react-router-config', 'react-router-dom',
+            'react-router-redux', 'redux', 'redux-thunk', 'react-tap-event-plugin', 'react-transition-group'],
         'tools': ['classnames', 'history', 'moment']
     },
 
