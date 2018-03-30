21cda133b6378c8616854b93c870b8e624d07b38
Liangxiaojun <fatalxiao@163.com>
2018-03-30 19:17:54 +0800

    update DraggableListItem

--- src/_DraggableListItem/_DraggableListItem.js
+++ src/_DraggableListItem/_DraggableListItem.js
@@ -14,10 +14,12 @@ import CircularLoading from '../CircularLoading';
 import TipProvider from '../TipProvider';
 import Theme from '../Theme';
 
+import PureRender from '../_vendors/PureRender';
 import Util from '../_vendors/Util';
 import Position from '../_statics/Position';
 import SelectMode from '../_statics/SelectMode';
 
+@PureRender
 class DraggableListItem extends Component {
 
     static SelectMode = SelectMode;
@@ -246,7 +248,7 @@ class DraggableListItem extends Component {
     }
 };
 
-DraggableListItem.propTypes = {
+process.env.NODE_ENV !== 'production' && (DraggableListItem.propTypes = {
 
     index: PropTypes.number,
 
@@ -290,37 +292,24 @@ DraggableListItem.propTypes = {
     onMouseEnter: PropTypes.func,
     onMouseLeave: PropTypes.func
 
-};
+});
 
 DraggableListItem.defaultProps = {
 
     index: 0,
 
-    className: null,
-    style: null,
     theme: Theme.DEFAULT,
 
     selectTheme: Theme.DEFAULT,
     selectMode: SelectMode.SINGLE_SELECT,
-
-    data: null,
-    value: null,
-    text: null,
-    desc: null,
 
     disabled: false,
     isLoading: false,
     checked: false,
     readOnly: false,
 
-    iconCls: null,
-    rightIconCls: null,
-
-    tip: null,
     tipPosition: Position.BOTTOM,
 
-    radioUncheckedIconCls: null,
-    radioCheckedIconCls: null,
     checkboxUncheckedIconCls: 'far fa-square',
     checkboxCheckedIconCls: 'fas fa-check-square',
     checkboxIndeterminateIconCls: 'fas fa-minus-square',
