ab97bc5bdded78bad06426784c5d7248300e2534
Liangxiaojun <fatalxiao@163.com>
2018-04-01 23:53:52 +0800

    update DraggableGridItem

--- src/_DraggableGridItem/_DraggableGridItem.js
+++ src/_DraggableGridItem/_DraggableGridItem.js
@@ -4,7 +4,6 @@
  */
 
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {Draggable} from 'react-beautiful-dnd';
 import classNames from 'classnames';
 
@@ -256,53 +255,59 @@ class DraggableGridItem extends Component {
     }
 };
 
-process.env.NODE_ENV !== 'production' && (DraggableGridItem.propTypes = {
+if (process.env.NODE_ENV === 'development') {
 
-    index: PropTypes.number,
+    const PropTypes = require('prop-types');
 
-    className: PropTypes.string,
-    style: PropTypes.object,
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+    DraggableGridItem.propTypes = {
 
-    selectTheme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-    selectMode: PropTypes.oneOf(Util.enumerateValue(SelectMode)),
+        index: PropTypes.number,
 
-    data: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    text: PropTypes.any,
-    desc: PropTypes.string,
+        className: PropTypes.string,
+        style: PropTypes.object,
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
 
-    disabled: PropTypes.bool,
-    isLoading: PropTypes.bool,
-    checked: PropTypes.bool,
-    readOnly: PropTypes.bool,
+        selectTheme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+        selectMode: PropTypes.oneOf(Util.enumerateValue(SelectMode)),
 
-    iconCls: PropTypes.string,
-    rightIconCls: PropTypes.string,
+        data: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        text: PropTypes.any,
+        desc: PropTypes.string,
 
-    tip: PropTypes.string,
-    tipPosition: PropTypes.oneOf(Util.enumerateValue(Position)),
+        disabled: PropTypes.bool,
+        isLoading: PropTypes.bool,
+        checked: PropTypes.bool,
+        readOnly: PropTypes.bool,
 
-    radioUncheckedIconCls: PropTypes.string,
-    radioCheckedIconCls: PropTypes.string,
-    checkboxUncheckedIconCls: PropTypes.string,
-    checkboxCheckedIconCls: PropTypes.string,
-    checkboxIndeterminateIconCls: PropTypes.string,
+        iconCls: PropTypes.string,
+        rightIconCls: PropTypes.string,
 
-    anchorIconCls: PropTypes.string,
+        tip: PropTypes.string,
+        tipPosition: PropTypes.oneOf(Util.enumerateValue(Position)),
 
-    col: PropTypes.number,
+        radioUncheckedIconCls: PropTypes.string,
+        radioCheckedIconCls: PropTypes.string,
+        checkboxUncheckedIconCls: PropTypes.string,
+        checkboxCheckedIconCls: PropTypes.string,
+        checkboxIndeterminateIconCls: PropTypes.string,
 
-    itemRenderer: PropTypes.func,
-    renderer: PropTypes.func,
+        anchorIconCls: PropTypes.string,
 
-    onTouchTap: PropTypes.func,
-    onSelect: PropTypes.func,
-    onDeselect: PropTypes.func,
-    onMouseEnter: PropTypes.func,
-    onMouseLeave: PropTypes.func
+        col: PropTypes.number,
 
-});
+        itemRenderer: PropTypes.func,
+        renderer: PropTypes.func,
+
+        onTouchTap: PropTypes.func,
+        onSelect: PropTypes.func,
+        onDeselect: PropTypes.func,
+        onMouseEnter: PropTypes.func,
+        onMouseLeave: PropTypes.func
+
+    };
+
+}
 
 DraggableGridItem.defaultProps = {
 
