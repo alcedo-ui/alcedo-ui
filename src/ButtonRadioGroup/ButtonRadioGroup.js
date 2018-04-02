75c216ff8a482516d232fbfa3221db2b14a36614
Liangxiaojun <fatalxiao@163.com>
2018-04-02 14:38:24 +0800

    update ButtonRadioGroup

--- src/ButtonRadioGroup/ButtonRadioGroup.js
+++ src/ButtonRadioGroup/ButtonRadioGroup.js
@@ -4,14 +4,15 @@
  */
 
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import ButtonRadio from '../ButtonRadio';
 import Theme from '../Theme';
 
+import PureRender from '../_vendors/PureRender';
 import Util from '../_vendors/Util';
 
+@PureRender
 class ButtonRadioGroup extends Component {
 
     static Theme = Theme;
@@ -91,81 +92,87 @@ class ButtonRadioGroup extends Component {
     }
 };
 
-ButtonRadioGroup.propTypes = {
+if (process.env.NODE_ENV === 'development') {
 
-    /**
-     * The CSS class name of the root element.
-     */
-    className: PropTypes.string,
+    const PropTypes = require('prop-types');
 
-    /**
-     * Override the styles of the root element.
-     */
-    style: PropTypes.object,
-
-    /**
-     * The ButtonCheckbox theme.
-     */
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-    /**
-     * The ButtonCheckbox activated theme.
-     */
-    activatedTheme: PropTypes.oneOf(Util.enumerateValue(Theme)),
-
-    /**
-     * The hidden input name for form submit.
-     */
-    name: PropTypes.string,
-
-    /**
-     * Data for ButtonRadioGroup.
-     */
-    data: PropTypes.arrayOf(PropTypes.shape({
+    ButtonRadioGroup.propTypes = {
 
         /**
-         * The className of RaisedButton.
+         * The CSS class name of the root element.
          */
         className: PropTypes.string,
 
         /**
-         * The style of RaisedButton.
+         * Override the styles of the root element.
          */
         style: PropTypes.object,
 
         /**
-         * The label of RaisedButton.
+         * The ButtonCheckbox theme.
          */
-        label: PropTypes.any,
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
 
         /**
-         * The value of RaisedButton.
+         * The ButtonCheckbox activated theme.
+         */
+        activatedTheme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+        /**
+         * The hidden input name for form submit.
+         */
+        name: PropTypes.string,
+
+        /**
+         * Data for ButtonRadioGroup.
+         */
+        data: PropTypes.arrayOf(PropTypes.shape({
+
+            /**
+             * The className of RaisedButton.
+             */
+            className: PropTypes.string,
+
+            /**
+             * The style of RaisedButton.
+             */
+            style: PropTypes.object,
+
+            /**
+             * The label of RaisedButton.
+             */
+            label: PropTypes.any,
+
+            /**
+             * The value of RaisedButton.
+             */
+            value: PropTypes.any,
+
+            /**
+             * If true, the RaisedButton will be disabled.
+             */
+            disabled: PropTypes.bool
+
+        })).isRequired,
+
+        /**
+         * Set one of the button activation.
          */
         value: PropTypes.any,
 
         /**
-         * If true, the RaisedButton will be disabled.
+         * If true, the ButtonRadioGroup will be disabled.
          */
-        disabled: PropTypes.bool
+        disabled: PropTypes.bool,
 
-    })).isRequired,
+        /**
+         * Callback function fired when click RaisedButton.
+         */
+        onChange: PropTypes.func
 
-    /**
-     * Set one of the button activation.
-     */
-    value: PropTypes.any,
+    };
 
-    /**
-     * If true, the ButtonRadioGroup will be disabled.
-     */
-    disabled: PropTypes.bool,
-
-    /**
-     * Callback function fired when click RaisedButton.
-     */
-    onChange: PropTypes.func
-
-};
+}
 
 ButtonRadioGroup.defaultProps = {
 
