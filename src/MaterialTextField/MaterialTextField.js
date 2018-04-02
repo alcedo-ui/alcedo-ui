2b3e149a8b07e506d70a7e7c0e2e0b00811afc2c
Liangxiaojun <fatalxiao@163.com>
2018-04-02 16:57:16 +0800

    update MaterialTextField

--- src/MaterialTextField/MaterialTextField.js
+++ src/MaterialTextField/MaterialTextField.js
@@ -11,8 +11,10 @@ import TextField from '../TextField';
 import MaterialProvider from '../MaterialProvider';
 import Theme from '../Theme';
 
+import PureRender from '../_vendors/PureRender';
 import Util from '../_vendors/Util';
 
+@PureRender
 class MaterialTextField extends Component {
 
     static Type = TextField.Type;
@@ -80,199 +82,203 @@ class MaterialTextField extends Component {
     }
 };
 
-MaterialTextField.propTypes = {
+if (process.env.NODE_ENV === 'development') {
 
-    /**
-     * The CSS class name of the root element.
-     */
-    className: PropTypes.string,
+    const PropTypes = require('prop-types');
 
-    /**
-     * Override the styles of the root element.
-     */
-    style: PropTypes.object,
+    MaterialTextField.propTypes = {
 
-    /**
-     * The TextField theme.
-     */
-    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+        /**
+         * The CSS class name of the root element.
+         */
+        className: PropTypes.string,
 
-    /**
-     * Specifies the type of input to display such as "password" or "text".
-     */
-    type: PropTypes.oneOf(Util.enumerateValue(TextField.Type)),
+        /**
+         * Override the styles of the root element.
+         */
+        style: PropTypes.object,
 
-    /**
-     * The name of the text field.
-     */
-    name: PropTypes.string,
+        /**
+         * The TextField theme.
+         */
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
 
-    /**
-     * The label of the text field.
-     */
-    label: PropTypes.any,
+        /**
+         * Specifies the type of input to display such as "password" or "text".
+         */
+        type: PropTypes.oneOf(Util.enumerateValue(TextField.Type)),
 
-    /**
-     * The animate of the text field.
-     */
-    isLabelAnimate: PropTypes.bool,
+        /**
+         * The name of the text field.
+         */
+        name: PropTypes.string,
 
-    /**
-     * The placeholder of the text field.
-     */
-    placeholder: PropTypes.string,
+        /**
+         * The label of the text field.
+         */
+        label: PropTypes.any,
 
-    /**
-     * The value of the text field.Type can be string or number.
-     */
-    value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+        /**
+         * The animate of the text field.
+         */
+        isLabelAnimate: PropTypes.bool,
 
-    /**
-     * Use this property to display an icon.
-     */
-    iconCls: PropTypes.string,
+        /**
+         * The placeholder of the text field.
+         */
+        placeholder: PropTypes.string,
 
-    /**
-     * Disables the textField if set to true.
-     */
-    disabled: PropTypes.bool,
+        /**
+         * The value of the text field.Type can be string or number.
+         */
+        value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
-    /**
-     * If true,the textField will can't editable.
-     */
-    readOnly: PropTypes.bool,
+        /**
+         * Use this property to display an icon.
+         */
+        iconCls: PropTypes.string,
 
-    /**
-     * If true,the textField will autoFocus.
-     */
-    autoFocus: PropTypes.bool,
+        /**
+         * Disables the textField if set to true.
+         */
+        disabled: PropTypes.bool,
 
-    /**
-     * The message of the textField.
-     */
-    infoMsg: PropTypes.string,
+        /**
+         * If true,the textField will can't editable.
+         */
+        readOnly: PropTypes.bool,
 
-    /**
-     * If true,clearButton will display when the textField is not empty.
-     */
-    clearButtonVisible: PropTypes.bool,
+        /**
+         * If true,the textField will autoFocus.
+         */
+        autoFocus: PropTypes.bool,
 
-    /**
-     * Use this property to display an icon.
-     */
-    rightIconCls: PropTypes.string,
+        /**
+         * The message of the textField.
+         */
+        infoMsg: PropTypes.string,
 
-    /**
-     * If true,passwordButton will display.
-     */
-    passwordButtonVisible: PropTypes.bool,
+        /**
+         * If true,clearButton will display when the textField is not empty.
+         */
+        clearButtonVisible: PropTypes.bool,
+
+        /**
+         * Use this property to display an icon.
+         */
+        rightIconCls: PropTypes.string,
+
+        /**
+         * If true,passwordButton will display.
+         */
+        passwordButtonVisible: PropTypes.bool,
 
 
-    // valid
-    /**
-     * If true,the textField must be required.
-     */
-    required: PropTypes.bool,
+        // valid
+        /**
+         * If true,the textField must be required.
+         */
+        required: PropTypes.bool,
 
-    /**
-     * The maximum length of textField to enter.
-     */
-    maxLength: PropTypes.number,
+        /**
+         * The maximum length of textField to enter.
+         */
+        maxLength: PropTypes.number,
 
-    /**
-     * Maximum number of textField that can be entered.
-     */
-    max: PropTypes.number,
+        /**
+         * Maximum number of textField that can be entered.
+         */
+        max: PropTypes.number,
 
-    /**
-     * Minimum number of textField that can be entered.
-     */
-    min: PropTypes.number,
+        /**
+         * Minimum number of textField that can be entered.
+         */
+        min: PropTypes.number,
 
-    /**
-     * The matching rule of textField.
-     */
-    pattern: PropTypes.object,
+        /**
+         * The matching rule of textField.
+         */
+        pattern: PropTypes.object,
 
-    /**
-     * The invalid message of textField.
-     */
-    patternInvalidMsg: PropTypes.string,
+        /**
+         * The invalid message of textField.
+         */
+        patternInvalidMsg: PropTypes.string,
 
-    /**
-     *
-     */
-    autoComplete: PropTypes.string,
+        /**
+         *
+         */
+        autoComplete: PropTypes.string,
 
-    /**
-     *
-     */
-    autoCorrect: PropTypes.string,
+        /**
+         *
+         */
+        autoCorrect: PropTypes.string,
 
-    /**
-     *
-     */
-    autoCapitalize: PropTypes.string,
+        /**
+         *
+         */
+        autoCapitalize: PropTypes.string,
 
-    /**
-     *
-     */
-    spellCheck: PropTypes.string,
+        /**
+         *
+         */
+        spellCheck: PropTypes.string,
 
-    /**
-     * Callback function fired when the textField is changed.
-     */
-    onChange: PropTypes.func,
+        /**
+         * Callback function fired when the textField is changed.
+         */
+        onChange: PropTypes.func,
 
-    /**
-     * Callback function fired when the press enter.
-     */
-    onPressEnter: PropTypes.func,
+        /**
+         * Callback function fired when the press enter.
+         */
+        onPressEnter: PropTypes.func,
 
-    /**
-     * Callback function fired when textField valid.
-     */
-    onValid: PropTypes.func,
+        /**
+         * Callback function fired when textField valid.
+         */
+        onValid: PropTypes.func,
 
-    /**
-     * Callback function fired when textField invalid.
-     */
-    onInvalid: PropTypes.func,
+        /**
+         * Callback function fired when textField invalid.
+         */
+        onInvalid: PropTypes.func,
 
-    /**
-     * Callback function fired when textField focus.
-     */
-    onFocus: PropTypes.func,
+        /**
+         * Callback function fired when textField focus.
+         */
+        onFocus: PropTypes.func,
 
-    /**
-     * Callback function fired when textField blur.
-     */
-    onBlur: PropTypes.func,
+        /**
+         * Callback function fired when textField blur.
+         */
+        onBlur: PropTypes.func,
 
-    /**
-     * Callback function fired when textField clear.
-     */
-    onClear: PropTypes.func,
+        /**
+         * Callback function fired when textField clear.
+         */
+        onClear: PropTypes.func,
 
-    /**
-     * Callback function fired when password visible.
-     */
-    onPasswordVisible: PropTypes.func,
+        /**
+         * Callback function fired when password visible.
+         */
+        onPasswordVisible: PropTypes.func,
 
-    /**
-     * Callback function fired when password invisible.
-     */
-    onPasswordInvisible: PropTypes.func,
+        /**
+         * Callback function fired when password invisible.
+         */
+        onPasswordInvisible: PropTypes.func,
 
-    onMouseOver: PropTypes.func,
-    onMouseOut: PropTypes.func
+        onMouseOver: PropTypes.func,
+        onMouseOut: PropTypes.func
 
-};
+    };
+
+}
 
 MaterialTextField.defaultProps = {
 
-    className: null,
-    style: null,
     theme: Theme.DEFAULT,
 
     type: 'text',
