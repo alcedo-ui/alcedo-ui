117d27e90bdb6f0e6581fe445aaab69563cf947d
fatalxiao <fatalxiao@163.com>
2018-04-13 15:26:33 +0800

    update DraggableListItem

--- src/_DraggableListItem/DraggableListItem.js
+++ src/_DraggableListItem/DraggableListItem.js
@@ -39,7 +39,6 @@ class DraggableListItem extends Component {
         super(props, ...restArgs);
 
         this.state = {
-            checked: props.checked,
             tipVisible: false
         };
 
@@ -71,32 +70,24 @@ class DraggableListItem extends Component {
     }
 
     checkboxChangeHandler(checked) {
-        this.setState({
-            checked
-        }, () => {
 
-            const {onSelect, onDeselect} = this.props;
+        const {onSelect, onDeselect} = this.props;
 
-            if (checked) {
-                onSelect && onSelect();
-            } else {
-                onDeselect && onDeselect();
-            }
+        if (checked) {
+            onSelect && onSelect();
+        } else {
+            onDeselect && onDeselect();
+        }
 
-        });
     }
 
     radioChangeHandler() {
 
-        const {checked} = this.state;
+        const {checked} = this.props;
 
         if (!checked) {
-            this.setState({
-                checked: true
-            }, () => {
-                const {onSelect} = this.props;
-                onSelect && onSelect();
-            });
+            const {onSelect} = this.props;
+            onSelect && onSelect();
         }
 
     }
@@ -116,7 +107,7 @@ class DraggableListItem extends Component {
 
         switch (this.props.selectMode) {
             case SelectMode.MULTI_SELECT:
-                this.checkboxChangeHandler(!this.state.checked);
+                this.checkboxChangeHandler(!this.props.checked);
                 return;
             case SelectMode.SINGLE_SELECT:
                 this.radioChangeHandler();
@@ -131,14 +122,6 @@ class DraggableListItem extends Component {
         onMouseOver && onMouseOver(e);
     }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.checked !== this.state.checked) {
-            this.setState({
-                checked: nextProps.checked
-            });
-        }
-    }
-
     render() {
 
         const {
@@ -148,7 +131,7 @@ class DraggableListItem extends Component {
                 index, className, theme, data, text, desc, iconCls, rightIconCls, tip, tipPosition,
                 disabled, isLoading, renderer, itemRenderer,
 
-                selectTheme, selectMode, radioUncheckedIconCls, radioCheckedIconCls,
+                checked, selectTheme, selectMode, radioUncheckedIconCls, radioCheckedIconCls,
                 checkboxUncheckedIconCls, checkboxCheckedIconCls, checkboxIndeterminateIconCls,
 
                 // not passing down these props
@@ -157,7 +140,7 @@ class DraggableListItem extends Component {
                 ...restProps
 
             } = this.props,
-            {checked, tipVisible} = this.state,
+            {tipVisible} = this.state,
 
             listItemClassName = classNames('draggable-list-item', {
                 [`theme-${theme}`]: theme,
