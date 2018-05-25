15c2783f4772297b2733f1a8c4c4e4d2c896937a
liangxiaojun <fatalxiao@163.com>
2018-05-25 09:24:34 +0800

    update VerticalPointStepExamples

--- examples/containers/app/modules/stepper/VerticalPointStepExamples.js
+++ examples/containers/app/modules/stepper/VerticalPointStepExamples.js
@@ -10,7 +10,7 @@ import doc from 'assets/propTypes/VerticalPointStep.json';
 
 import 'sass/containers/app/modules/stepper/VerticalPointStepExamples.scss';
 
-export default class VerticalPointStepExamples extends Component {
+class VerticalPointStepExamples extends Component {
 
     constructor(props) {
 
@@ -21,26 +21,21 @@ export default class VerticalPointStepExamples extends Component {
             finishedStep: 0
         };
 
-        this.updateStep = ::this.updateStep;
-        this.prev = ::this.prev;
-        this.next = ::this.next;
-        this.reset = ::this.reset;
-
     }
 
-    updateStep(step) {
+    updateStep = step => {
         this.setState({
             ...step
         });
-    }
+    };
 
-    prev() {
+    prev = () => {
         this.setState({
             activatedStep: this.state.activatedStep - 1
         });
-    }
+    };
 
-    next() {
+    next = () => {
 
         const activatedStep = this.state.activatedStep + 1,
             finishedStep = this.state.finishedStep > activatedStep ? this.state.finishedStep : activatedStep;
@@ -50,14 +45,14 @@ export default class VerticalPointStepExamples extends Component {
             finishedStep
         });
 
-    }
+    };
 
-    reset() {
+    reset = () => {
         this.setState({
             activatedStep: 0,
             finishedStep: 0
         });
-    }
+    };
 
     render() {
 
@@ -132,3 +127,5 @@ export default class VerticalPointStepExamples extends Component {
         );
     }
 };
+
+export default VerticalPointStepExamples;
