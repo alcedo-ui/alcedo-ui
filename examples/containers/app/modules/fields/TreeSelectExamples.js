45ea450f89c899c6e66a5a6067640b682f89c3ba
liangxiaojun <fatalxiao@163.com>
2018-05-29 11:30:51 +0800

    update TreeSelect examples

--- examples/containers/app/modules/fields/TreeSelectExamples.js
+++ examples/containers/app/modules/fields/TreeSelectExamples.js
@@ -104,6 +104,7 @@ class TreeSelectExamples extends Component {
                                 <TreeSelect selectMode={TreeSelect.SelectMode.MULTI_SELECT}
                                             data={this.data}
                                             autoClose={false}
+                                            isSelectRecursive={true}
                                             onChange={this.changeHandler}/>
                             </div>
                         </div>
