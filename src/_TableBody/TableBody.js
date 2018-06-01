dd6c452a8d72be9da516f4e47c2e85ecc93c131e
liangxiaojun <fatalxiao@163.com>
2018-06-01 15:12:53 +0800

    optimize TableBody

--- src/_TableBody/TableBody.js
+++ src/_TableBody/TableBody.js
@@ -47,7 +47,7 @@ class TableBody extends Component {
         return (
             <tbody className="table-body">
                 {
-                    data.map((row, rowIndex) => row ?
+                    data && data.map((row, rowIndex) => row ?
                         <TableRow key={idProp && idProp in row ? row[idProp] : rowIndex}
                                   rowIndex={startIndex + rowIndex}
                                   columns={columns}
