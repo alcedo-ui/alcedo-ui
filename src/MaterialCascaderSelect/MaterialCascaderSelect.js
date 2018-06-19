6cd6f63a3256f8cfebe18316389580fb151b07a1
liangxiaojun <fatalxiao@163.com>
2018-06-19 15:31:17 +0800

    add MaterialCascaderSelect

---
+++ src/MaterialCascaderSelect/MaterialCascaderSelect.js
@@ -0,0 +1,335 @@
+/**
+ * @file MaterialCascaderSelect component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import TreeSelect from '../TreeSelect';
+import Tip from '../Tip';
+import MaterialProvider from '../MaterialProvider';
+import Theme from '../Theme';
+
+import SelectMode from '../_statics/SelectMode';
+
+import Util from '../_vendors/Util';
+
+class MaterialCascaderSelect extends Component {
+
+    static SelectMode = SelectMode;
+    static Theme = Theme;
+    static Position = TreeSelect.Position;
+
+    constructor(props, ...restArgs) {
+
+        super(props, ...restArgs);
+
+        this.state = {
+            value: props.value
+        };
+
+    }
+
+    triggerChangeHandler = value => {
+        this.setState({
+            value
+        }, () => {
+            const {onChange} = this.props;
+            onChange && onChange(value);
+        });
+    };
+
+    closePopup = () => {
+        this.refs.treeSelect && this.refs.treeSelect.closePopup();
+    };
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.value !== this.state.value) {
+            this.setState({
+                value: nextProps.value
+            });
+        }
+    }
+
+    render() {
+
+        const {
+                className, style, theme, label, isLabelAnimate, disabled, required,
+                ...restProps
+            } = this.props,
+            {value} = this.state,
+
+            wrapperClassName = classNames('material-cascader-select', {
+                [className]: className
+            });
+
+        return (
+            <MaterialProvider className={wrapperClassName}
+                              style={style}
+                              theme={theme}
+                              label={label}
+                              isLabelAnimate={isLabelAnimate}
+                              hasValue={value && value.length > 0}
+                              disabled={disabled}
+                              required={required}>
+
+                <TreeSelect {...restProps}
+                            ref="treeSelect"
+                            value={value}
+                            disabled={disabled}
+                            onChange={this.triggerChangeHandler}/>
+
+            </MaterialProvider>
+        );
+
+    }
+}
+
+MaterialCascaderSelect.propTypes = {
+
+    /**
+     * The CSS class name of the root element.
+     */
+    className: PropTypes.string,
+
+    /**
+     * The class name of the popup element.
+     */
+    popupClassName: PropTypes.string,
+
+    /**
+     * Override the styles of the root element.
+     */
+    style: PropTypes.object,
+
+    /**
+     * Override the styles of the popup element.
+     */
+    popupStyle: PropTypes.object,
+
+    /**
+     * The theme.
+     */
+    theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+    position: PropTypes.oneOf(Util.enumerateValue(TreeSelect.Position)),
+
+    /**
+     * The name of the MaterialCascaderSelect.
+     */
+    name: PropTypes.string,
+
+    /**
+     * The label of the text field.
+     */
+    label: PropTypes.any,
+
+    /**
+     * The animate of the text field.
+     */
+    isLabelAnimate: PropTypes.bool,
+
+    /**
+     * The value of the MaterialCascaderSelect.
+     */
+    value: PropTypes.any,
+
+    /**
+     * The placeholder of the MaterialCascaderSelect.
+     */
+    placeholder: PropTypes.string,
+
+    rightIconCls: PropTypes.string,
+
+    /**
+     * The options data.
+     */
+    /**
+     * The options data.
+     */
+    data: PropTypes.shape({
+
+        /**
+         * The CSS class name of the tree node.
+         */
+        className: PropTypes.string,
+
+        /**
+         * Override the styles of the tree node.
+         */
+        style: PropTypes.object,
+
+        /**
+         * The theme of the tree button.
+         */
+        theme: PropTypes.oneOf(Util.enumerateValue(Theme)),
+
+        /**
+         * The text value of the tree button.Type can be string or number.
+         */
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+        /**
+         * The tree node's display text. Type can be string, number or bool.
+         */
+        text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+        /**
+         * The desc value of the tree node. Type can be string or number.
+         */
+        desc: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+        /**
+         * If true,the tree node will be disabled.
+         */
+        disabled: PropTypes.bool,
+
+        /**
+         * If true,the button will be have loading effect.
+         */
+        isLoading: PropTypes.bool,
+
+        /**
+         * Use this property to display an icon. It will display on the left.
+         */
+        iconCls: PropTypes.string,
+
+        /**
+         * Use this property to display an icon. It will display on the right.
+         */
+        rightIconCls: PropTypes.string,
+
+        /**
+         * The message of tip.
+         */
+        title: PropTypes.string,
+
+        children: PropTypes.array,
+
+        /**
+         * You can create a complicated renderer callback instead of value and desc prop.
+         */
+        itemRenderer: PropTypes.func,
+
+        /**
+         * Callback function fired when a tree node touch-tapped.
+         */
+        onClick: PropTypes.func
+
+    }),
+
+    /**
+     * The invalid message of dropDownSelect.
+     */
+    invalidMsg: PropTypes.string,
+
+    /**
+     * If true,the dropDownSelect will be disabled.
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * The select mode of listItem.Can be normal,checkbox.
+     */
+    selectMode: PropTypes.oneOf(Util.enumerateValue(SelectMode)),
+
+    /**
+     * The value field name in data. (default: "value")
+     */
+    valueField: PropTypes.string,
+
+    /**
+     * The display field name in data. (default: "text")
+     */
+    displayField: PropTypes.string,
+
+    /**
+     * The description field name in data. (default: "desc")
+     */
+    descriptionField: PropTypes.string,
+
+    /**
+     * The message of the MaterialCascaderSelect.
+     */
+    infoMsg: PropTypes.string,
+
+    /**
+     * Use this function to format the options's text.
+     */
+    textFormat: PropTypes.func,
+
+    /**
+     * If true,the drop-down box automatically closed after selection.
+     */
+    autoClose: PropTypes.bool,
+
+    shouldPreventContainerScroll: PropTypes.bool,
+    isSelectRecursive: PropTypes.bool,
+    allowCollapse: PropTypes.bool,
+    collapsedIconCls: PropTypes.string,
+    expandedIconCls: PropTypes.string,
+    radioUncheckedIconCls: PropTypes.string,
+    radioCheckedIconCls: PropTypes.string,
+    checkboxUncheckedIconCls: PropTypes.string,
+    checkboxCheckedIconCls: PropTypes.string,
+    checkboxIndeterminateIconCls: PropTypes.string,
+
+    popupChildren: PropTypes.any,
+
+    renderer: PropTypes.func,
+
+    triggerRenderer: PropTypes.func,
+
+    /**
+     * Callback function fired when the button is touch-tapped.
+     */
+    onItemClick: PropTypes.func,
+
+    /**
+     * Callback function fired when the popup is closed.
+     */
+    onClosePopup: PropTypes.func,
+
+    /**
+     * Callback function fired when a menu item is selected.
+     */
+    onChange: PropTypes.func,
+
+    onWheel: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    onMouseOver: PropTypes.func,
+    onMouseOut: PropTypes.func
+
+};
+
+MaterialCascaderSelect.defaultProps = {
+
+    theme: Theme.DEFAULT,
+    popupTheme: Theme.DEFAULT,
+
+    position: TreeSelect.Position.LEFT,
+    isLabelAnimate: true,
+    placeholder: 'Please select ...',
+    rightIconCls: 'fas fa-angle-down',
+    data: [],
+    disabled: false,
+    selectMode: SelectMode.SINGLE_SELECT,
+
+    valueField: 'value',
+    displayField: 'text',
+    descriptionField: 'desc',
+
+    autoClose: true,
+
+    shouldPreventContainerScroll: true,
+    isSelectRecursive: false,
+    allowCollapse: true,
+
+    required: false
+
+};
+
+export default MaterialCascaderSelect;
