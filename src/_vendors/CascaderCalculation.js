255b269f9838bfaa7ed8f649d20f2a8dd248d0bb
liangxiaojun <fatalxiao@163.com>
2018-06-19 11:09:27 +0800

    update CascaderCalculation

--- src/_vendors/CascaderCalculation.js
+++ src/_vendors/CascaderCalculation.js
@@ -3,6 +3,8 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
+import Util from './Util';
+
 function getMaxDepth(activatedPath) {
 
     if (!activatedPath || activatedPath.length < 1) {
@@ -20,6 +22,69 @@ function getMaxDepth(activatedPath) {
 
 }
 
+function calPath(value, data, props) {
+
+    if (!value || !data) {
+        return;
+    }
+
+    return traverseDataCalPath({children: data}, value, props, data, null);
+
+}
+
+function traverseDataCalPath(node, value, props, rootList, parent, index = 0) {
+
+    if (!node || node.length < 1 || !value) {
+        return;
+    }
+
+    const {valueField, displayField} = props;
+
+    if (Util.getValueByValueField(node, valueField, displayField)
+        === Util.getValueByValueField(value, valueField, displayField)) {
+
+        if (!parent) { // root
+            return null;
+        } else {
+            return [{
+                node,
+                index
+            }];
+        }
+
+    }
+
+    // find in children
+    if (node.children && node.children.length > 0) {
+
+        for (let i = 0, len = node.children.length; i < len; i++) {
+
+            // traverse child node
+            const path = traverseDataCalPath(node.children[i], value, props, rootList, node, i);
+
+            // if finded in child node
+            if (path) {
+
+                if (!parent || parent == rootList) { // root
+                    return path;
+                }
+
+                path.unshift({
+                    node,
+                    index
+                });
+                return path;
+
+            }
+
+        }
+    }
+
+    return;
+
+}
+
 export default {
-    getMaxDepth
+    getMaxDepth,
+    calPath
 };
