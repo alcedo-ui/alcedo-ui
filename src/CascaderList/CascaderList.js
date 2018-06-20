09809b11500df7d93a2212e4871bf7433b3b4cb2
liangxiaojun <fatalxiao@163.com>
2018-06-20 11:21:58 +0800

    update CascaderList

--- src/CascaderList/CascaderList.js
+++ src/CascaderList/CascaderList.js
@@ -109,10 +109,10 @@ class CascaderList extends Component {
 
     };
 
-    nodeClickHandler = (node, index, path) => {
+    nodeClickHandler = (node, index, path, e) => {
 
         const {onNodeClick} = this.props;
-        onNodeClick && onNodeClick(node, index);
+        onNodeClick && onNodeClick(node, index, path, e);
 
         this.setState({
             activatedPath: path
