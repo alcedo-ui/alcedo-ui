433384a84255332d5859d1927924611776f07742
liangxiaojun <fatalxiao@163.com>
2018-09-01 14:26:02 +0800

    update babelrc

---
+++ .babelrc.js
@@ -0,0 +1,84 @@
+const presets = [
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                modules: false,
+                targets: {
+                    browsers: [
+                        'last 10 major versions',
+                        'safari >= 7',
+                        'ie >= 9'
+                    ]
+                }
+            }
+        ],
+        '@babel/preset-react'
+    ],
+    commonPlugins = [
+        '@babel/plugin-proposal-class-properties',
+        [
+            '@babel/plugin-proposal-decorators',
+            {
+                legacy: true
+            }
+        ],
+        '@babel/plugin-proposal-do-expressions',
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/plugin-proposal-export-namespace-from',
+        '@babel/plugin-proposal-function-bind',
+        '@babel/plugin-proposal-function-sent',
+        '@babel/plugin-proposal-json-strings',
+        '@babel/plugin-proposal-logical-assignment-operators',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-optional-chaining',
+        [
+            '@babel/plugin-proposal-pipeline-operator',
+            {
+                proposal: 'minimal'
+            }
+        ],
+        '@babel/plugin-proposal-throw-expressions',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-syntax-import-meta',
+        '@babel/plugin-transform-runtime'
+    ],
+    packagePlugins = [
+        [
+            'transform-react-remove-prop-types',
+            {
+                removeImport: true
+            }
+        ]
+    ],
+    packageConfig = {
+        presets,
+        plugins: [
+            ...commonPlugins,
+            ...packagePlugins
+        ]
+    };
+
+module.exports = {
+    'env': {
+
+        'development': {
+            presets,
+            plugins: [
+                ...commonPlugins,
+                'transform-import-sync'
+            ]
+        },
+
+        'test': {
+            'presets': [
+                '@babel/preset-env',
+                '@babel/preset-react'
+            ]
+        },
+
+        'production': packageConfig
+
+    }
+};
