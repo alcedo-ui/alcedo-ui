f86c8a8092019f89877e778ef83cb1862b0d2f8d
liangxiaojun <fatalxiao@163.com>
2018-09-25 15:41:41 +0800

    update build util

--- build/utils.js
+++ build/utils.js
@@ -2,14 +2,89 @@ const path = require('path'),
 
     config = require('./config.js');
 
-exports.assetsPath = p => {
+function assetsPath(p) {
     return path.posix.join(config.assetsDirectory, p);
-};
+}
 
-exports.assetsSubPath = p => {
+function assetsSubPath(p) {
     return path.posix.join(config.assetsSubDirectory, p);
-};
+}
 
-exports.assetsVendorsAbsolutePath = p => {
+function assetsVendorsAbsolutePath(p) {
     return path.posix.join(config.build.assetsRoot, exports.assetsSubPath(`vendors/${p}`));
-};
+}
+
+function fsExistsSync(p) {
+    try {
+        fs.accessSync(p, (fs.constants && fs.constants.F_OK) || fs.F_OK);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+function copyRecursionSync(src, dist, excludes) {
+
+    const paths = fs.readdirSync(src);
+
+    for (let p of paths) {
+
+        if (excludes && excludes.findIndex(item => p.includes(item)) > -1) {
+            continue;
+        }
+
+        const srcPath = src + '/' + p,
+            distPath = dist + '/' + p,
+
+            stat = fs.statSync(srcPath);
+
+        if (stat.isDirectory()) {
+
+            if (!fsExistsSync(distPath)) {
+                fs.mkdirSync(distPath);
+            }
+
+            copyRecursionSync(srcPath, distPath, excludes);
+
+        } else {
+            fs.copyFileSync(srcPath, distPath);
+        }
+
+    }
+
+}
+
+function rmRecursionSync(p) {
+
+    const paths = fs.readdirSync(p);
+
+    for (let path of paths) {
+
+        const rmPath = p + '/' + path,
+            stat = fs.statSync(rmPath);
+
+        if (stat.isDirectory()) {
+            rmRecursionSync(rmPath);
+            if (fsExistsSync(rmPath)) {
+                fs.rmdirSync(rmPath);
+            }
+        } else {
+            if (fsExistsSync(rmPath)) {
+                fs.unlinkSync(rmPath);
+            }
+        }
+
+    }
+
+    if (fsExistsSync(p)) {
+        fs.rmdirSync(p);
+    }
+
+}
+
+exports.assetsPath = assetsPath;
+exports.assetsSubPath = assetsSubPath;
+exports.assetsVendorsAbsolutePath = assetsVendorsAbsolutePath;
+exports.fsExistsSync = fsExistsSync;
+exports.copyRecursionSync = copyRecursionSync;
+exports.rmRecursionSync = rmRecursionSync;
