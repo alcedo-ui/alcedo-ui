2fa962cdf4e633bb8387f2faf22b9bde55849c31
liangxiaojun <fatalxiao@163.com>
2019-01-18 15:37:34 +0800

    update OpacityPickerExamples

--- examples/containers/app/modules/media/OpacityPickerExamples.js
+++ examples/containers/app/modules/media/OpacityPickerExamples.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import Widget from 'src/Widget';
 import WidgetHeader from 'src/WidgetHeader';
 import OpacityPicker from 'src/OpacityPicker';
+import PopupProvider from 'src/PopupProvider';
 
 import PropTypeDescTable from 'components/PropTypeDescTable';
 import doc from 'assets/propTypes/OpacityPicker.json';
@@ -65,6 +66,47 @@ class OpacityPickerExamples extends Component {
 
                                     <div className="picked-opacity-value">{opacity}</div>
 
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </Widget>
+
+                <Widget>
+
+                    <WidgetHeader className="example-header" title="Basic"/>
+
+                    <div className="widget-content">
+                        <div className="example-content">
+
+                            <div className="examples-wrapper">
+
+                                <div className="opacity-select-scroller-wrapper">
+                                    <div className="opacity-select-scroller">
+                                        <PopupProvider className="opacity-select-pop"
+                                                       position={PopupProvider.Position.BOTTOM_LEFT}
+                                                       parentEl={document.querySelector('.opacity-select-scroller-wrapper')}
+                                                       popupContent={
+                                                           <OpacityPicker value={opacity}
+                                                                          color={Color.hex2rgb('38b1eb')}
+                                                                          scrollEl={document.querySelector('.opacity-select-scroller-wrapper')}
+                                                                          onChange={this.changeHandler}/>
+                                                       }>
+                                            <div className="picked-opacity-wrapper">
+                                                <div className="picked-opacity">
+                                                    <div className="picked-opacity-color"
+                                                         style={{
+                                                             background: '#38b1eb',
+                                                             opacity: opacity
+                                                         }}></div>
+                                                </div>
+                                                <div className="picked-opacity-value">{opacity}</div>
+                                            </div>
+                                        </PopupProvider>
+                                    </div>
                                 </div>
 
                             </div>
