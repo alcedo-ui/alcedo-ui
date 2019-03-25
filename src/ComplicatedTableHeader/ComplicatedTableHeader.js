aae7985b5334227e691e603d7097f2dc5cda5d47
liangxiaojun <fatalxiao@163.com>
2019-03-25 16:47:02 +0800

    add ComplicatedTableHeader

---
+++ src/ComplicatedTableHeader/ComplicatedTableHeader.js
@@ -0,0 +1,57 @@
+/**
+ * @file ComplicatedTableHeader component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import SelectMode from '../_statics/SelectMode';
+import SelectAllMode from '../_statics/SelectAllMode';
+import SortType from '../_statics/SortType';
+
+class ComplicatedTableHeader extends Component {
+
+    static SelectMode = SelectMode;
+    static SelectAllMode = SelectAllMode;
+    static SortType = SortType;
+
+    constructor(props, ...restArgs) {
+        super(props, ...restArgs);
+    }
+
+    render() {
+
+        const {className, style} = this.props,
+
+            tableClassName = classNames('complicated-table-header', {
+                [className]: className
+            });
+
+        return (
+            <div className={tableClassName}
+                 style={style}>
+
+            </div>
+        );
+    }
+}
+
+ComplicatedTableHeader.propTypes = {
+
+    /**
+     * The CSS class name of the root element.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Override the styles of the root element.
+     */
+    style: PropTypes.object
+
+};
+
+ComplicatedTableHeader.defaultProps = {};
+
+export default ComplicatedTableHeader;
---
+++ src/ComplicatedTableHeader/index.js
@@ -0,0 +1,6 @@
+/**
+ * @file ComplicatedTableHeader
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+export default from './ComplicatedTableHeader';
