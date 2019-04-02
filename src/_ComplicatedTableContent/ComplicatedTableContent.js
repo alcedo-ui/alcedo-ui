3917bfb7f717494858df41199529c9c0762cb869
liangxiaojun <fatalxiao@163.com>
2019-04-02 17:18:19 +0800

    update ComplicatedTableContent

--- src/_ComplicatedTableContent/ComplicatedTableContent.js
+++ src/_ComplicatedTableContent/ComplicatedTableContent.js
@@ -7,9 +7,12 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import Table from '../_BaseTable';
+
 import SelectMode from '../_statics/SelectMode';
 import SelectAllMode from '../_statics/SelectAllMode';
 import SortType from '../_statics/SortType';
+import Util from '../_vendors/Util';
 
 class ComplicatedTableContent extends Component {
 
@@ -23,16 +26,19 @@ class ComplicatedTableContent extends Component {
 
     render() {
 
-        const {className, style} = this.props,
-
-            tableClassName = classNames('complicated-table-content', {
-                [className]: className
-            });
+        const {
+            className, style, columns, data,
+            onSort
+        } = this.props;
 
         return (
-            <div className={tableClassName}
+            <div className={classNames('complicated-table-content', {
+                [className]: className
+            })}
                  style={style}>
-
+                <Table columns={columns}
+                       data={data}
+                       onSort={onSort}/>
             </div>
         );
     }
@@ -48,7 +54,84 @@ ComplicatedTableContent.propTypes = {
     /**
      * Override the styles of the root element.
      */
-    style: PropTypes.object
+    style: PropTypes.object,
+
+    /**
+     * Children passed into table header.
+     */
+    columns: PropTypes.arrayOf(PropTypes.shape({
+
+        /**
+         * The class name of header.
+         */
+        headerClassName: PropTypes.string,
+
+        /**
+         * Override the styles of header.
+         */
+        headerStyle: PropTypes.object,
+
+        /**
+         * The render content in header.
+         * (1) string，example： 'id'
+         * (2) callback，example：function (colIndex) {return colIndex;}
+         */
+        headerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * The class name of td.
+         */
+        cellClassName: PropTypes.string,
+
+        /**
+         * Override the styles of td.
+         */
+        cellStyle: PropTypes.object,
+
+        /**
+         * The render content in table.
+         * (1) data key，example： 'id'
+         * (2) data key tamplate，example：'${id} - ${name}'
+         * (3) callback，example：function (rowData, rowIndex, colIndex) {return rowData.id;}
+         */
+        cellRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * The class name of footer.
+         */
+        footerClassName: PropTypes.string,
+
+        /**
+         * Override the styles of footer.
+         */
+        footerStyle: PropTypes.object,
+
+        /**
+         * The render content in footer.
+         * (1) string，example： 'id'
+         * (2) callback，example：function (colIndex) {return colIndex;}
+         */
+        footerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * If true,this column can be sorted.
+         */
+        sortable: PropTypes.bool,
+
+        /**
+         * Sort field.
+         */
+        sortProp: PropTypes.string,
+
+        defaultSortType: PropTypes.oneOf(Util.enumerateValue(SortType)),
+
+        span: PropTypes.func
+
+    })).isRequired,
+
+    data: PropTypes.array,
+
+    onSort: PropTypes.func
 
 };
 
