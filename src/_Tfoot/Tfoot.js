83ef2beaf7ceee27631f6bc6d361c2a5f1abda13
liangxiaojun <fatalxiao@163.com>
2019-04-03 13:57:24 +0800

    update Tfoot

---
+++ src/_Tfoot/Tfoot.js
@@ -0,0 +1,148 @@
+/**
+ * @file Tfoot component
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Td from '../_Td';
+
+class Tfoot extends Component {
+
+    constructor(props, ...restArgs) {
+        super(props, ...restArgs);
+    }
+
+    handleClick = e => {
+        const {data, disabled, onFootClick} = this.props;
+        !disabled && onFootClick && onFootClick(data, e);
+    };
+
+    render() {
+
+        const {
+            className, columnsWithSpan, data, disabled,
+            onCellClick
+        } = this.props;
+
+        return (
+            <tfoot className={classNames({
+                [className]: className,
+                [data.rowClassName]: data.rowClassName
+            })}
+                   style={data.rowStyle}
+                   disabled={disabled}
+                   onClick={this.handleClick}>
+                <tr>
+                    {
+                        columnsWithSpan && columnsWithSpan.map((column, colIndex) =>
+                            <Td key={colIndex}
+                                colIndex={colIndex}
+                                data={data}
+                                className={column.footerClassName}
+                                style={column.footerStyle}
+                                renderer={column.footerRenderer}
+                                span={column.span}
+                                disabled={disabled}
+                                onCellClick={onCellClick}/>
+                        )
+                    }
+                </tr>
+            </tfoot>
+        );
+
+    }
+}
+
+Tfoot.propTypes = {
+
+    className: PropTypes.string,
+
+    /**
+     * Children passed into table header.
+     */
+    columnsWithSpan: PropTypes.arrayOf(PropTypes.shape({
+
+        /**
+         * The class name of header.
+         */
+        headerClassName: PropTypes.string,
+
+        /**
+         * Override the styles of header.
+         */
+        headerStyle: PropTypes.object,
+
+        /**
+         * The render content in header.
+         * (1) string，example： 'id'
+         * (2) callback，example：function (colIndex) {return colIndex;}
+         */
+        headerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * The class name of td.
+         */
+        cellClassName: PropTypes.string,
+
+        /**
+         * Override the styles of td.
+         */
+        cellStyle: PropTypes.object,
+
+        /**
+         * The render content in table.
+         * (1) data key，example： 'id'
+         * (2) data key tamplate，example：'${id} - ${name}'
+         * (3) callback，example：function (rowData, rowIndex, colIndex) {return rowData.id;}
+         */
+        cellRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * The class name of footer.
+         */
+        footerClassName: PropTypes.string,
+
+        /**
+         * Override the styles of footer.
+         */
+        footerStyle: PropTypes.object,
+
+        /**
+         * The render content in footer.
+         * (1) string，example： 'id'
+         * (2) callback，example：function (colIndex) {return colIndex;}
+         */
+        footerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * If true,this column can be sorted.
+         */
+        sortable: PropTypes.bool,
+
+        /**
+         * Sort field.
+         */
+        sortProp: PropTypes.string,
+
+        defaultSortType: PropTypes.oneOf(Util.enumerateValue(SortType)),
+
+        span: PropTypes.func
+
+    })).isRequired,
+
+    data: PropTypes.array,
+    disabled: PropTypes.bool,
+
+    onFootClick: PropTypes.func,
+    onCellClick: PropTypes.func
+
+};
+
+Tfoot.defaultProps = {
+    disabled: false
+};
+
+export default Tfoot;
---
+++ src/_Tfoot/index.js
@@ -0,0 +1,6 @@
+/**
+ * @file Tfoot
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+export default from './Tfoot';
