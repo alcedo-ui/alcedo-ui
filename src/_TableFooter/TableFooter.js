911f042088ccbb2f7f94a33ccd7487e9c700ce57
liangxiaojun <fatalxiao@163.com>
2019-04-04 16:36:16 +0800

    update TableFooter

--- src/_TableFooter/TableFooter.js
+++ src/_TableFooter/TableFooter.js
@@ -9,13 +9,13 @@ import classNames from 'classnames';
 
 import SelectMode from '../_statics/SelectMode';
 import SelectAllMode from '../_statics/SelectAllMode';
-import SortType from '../_statics/SortType';
+import SortingType from '../_statics/SortingType';
 
 class TableFooter extends Component {
 
     static SelectMode = SelectMode;
     static SelectAllMode = SelectAllMode;
-    static SortType = SortType;
+    static SortingType = SortingType;
 
     constructor(props, ...restArgs) {
         super(props, ...restArgs);
