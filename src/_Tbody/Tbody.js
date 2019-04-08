84ac0253a7f78082a92c5cf4b3e8fb568bc1b512
liangxiaojun <fatalxiao@163.com>
2019-04-08 14:25:45 +0800

    update Tbody

--- src/_Tbody/Tbody.js
+++ src/_Tbody/Tbody.js
@@ -78,6 +78,16 @@ Tbody.propTypes = {
     columns: PropTypes.arrayOf(PropTypes.shape({
 
         /**
+         * width of column
+         */
+        width: PropTypes.number,
+
+        /**
+         * minimum width of column
+         */
+        minWidth: PropTypes.number,
+
+        /**
          * The class name of header.
          */
         headerClassName: PropTypes.string,
