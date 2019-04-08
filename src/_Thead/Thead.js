dc97930d27330c124d724770d32fd6c45f7f5a39
liangxiaojun <fatalxiao@163.com>
2019-04-08 16:57:09 +0800

    update Thead

--- src/_Thead/Thead.js
+++ src/_Thead/Thead.js
@@ -8,10 +8,20 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import Th from '../_Th';
-import Util from '../_vendors/Util';
+
+import HorizontalAlign from '../_statics/HorizontalAlign';
+import SelectMode from '../_statics/SelectMode';
+import SelectAllMode from '../_statics/SelectAllMode';
 import SortingType from '../_statics/SortingType';
 
+import Util from '../_vendors/Util';
+
 class Thead extends Component {
+
+    static Align = HorizontalAlign;
+    static SelectMode = SelectMode;
+    static SelectAllMode = SelectAllMode;
+    static SortingType = SortingType;
 
     constructor(props, ...restArgs) {
         super(props, ...restArgs);
@@ -34,9 +44,9 @@ class Thead extends Component {
                     {
                         columns && columns.map((item, index) => item ?
                             <Th key={index}
-                                className={item.headerClassName}
-                                style={item.headerStyle}
-                                renderer={item.headerRenderer}
+                                className={item.headClassName}
+                                style={item.headStyle}
+                                renderer={item.headRenderer}
                                 colIndex={index}
                                 data={data}
                                 sorting={sorting}
@@ -80,29 +90,34 @@ Thead.propTypes = {
         /**
          * The class name of header.
          */
-        headerClassName: PropTypes.string,
+        headClassName: PropTypes.string,
 
         /**
          * Override the styles of header.
          */
-        headerStyle: PropTypes.object,
+        headStyle: PropTypes.object,
+
+        /**
+         * align of table header cell
+         */
+        headAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
 
         /**
          * The render content in header.
          * (1) string，example： 'id'
-         * (2) callback，example：function (tableData, colIndex) {return colIndex;}
+         * (2) callback，example：function (colIndex) {return colIndex;}
          */
-        headerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+        headRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
 
         /**
          * The class name of td.
          */
-        cellClassName: PropTypes.string,
+        bodyClassName: PropTypes.string,
 
         /**
          * Override the styles of td.
          */
-        cellStyle: PropTypes.object,
+        bodyStyle: PropTypes.object,
 
         /**
          * The render content in table.
@@ -110,41 +125,47 @@ Thead.propTypes = {
          * (2) data key tamplate，example：'${id} - ${name}'
          * (3) callback，example：function (rowData, rowIndex, colIndex) {return rowData.id;}
          */
-        cellRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+        bodyRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * align of table body cell
+         */
+        bodyAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
 
         /**
          * The class name of footer.
          */
-        footerClassName: PropTypes.string,
+        footClassName: PropTypes.string,
 
         /**
          * Override the styles of footer.
          */
-        footerStyle: PropTypes.object,
+        footStyle: PropTypes.object,
 
         /**
          * The render content in footer.
          * (1) string，example： 'id'
-         * (2) callback，example：function (tableData, colIndex) {return colIndex;}
+         * (2) callback，example：function (colIndex) {return colIndex;}
          */
-        footerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+        footRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
 
         /**
-         * If true, this column can be sorted.
+         * align of table footer cell
+         */
+        footAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * If true,this column can be sorted.
          */
         sortable: PropTypes.bool,
 
         /**
-         * Sort field.
+         * Sorting property.
          */
         sortingProp: PropTypes.string,
 
         defaultSortingType: PropTypes.oneOf(Util.enumerateValue(SortingType)),
 
-        /**
-         * Column span callback
-         * example: function (colIndex, rowIndex) {return colIndex === 0 ? 2 : null;}
-         */
         span: PropTypes.func
 
     })).isRequired,
