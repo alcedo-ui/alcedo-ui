175e91495d02706390ca7e240a8cafecc071bb45
liangxiaojun <fatalxiao@163.com>
2019-04-08 14:25:34 +0800

    update Thead

--- src/_Thead/Thead.js
+++ src/_Thead/Thead.js
@@ -62,7 +62,93 @@ Thead.propTypes = {
     className: PropTypes.string,
     style: PropTypes.object,
 
-    columns: PropTypes.array,
+    /**
+     * Children passed into table header.
+     */
+    columns: PropTypes.arrayOf(PropTypes.shape({
+
+        /**
+         * width of column
+         */
+        width: PropTypes.number,
+
+        /**
+         * minimum width of column
+         */
+        minWidth: PropTypes.number,
+
+        /**
+         * The class name of header.
+         */
+        headerClassName: PropTypes.string,
+
+        /**
+         * Override the styles of header.
+         */
+        headerStyle: PropTypes.object,
+
+        /**
+         * The render content in header.
+         * (1) string，example： 'id'
+         * (2) callback，example：function (tableData, colIndex) {return colIndex;}
+         */
+        headerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * The class name of td.
+         */
+        cellClassName: PropTypes.string,
+
+        /**
+         * Override the styles of td.
+         */
+        cellStyle: PropTypes.object,
+
+        /**
+         * The render content in table.
+         * (1) data key，example： 'id'
+         * (2) data key tamplate，example：'${id} - ${name}'
+         * (3) callback，example：function (rowData, rowIndex, colIndex) {return rowData.id;}
+         */
+        cellRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * The class name of footer.
+         */
+        footerClassName: PropTypes.string,
+
+        /**
+         * Override the styles of footer.
+         */
+        footerStyle: PropTypes.object,
+
+        /**
+         * The render content in footer.
+         * (1) string，example： 'id'
+         * (2) callback，example：function (tableData, colIndex) {return colIndex;}
+         */
+        footerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+        /**
+         * If true, this column can be sorted.
+         */
+        sortable: PropTypes.bool,
+
+        /**
+         * Sort field.
+         */
+        sortingProp: PropTypes.string,
+
+        defaultSortingType: PropTypes.oneOf(Util.enumerateValue(SortingType)),
+
+        /**
+         * Column span callback
+         * example: function (colIndex, rowIndex) {return colIndex === 0 ? 2 : null;}
+         */
+        span: PropTypes.func
+
+    })).isRequired,
+
     data: PropTypes.array,
 
     /**
