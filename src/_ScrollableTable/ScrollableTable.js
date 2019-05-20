b3401127970d79b84d701c0610f3a31c5e8b4678
liangxiaojun <fatalxiao@163.com>
2019-05-20 10:39:28 +0800

    update ScrollableTable

--- src/_ScrollableTable/ScrollableTable.js
+++ src/_ScrollableTable/ScrollableTable.js
@@ -187,6 +187,11 @@ ScrollableTable.propTypes = {
         minWidth: PropTypes.number,
 
         /**
+         * align of current column
+         */
+        align: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
          * The class name of header.
          */
         headClassName: PropTypes.string,
@@ -197,16 +202,21 @@ ScrollableTable.propTypes = {
         headStyle: PropTypes.object,
 
         /**
-         * The render content in header.
-         * (1) string，example： 'id'
-         * (2) callback，example：function (colIndex) {return colIndex;}
-         */
-        headRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-
-        /**
          * align of table header cell
          */
         headAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * The render content in table head.
+         *  (1) callback:
+         *      function (tableData, colIndex) {
+         *          return colIndex;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        headRenderer: PropTypes.any,
 
         /**
          * column span of table header
@@ -224,17 +234,21 @@ ScrollableTable.propTypes = {
         bodyStyle: PropTypes.object,
 
         /**
-         * The render content in table.
-         * (1) data key，example： 'id'
-         * (2) data key tamplate，example：'${id} - ${name}'
-         * (3) callback，example：function (rowData, rowIndex, colIndex) {return rowData.id;}
-         */
-        bodyRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-
-        /**
          * align of table body cell
          */
         bodyAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * The render content in table body.
+         *  (1) callback:
+         *      function (rowData, rowIndex, colIndex) {
+         *          return rowData.id;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        bodyRenderer: PropTypes.any,
 
         /**
          * column span of table body
@@ -252,16 +266,21 @@ ScrollableTable.propTypes = {
         footStyle: PropTypes.object,
 
         /**
-         * The render content in footer.
-         * (1) string，example： 'id'
-         * (2) callback，example：function (colIndex) {return colIndex;}
-         */
-        footRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-
-        /**
          * align of table footer cell
          */
         footAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * The render content in table foot.
+         *  (1) callback:
+         *      function (tableData, colIndex) {
+         *          return colIndex;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        footRenderer: PropTypes.any,
 
         /**
          * column span of table foot
