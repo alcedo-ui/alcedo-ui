bef33a30131780fbc908646a9dbbdd0d13aa7747
liangxiaojun <fatalxiao@163.com>
2019-05-20 10:36:50 +0800

    update TableContent

--- src/_TableContent/TableContent.js
+++ src/_TableContent/TableContent.js
@@ -645,9 +645,9 @@ TableContent.propTypes = {
     columns: PropTypes.arrayOf(PropTypes.shape({
 
         /**
-         * fixed position of column ( 'left' / 'right' )
+         * fixed position of column ( true / 'left' / 'right' )
          */
-        fixed: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+        fixed: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(Util.enumerateValue(HorizontalAlign))]),
 
         /**
          * width of column
@@ -660,6 +660,11 @@ TableContent.propTypes = {
         minWidth: PropTypes.number,
 
         /**
+         * align of current column
+         */
+        align: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
          * The class name of header.
          */
         headClassName: PropTypes.string,
@@ -670,16 +675,21 @@ TableContent.propTypes = {
         headStyle: PropTypes.object,
 
         /**
-         * The render content in header.
-         * (1) string，example： 'id'
-         * (2) callback，example：function (colIndex) {return colIndex;}
-         */
-        headRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-
-        /**
          * align of table header cell
          */
         headAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * The render content in table head.
+         *  (1) callback:
+         *      function (tableData, colIndex) {
+         *          return colIndex;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        headRenderer: PropTypes.any,
 
         /**
          * column span of table header
@@ -697,17 +707,21 @@ TableContent.propTypes = {
         bodyStyle: PropTypes.object,
 
         /**
-         * The render content in table.
-         * (1) data key，example： 'id'
-         * (2) data key tamplate，example：'${id} - ${name}'
-         * (3) callback，example：function (rowData, rowIndex, colIndex) {return rowData.id;}
-         */
-        bodyRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-
-        /**
          * align of table body cell
          */
         bodyAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * The render content in table body.
+         *  (1) callback:
+         *      function (rowData, rowIndex, colIndex) {
+         *          return rowData.id;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        bodyRenderer: PropTypes.any,
 
         /**
          * column span of table body
@@ -725,16 +739,21 @@ TableContent.propTypes = {
         footStyle: PropTypes.object,
 
         /**
-         * The render content in footer.
-         * (1) string，example： 'id'
-         * (2) callback，example：function (colIndex) {return colIndex;}
-         */
-        footRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-
-        /**
          * align of table footer cell
          */
         footAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * The render content in table foot.
+         *  (1) callback:
+         *      function (tableData, colIndex) {
+         *          return colIndex;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        footRenderer: PropTypes.any,
 
         /**
          * column span of table foot
@@ -751,9 +770,7 @@ TableContent.propTypes = {
          */
         sortingProp: PropTypes.string,
 
-        defaultSortingType: PropTypes.oneOf(Util.enumerateValue(SortingType)),
-
-        children: PropTypes.array
+        defaultSortingType: PropTypes.oneOf(Util.enumerateValue(SortingType))
 
     })).isRequired,
 
