20386022c85cff8852e9b0fe3b00d876b5ba80b1
liangxiaojun <fatalxiao@163.com>
2019-06-06 11:21:37 +0800

    update TableContent

--- src/_TableContent/TableContent.js
+++ src/_TableContent/TableContent.js
@@ -724,24 +724,29 @@ TableContent.propTypes = {
     columns: PropTypes.arrayOf(PropTypes.shape({
 
         /**
-         * fixed position of column ( true / 'left' / 'right' )
+         * fixed position of column ( true / 'left' / 'right' ).
          */
         fixed: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(Util.enumerateValue(HorizontalAlign))]),
 
         /**
-         * width of column
+         * width of column.
          */
         width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
         /**
-         * minimum width of column
+         * minimum width of column.
          */
         minWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
         /**
-         * align of current column
+         * align of current column.
          */
         align: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
+
+        /**
+         * no wrap of current column.
+         */
+        noWrap: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
 
         /**
          * The class name of header.
@@ -771,7 +776,7 @@ TableContent.propTypes = {
         headRenderer: PropTypes.any,
 
         /**
-         * column span of table header
+         * column span of table header.
          *  (1) function callback:
          *      function (tableData, colIndex) {
          *          return null;
@@ -781,6 +786,11 @@ TableContent.propTypes = {
          *      render whatever you pass
          */
         headSpan: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
+
+        /**
+         * no wrap of table header.
+         */
+        headNoWrap: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
 
         /**
          * The class name of td.
@@ -793,7 +803,7 @@ TableContent.propTypes = {
         bodyStyle: PropTypes.object,
 
         /**
-         * align of table body cell
+         * align of table body cell.
          */
         bodyAlign: PropTypes.oneOf(Util.enumerateValue(HorizontalAlign)),
 
@@ -810,7 +820,7 @@ TableContent.propTypes = {
         bodyRenderer: PropTypes.any,
 
         /**
-         * column span of table body
+         * column span of table body.
          *  (1) function callback:
          *      function (rowData, colIndex, rowIndex) {
          *          return null;
@@ -820,6 +830,11 @@ TableContent.propTypes = {
          *      render whatever you pass
          */
         bodySpan: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
+
+        /**
+         * no wrap of table body.
+         */
+        bodyNoWrap: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
 
         /**
          * The class name of footer.
@@ -859,6 +874,11 @@ TableContent.propTypes = {
          *      render whatever you pass
          */
         footSpan: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
+
+        /**
+         * no wrap of table foot.
+         */
+        footNoWrap: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
 
         /**
          * If true,this column can be sorted.
