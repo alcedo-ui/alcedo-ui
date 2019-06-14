2733f7db6a6eb5d9015594c2a762152b30cc76fe
liangxiaojun <fatalxiao@163.com>
2019-06-14 10:06:07 +0800

    update TableContent

--- src/_TableContent/TableContent.js
+++ src/_TableContent/TableContent.js
@@ -561,7 +561,7 @@ class TableContent extends Component {
                 // not passing down these props
                 isSelectRecursive, selectUncheckedIconCls, selectCheckedIconCls, selectIndeterminateIconCls,
                 selectColumn, expandIconCls, autoSorting, isPaginated, page, pageSize, sortingFunc,
-                onInit, onExpand, onCollapse, onExpandChange, onDataUpdate,
+                onInit, onChange, onExpand, onCollapse, onExpandChange, onDataUpdate,
                 onSelect, onSelectAll, onDeselect, onDeselectAll,
 
                 ...restProps
