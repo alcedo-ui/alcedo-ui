cc1365e53c3a84dbeafa86ce39af1dea3cfaa9b3
liangxiaojun <fatalxiao@163.com>
2019-07-10 16:38:16 +0800

    update ScrollTable

--- src/_ScrollTable/ScrollTable.js
+++ src/_ScrollTable/ScrollTable.js
@@ -278,7 +278,7 @@ ScrollTable.propTypes = {
         /**
          * The render content in table body.
          *  (1) callback:
-         *      function (rowData, rowIndex, colIndex) {
+         *      function (rowData, rowIndex, colIndex, parentData, tableData, collapsed, depth, path) {
          *          return rowData.id;
          *      }
          *
