0fd074cfa3f700d1fe97e8778aefcce67dffc513
liangxiaojun <fatalxiao@163.com>
2019-07-10 16:38:49 +0800

    update Tfoot

--- src/_Tfoot/Tfoot.js
+++ src/_Tfoot/Tfoot.js
@@ -171,7 +171,7 @@ Tfoot.propTypes = {
         /**
          * The render content in table body.
          *  (1) callback:
-         *      function (rowData, rowIndex, colIndex) {
+         *      function (rowData, rowIndex, colIndex, parentData, tableData, collapsed, depth, path) {
          *          return rowData.id;
          *      }
          *
