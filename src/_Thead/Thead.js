2ab2789db9b90bbf0263008b93c1e3e6f48cb244
liangxiaojun <fatalxiao@163.com>
2019-07-10 16:38:58 +0800

    update Thead

--- src/_Thead/Thead.js
+++ src/_Thead/Thead.js
@@ -179,7 +179,7 @@ Thead.propTypes = {
         /**
          * The render content in table body.
          *  (1) callback:
-         *      function (rowData, rowIndex, colIndex) {
+         *      function (rowData, rowIndex, colIndex, parentData, tableData, collapsed, depth, path) {
          *          return rowData.id;
          *      }
          *
