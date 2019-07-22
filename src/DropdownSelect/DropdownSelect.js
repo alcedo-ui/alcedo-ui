36b22fccb11f69f84e9265c551d699e5d0ae98ff
liangxiaojun <fatalxiao@163.com>
2019-07-22 14:59:35 +0800

    update DropdownSelect

--- src/DropdownSelect/DropdownSelect.js
+++ src/DropdownSelect/DropdownSelect.js
@@ -321,7 +321,7 @@ class DropdownSelect extends Component {
                 checkboxUncheckedIconCls, checkboxCheckedIconCls, checkboxIndeterminateIconCls, isGrouped,
 
                 // not passing down these props
-                clearHiddenInputFilterInterval, onOpenPopup, onClosePopup,
+                clearHiddenInputFilterInterval, onOpenPopup, onClosePopup, onItemClick,
 
                 ...restProps
 
