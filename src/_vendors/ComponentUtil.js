a9c077804d0830f72239529d32026c7f70d8b7e6
liangxiaojun <fatalxiao@163.com>
2019-09-09 10:42:07 +0800

    update ComponentUtil

--- src/_vendors/ComponentUtil.js
+++ src/_vendors/ComponentUtil.js
@@ -18,6 +18,47 @@ function getDerivedState(props, state, key) {
 
 }
 
+function is(x, y) {
+    return x === y ?
+        x !== 0 || y !== 0 || 1 / x === 1 / y
+        :
+        x !== x && y !== y;
+}
+
+function isPropsEqual(x, y, excludes) {
+
+    if (is(x, y)) {
+        return true;
+    }
+
+    if (typeof x !== 'object' || x === null || typeof y !== 'object' || y === null) {
+        return false;
+    }
+
+    const xKeys = Object.keys(x),
+        yKeys = Object.keys(y);
+
+    if (xKeys.length !== yKeys.length) {
+        return false;
+    }
+
+    for (let i = 0; i < xKeys.length; i++) {
+
+        if (excludes.includes(xKeys[i]) || typeof x[xKeys[i]] === 'function' || typeof y[yKeys[i]] === 'function') {
+            continue;
+        }
+
+        if (!hasOwnProperty.call(y, xKeys[i]) || !is(x[xKeys[i]], y[xKeys[i]])) {
+            return false;
+        }
+
+    }
+
+    return true;
+
+}
+
 export default {
-    getDerivedState
+    getDerivedState,
+    isPropsEqual
 };
