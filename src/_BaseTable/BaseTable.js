df89402ecddc7b1f3f01d571e44db4631abe14af
liangxiaojun <fatalxiao@163.com>
2019-12-17 17:26:21 +0800

    update BaseTable

--- src/_BaseTable/BaseTable.js
+++ src/_BaseTable/BaseTable.js
@@ -338,7 +338,7 @@ BaseTable.propTypes = {
     expandRows: PropTypes.array,
     isMouseEventForbidden: PropTypes.bool,
     ignoreColumnWidth: PropTypes.bool,
-    parentEl: PropTypes.object,
+    scrollEl: PropTypes.object,
 
     /**
      * fixed
