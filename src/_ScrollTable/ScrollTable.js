9d25eba5684ab28701aa8b6512a39ee7a8046459
liangxiaojun <fatalxiao@163.com>
2020-01-14 10:59:15 +0800

    update ScrollTable

--- src/_ScrollTable/ScrollTable.js
+++ src/_ScrollTable/ScrollTable.js
@@ -116,7 +116,6 @@ class ScrollTable extends Component {
                                                    columns={columns}
                                                    headColumns={headColumns}
                                                    bodyColumns={bodyColumns}
-                                                   ignoreColumnSpan={true}
                                                    scrollEl={scrollEl}
                                                    onRequestColumnsSpan={this.handleColumnsSpan}/>
                                 }
