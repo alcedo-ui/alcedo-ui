c52b320c7d9044fd9df6c68bd591053b12adc5fe
liangxiaojun <fatalxiao@163.com>
2020-04-08 09:32:38 +0800

    update Tr

--- src/_Tr/Tr.js
+++ src/_Tr/Tr.js
@@ -127,6 +127,7 @@ class Tr extends Component {
                                 rowIndex={rowIndex}
                                 colIndex={baseColIndex + colIndex}
                                 data={data}
+                                title={column.bodyTitle}
                                 parentData={parentData}
                                 tableData={tableData}
                                 collapsed={collapsed}
@@ -242,6 +243,18 @@ Tr.propTypes = {
         headRenderer: PropTypes.any,
 
         /**
+         * The title in table head.
+         *  (1) callback:
+         *      function (tableData, colIndex) {
+         *          return colIndex;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        headTitle: PropTypes.any,
+
+        /**
          * column span of table header.
          *  (1) function callback:
          *      function (tableData, colIndex) {
@@ -286,6 +299,18 @@ Tr.propTypes = {
         bodyRenderer: PropTypes.any,
 
         /**
+         * The title in table body.
+         *  (1) callback:
+         *      function (rowData, rowIndex, colIndex, parentData, tableData, collapsed, depth, path) {
+         *          return rowData.id;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        bodyTitle: PropTypes.any,
+
+        /**
          * column span of table body.
          *  (1) function callback:
          *      function (rowData, colIndex, rowIndex) {
@@ -328,6 +353,18 @@ Tr.propTypes = {
          *      render whatever you pass
          */
         footRenderer: PropTypes.any,
+
+        /**
+         * The title in table foot.
+         *  (1) callback:
+         *      function (tableData, colIndex) {
+         *          return colIndex;
+         *      }
+         *
+         *  (2) others:
+         *      render whatever you pass
+         */
+        footTitle: PropTypes.any,
 
         /**
          * column span of table foot.
