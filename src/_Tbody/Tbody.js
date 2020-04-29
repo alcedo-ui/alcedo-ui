8eabf0654c604e064e01b10c7edad2c80b2731c8
liangxiaojun <fatalxiao@163.com>
2020-04-29 11:10:17 +0800

    update Tbody

--- src/_Tbody/Tbody.js
+++ src/_Tbody/Tbody.js
@@ -132,7 +132,7 @@ Tbody.propTypes = {
         /**
          * width of column.
          */
-        width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+        width: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.func]),
 
         /**
          * minimum width of column.
