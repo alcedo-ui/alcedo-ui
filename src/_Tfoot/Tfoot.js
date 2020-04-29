3e4c931ba5dbb8c5d438139fb7e3d8466327f09c
liangxiaojun <fatalxiao@163.com>
2020-04-29 11:10:28 +0800

    update Tfoot

--- src/_Tfoot/Tfoot.js
+++ src/_Tfoot/Tfoot.js
@@ -151,7 +151,7 @@ Tfoot.propTypes = {
         /**
          * width of column.
          */
-        width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+        width: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.func]),
 
         /**
          * minimum width of column.
