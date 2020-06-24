3d59819fb428e2ac4036de72ae519a4e83fdbaf5
liangxiaojun <fatalxiao@163.com>
2020-06-24 17:16:06 +0800

    update LandingExamples

--- examples/containers/landing/sections/LandingExamples.js
+++ examples/containers/landing/sections/LandingExamples.js
@@ -1,54 +1,56 @@
-import React, {Component} from 'react';
+/**
+ * @file LandingExamples.js
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+import React, {useCallback} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 import * as actions from 'reduxes/actions';
 
+// Components
 import Paper from 'src/Paper';
 import FlatButton from 'src/FlatButton';
 
+// Styles
 import 'scss/containers/landing/sections/LandingExamples.scss';
 
-class LandingExamples extends Component {
+function LandingExamples({routerPush}) {
 
-    constructor(props) {
-        super(props);
-    }
+    const goToExamples = useCallback(() => {
+        routerPush('/components');
+    });
 
-    goToExamples = () => {
-        this.props.routerPush('/components');
-    };
+    return (
+        <section id="landing-examples"
+                 className="landing-section">
 
-    render() {
-        return (
-            <section id="landing-examples"
-                     className="landing-section">
+            <div className="landing-section-inner">
 
-                <div className="landing-section-inner">
+                <div className="landing-section-title">Examples</div>
 
-                    <div className="landing-section-title">Examples</div>
+                <FlatButton className="ghost-button"
+                            value="Go to Examples"
+                            onClick={goToExamples}/>
 
-                    <FlatButton className="ghost-button"
-                                value="Go to Examples"
-                                onClick={this.goToExamples}/>
+                <Paper className="landing-examples-demo"
+                       depth={4}>
+                    <div className="window-header">
+                        <div className="window-header-dot"></div>
+                        <div className="window-header-dot"></div>
+                        <div className="window-header-dot"></div>
+                    </div>
+                    <img className="landing-examples-demo-img"
+                         src={import('assets/images/example.jpg')}/>
+                </Paper>
 
-                    <Paper className="landing-examples-demo"
-                           depth={4}>
-                        <div className="window-header">
-                            <div className="window-header-dot"></div>
-                            <div className="window-header-dot"></div>
-                            <div className="window-header-dot"></div>
-                        </div>
-                        <img className="landing-examples-demo-img"
-                             src={import('assets/images/example.jpg')}/>
-                    </Paper>
+            </div>
 
-                </div>
+        </section>
+    );
 
-            </section>
-        );
-    }
 }
 
 LandingExamples.propTypes = {
