f80144cbc29dc21c16862c0041d315ba44a14eee
liangxiaojun <fatalxiao@163.com>
2020-06-24 10:15:04 +0800

    update LandingFoot

--- examples/containers/landing/LandingFoot.js
+++ examples/containers/landing/LandingFoot.js
@@ -1,36 +1,36 @@
-import React, {Component} from 'react';
+/**
+ * @file LandingFoot.js
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
 
+import React from 'react';
+
+// Components
 import IconAnchor from 'src/IconAnchor';
 
+// Styles
 import 'scss/containers/landing/LandingFoot.scss';
 
-class LandingFoot extends Component {
+function LandingFoot() {
+    return (
+        <div className="landing-foot">
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="landing-foot">
-
-                <div>
-                    <span className="name">Alcedo-UI </span>
-                    is freely distributable under the terms of the
-                    <a className="licence"
-                       href="https://github.com/alcedo-ui/alcedo-ui/blob/dev/LICENSE">
-                        {' MIT license'}
-                    </a>.
-                </div>
-
-                <IconAnchor className="github-icon"
-                            iconCls="fab fa-github"
-                            href="https://github.com/alcedo-ui/alcedo-ui"/>
-
+            <div>
+                <span className="name">Alcedo-UI</span>
+                is freely distributable under the terms of the
+                <a className="licence"
+                   href="https://github.com/alcedo-ui/alcedo-ui/blob/dev/LICENSE">
+                    {' MIT license'}
+                </a>.
             </div>
 
-        );
-    }
+            <IconAnchor className="github-icon"
+                        iconCls="fab fa-github"
+                        href="https://github.com/alcedo-ui/alcedo-ui"/>
+
+        </div>
+
+    );
 }
 
 export default LandingFoot;
