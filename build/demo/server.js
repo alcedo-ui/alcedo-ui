f3a62e6f152e4a6d239bf44bc05b560991f4afce
liangxiaojun <fatalxiao@163.com>
2020-10-12 16:53:22 +0800

    update demo server

--- build/demo/server.js
+++ build/demo/server.js
@@ -1,7 +1,6 @@
 const express = require('express'),
     history = require('connect-history-api-fallback'),
     open = require('open'),
-    compression = require('compression'),
     log = require('friendly-errors-webpack-plugin/src/output'),
 
     config = require('../config.js'),
@@ -9,24 +8,23 @@ const express = require('express'),
     app = express(),
     uri = 'http://localhost:' + config.demo.port;
 
-app.use(compression())
-   .use(history())
-   .use(express.static(config.build.assetsRoot, {
-       setHeaders: (res, path) => {
-           res.setHeader('Cache-Control', path.endsWith('index.html') ?
-               'no-cache, no-store, no_store, max-age=0, must-revalidate' : 'max-age=315360000'
-           );
-       }
-   }))
-   .listen(config.demo.port, err => {
+app.use(history())
+    .use(express.static(config.build.assetsRoot, {
+        setHeaders: (res, path) => {
+            res.setHeader('Cache-Control', path.endsWith('index.html') ?
+                'no-cache, no-store, no_store, max-age=0, must-revalidate' : 'max-age=315360000'
+            );
+        }
+    }))
+    .listen(config.demo.port, err => {
 
-       if (err) {
-           log.title('error', 'ERROR', `${err}`);
-           return;
-       }
+        if (err) {
+            log.title('error', 'ERROR', `${err}`);
+            return;
+        }
 
-       log.title('success', 'DONE', `Listening At ${uri}`);
+        log.title('success', 'DONE', `Listening At ${uri}`);
 
-       open(uri);
+        open(uri);
 
-   });
+    });
