f2e1cfa74a82ed7e9675a5856910af0302c3f78a
liangxiaojun <fatalxiao@163.com>
2021-03-25 10:37:21 +0800

    update Popup

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -77,7 +77,7 @@ class Popup extends Component {
     triggerHandler = (el, triggerEl, popupEl, currentVisible, isBlurClose) => {
 
         // el is missing
-        if (el && !domContains(document.body, el)) {
+        if (el && !domContains(document, el)) {
             return currentVisible;
         }
 
