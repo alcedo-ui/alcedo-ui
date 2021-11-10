0ddbe3637a52a0a6f74d216390bb358d2f0b0d96
liangxiaojun <fatalxiao@163.com>
2021-11-10 11:01:53 +0800

    update Landing

--- examples/containers/landing/Landing.js
+++ examples/containers/landing/Landing.js
@@ -4,7 +4,6 @@
  */
 
 import React, {useState, useCallback, useEffect, useRef} from 'react';
-import {findDOMNode} from 'react-dom';
 
 // Components
 import LandingNav from './LandingNav';
@@ -16,87 +15,83 @@ import LandingExamples from './sections/LandingExamples';
 import LandingFoot from './LandingFoot';
 
 // Vendors
+import {findDOMNode} from 'react-dom';
 import Event from 'vendors/Event';
 
 // Styles
 import 'scss/containers/landing/Landing.scss';
 
+// eslint-disable-next-line require-jsdoc
 function Landing() {
 
-    const MENU = [{
-            name: 'Intro',
-            hash: '#landing-intro'
-        }, {
-            name: 'Get Started',
-            hash: '#landing-get-started'
-        }, {
-            name: 'Usage',
-            hash: '#landing-usage'
-        }, {
-            name: 'Examples',
-            hash: '#landing-examples'
-        }],
+    const nav = useRef(null);
 
-        nav = useRef(null),
+    const [bodyScrollTop, setBodyScrollTop] = useState(0);
+    const [activatedMenu, setActivatedMenu] = useState(Landing.MENU[0]);
+    const [isNavFixed, setIsNavFixed] = useState(false);
+    const [navHeight, setNavHeight] = useState(0);
 
-        [bodyScrollTop, setBodyScrollTop] = useState(0),
-        [activatedMenu, setActivatedMenu] = useState(MENU[0]),
-        [isNavFixed, setIsNavFixed] = useState(false),
-        [navHeight, setNavHeight] = useState(0),
+    const getIsNavFixed = useCallback(bodyScrollTop => {
+        const introEl = document.querySelector(Landing.MENU[0].hash);
+        return !!(introEl && (bodyScrollTop > introEl.clientHeight - navHeight));
+    }, [
+        navHeight
+    ]);
 
-        getIsNavFixed = useCallback(bodyScrollTop => {
-            const introEl = document.querySelector(MENU[0].hash);
-            return !!(introEl && (bodyScrollTop > introEl.clientHeight - navHeight));
-        }, [navHeight]),
+    const getActivatedMenu = useCallback(bodyScrollTop => {
 
-        getActivatedMenu = useCallback(bodyScrollTop => {
+        let activatedMenu = Landing.MENU[0];
 
-            let activatedMenu = MENU[0];
-
-            for (let i = 0, len = MENU.length; i < len; i++) {
-                const el = document.querySelector(MENU[i].hash);
-                if (el && bodyScrollTop >= el.offsetTop - navHeight) {
-                    activatedMenu = MENU[i];
-                }
+        for (let i = 0, len = Landing.MENU.length; i < len; i++) {
+            const el = document.querySelector(Landing.MENU[i].hash);
+            if (el && bodyScrollTop >= el.offsetTop - navHeight) {
+                activatedMenu = Landing.MENU[i];
             }
+        }
 
-            return activatedMenu;
+        return activatedMenu;
 
-        }),
+    }, [
+        navHeight
+    ]);
 
-        handleScroll = useCallback(() => {
+    const handleScroll = useCallback(() => {
 
-            const nextBodyScrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+        const nextBodyScrollTop = document.body.scrollTop || document.documentElement.scrollTop;
 
-            if (nextBodyScrollTop !== bodyScrollTop) {
-                setBodyScrollTop(nextBodyScrollTop);
-                setActivatedMenu(getActivatedMenu(nextBodyScrollTop));
-                setIsNavFixed(getIsNavFixed(nextBodyScrollTop));
-            }
+        if (nextBodyScrollTop !== bodyScrollTop) {
+            setBodyScrollTop(nextBodyScrollTop);
+            setActivatedMenu(getActivatedMenu(nextBodyScrollTop));
+            setIsNavFixed(getIsNavFixed(nextBodyScrollTop));
+        }
 
-        }, [bodyScrollTop]);
+    }, [
+        bodyScrollTop,
+        getActivatedMenu, getIsNavFixed
+    ]);
 
     useEffect(() => {
 
-        // mount
         Event.addEvent(document, 'scroll', handleScroll);
 
         const navEl = findDOMNode(nav?.current);
         setNavHeight(navEl?.offsetHeight || 80);
         handleScroll();
 
-        // unmount
         return () => {
             Event.removeEvent(document, 'scroll', handleScroll);
         };
 
-    }, [bodyScrollTop, navHeight]);
+    }, [
+        bodyScrollTop, navHeight,
+        handleScroll
+    ]);
 
     return (
         <div className="landing">
 
             <LandingNav ref={nav}
-                        menu={MENU}
+                        menu={Landing.MENU}
                         activatedMenu={activatedMenu}
                         isFixed={isNavFixed}/>
 
@@ -116,5 +111,19 @@ function Landing() {
     );
 
 }
+
+Landing.MENU = [{
+    name: 'Intro',
+    hash: '#landing-intro'
+}, {
+    name: 'Get Started',
+    hash: '#landing-get-started'
+}, {
+    name: 'Usage',
+    hash: '#landing-usage'
+}, {
+    name: 'Examples',
+    hash: '#landing-examples'
+}];
 
 export default Landing;
