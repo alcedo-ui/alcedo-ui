e6208ea7db19b09a9dc7b9f7b46474c006d19bb4
liangxiaojun <fatalxiao@163.com>
2021-11-17 09:40:19 +0800

    update DropdownCalculation

--- src/_vendors/DropdownCalculation.js
+++ src/_vendors/DropdownCalculation.js
@@ -2,28 +2,30 @@
  * @file DropdownCalculation vendor
  */
 
+// Vendors
 import Dom from './Dom';
 
-function isAbove(dropdownEl, triggerEl, popupEl) {
+/**
+ * @param dropdownEl
+ * @param triggerEl
+ * @param popupEl
+ * @returns {boolean}
+ */
+export function isAbove(dropdownEl, triggerEl, popupEl) {
 
     if (!dropdownEl || !triggerEl || !popupEl) {
         return false;
     }
 
-    const {top} = Dom.getOffset(dropdownEl),
-        scrollTop = Dom.getScrollTop(),
-        triggerHeight = triggerEl.clientHeight,
-        popupHeight = popupEl.clientHeight;
+    const {top} = Dom.getOffset(dropdownEl);
+    const scrollTop = Dom.getScrollTop();
+    const triggerHeight = triggerEl.clientHeight;
+    const popupHeight = popupEl.clientHeight;
 
-    if (
+    return (
         top + triggerHeight + popupHeight - scrollTop > window.innerHeight // bottom overflow
-        &&
-        top - popupHeight - scrollTop >= 0 // no top overflow
-    ) {
-        return true;
-    }
-
-    return false;
+        && top - popupHeight - scrollTop >= 0 // no top overflow
+    );
 
 }
 
