d1e40ef1399f361e6abaf582844e2bfe62f09b75
liangxiaojun <fatalxiao@163.com>
2021-12-10 17:04:40 +0800

    update SkeletonLoading

--- src/SkeletonLoading/SkeletonLoading.js
+++ src/SkeletonLoading/SkeletonLoading.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 
 // Vendors
 import classNames from 'classnames';
+import {hex2rgb} from '../_vendors/Color';
 
 class SkeletonLoading extends Component {
 
@@ -18,8 +19,11 @@ class SkeletonLoading extends Component {
 
         const {
             className, style,
-            isCircular, isAnimated, animationDuration
+            size, animationDuration, highlightColor, highlightDegree, highlightTransparency,
+            isCircular, isAnimated
         } = this.props;
+
+        const highlightColorRgb = hex2rgb(highlightColor)?.join(',') || '255,255,255';
 
         return (
             <div className={classNames('skeleton-loading', {
@@ -29,7 +33,22 @@ class SkeletonLoading extends Component {
             })}
                  style={{
                      ...style,
-                     animationDuration: animationDuration && typeof animationDuration === 'number' ?
+                     width: !isNaN(size) ?
+                         isCircular ?
+                             size
+                             :
+                             null
+                         :
+                         null,
+                     height: !isNaN(size) ?
+                         size
+                         :
+                         null,
+                     backgroundImage: !isNaN(highlightDegree) && highlightColorRgb && !isNaN(highlightTransparency) ?
+                         `linear-gradient(${highlightDegree}deg, rgba(${highlightColorRgb}, 0) 40%, rgba(${highlightColorRgb}, ${highlightTransparency}) 50%, rgba(${highlightColorRgb}, 0) 60%)`
+                         :
+                         null,
+                     animationDuration: !isNaN(animationDuration) ?
                          `${animationDuration}ms`
                          :
                          null
@@ -51,16 +70,27 @@ SkeletonLoading.propTypes = {
      */
     style: PropTypes.object,
 
-    isCircular: PropTypes.bool,
-    isAnimated: PropTypes.bool,
+    size: PropTypes.number,
+    animationDuration: PropTypes.number,
+    highlightColor: PropTypes.string,
+    highlightDegree: PropTypes.number,
+    highlightTransparency: PropTypes.number,
 
-    animationDuration: PropTypes.number
+    isCircular: PropTypes.bool,
+    isAnimated: PropTypes.bool
 
 };
 
 SkeletonLoading.defaultProps = {
+
+    size: 24,
+    highlightColor: '#fff',
+    highlightDegree: 100,
+    highlightTransparency: .4,
+
     isCircular: false,
     isAnimated: true
+
 };
 
 export default SkeletonLoading;
